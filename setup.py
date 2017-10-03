4773cd2ebb3370449fa2ff32c9f17a695c0138c9
Jonathan Prescott-Roy <jjpr@mit.edu>
2017-10-03 17:19:21 -0400

    Added dependencies.

--- setup.py
+++ setup.py
@@ -10,7 +10,12 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    # TODO: put package requirements here
+    "numpy",
+    "pandas",
+    "xarray",
+    "six",
+    "requests",
+    "boto3"
 ]
 
 test_requirements = [

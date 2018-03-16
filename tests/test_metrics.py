93ea69660d605d136af9511ebd495a536407e676
Martin Schrimpf <m4rtinsch@gmail.com>
2018-03-16 15:36:18 -0400

    add RDMMetric using RDM correlation coefficient; add citation

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -14,16 +14,13 @@ import numpy as np
 from pytest import approx
 
 import mkgu
-from mkgu.metrics.rdm import RSA
+from mkgu.metrics.rdm import RSA, RDMMetric
 
 
 def test_hvm_it_rdm():
     loaded = np.load(os.path.join(os.path.dirname(__file__), "it_rdm.p"), encoding="latin1")
 
-    assy_hvm = mkgu.get_assembly(name="HvM")
-    hvm_it_v6 = assy_hvm.sel(var="V6").sel(region="IT")
-    hvm_it_v6.load()
-    hvm_it_v6_obj = hvm_it_v6.multi_groupby(["category", "obj"]).mean(dim="presentation").squeeze("time_bin").T
+    hvm_it_v6_obj = _load_hvm()
 
     assert hvm_it_v6_obj.shape == (64, 168)
 
@@ -32,3 +29,18 @@ def test_hvm_it_rdm():
 
     assert rsa.shape == (64, 64)
     assert rsa == approx(loaded, abs=1e-6)
+
+
+def test_rdm_metric():
+    hvm = _load_hvm()
+    rdm_metric = RDMMetric()
+    score = rdm_metric.apply(hvm, hvm)
+    assert score == 1.
+
+
+def _load_hvm():
+    assy_hvm = mkgu.get_assembly(name="HvM")
+    hvm_it_v6 = assy_hvm.sel(var="V6").sel(region="IT")
+    hvm_it_v6.load()
+    hvm_it_v6_obj = hvm_it_v6.multi_groupby(["category", "obj"]).mean(dim="presentation").squeeze("time_bin").T
+    return hvm_it_v6_obj

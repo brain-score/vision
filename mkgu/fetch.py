b7a49ef19d43baba7d3b7b05d2012cbd0a117076
Jonathan Prescott-Roy <jjpr@mit.edu>
2018-04-11 16:38:07 -0400

    Converting lookup to peewee.

--- mkgu/fetch.py
+++ mkgu/fetch.py
@@ -2,14 +2,13 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import hashlib
 import os
-import sqlite3
-
+import peewee
 import boto3
 import xarray as xr
 from six.moves.urllib.parse import urlparse
 
 from mkgu import assemblies
-from mkgu.assemblies import gather_indexes
+from mkgu.lookup import get_lookup
 
 _local_data_path = os.path.expanduser("~/.mkgu/data")
 
@@ -81,111 +80,6 @@ class LocalFetcher(Fetcher):
         super(LocalFetcher, self).__init__(location, assembly_name)
 
 
-class Lookup(object):
-    """A Lookup gives an abstraction layer for findoing out where a DataAssembly can be fetched from.  """
-    def __init__(self):
-        pass
-
-    def lookup_assembly(self, name):
-        pass
-
-
-class SQLiteLookup(Lookup):
-    """A Lookup that uses a local SQLite file.  """
-    sql_lookup_assy = """SELECT
-    a.id as a_id, a.name, a.class
-    FROM
-    assembly a
-    WHERE
-    a.name = ?
-    """
-
-    sql_get_assy = """SELECT
-    a.id as a_id, a.name, a.class, a_s.id as a_s_id, a_s.role,
-    s.id as s_id, s.type, s.location
-    FROM
-    assembly a
-    JOIN assembly_store a_s ON a.id = a_s.assembly_id
-    JOIN store s ON a_s.store_id = s.id
-    WHERE
-    a.name = ?
-    """
-
-    def __init__(self, db_file=None):
-        super(SQLiteLookup, self).__init__()
-        if db_file is None:
-            db_file = os.path.join(os.path.dirname(__file__), "lookup.db")
-        self.db_file = db_file
-
-    def get_connection(self):
-        if not hasattr(self, '_connection'):
-            self._connection = sqlite3.connect(self.db_file)
-            self._connection.row_factory = sqlite3.Row
-        return self._connection
-
-    def lookup_assembly(self, name):
-        conn = self.get_connection()
-        cursor = conn.cursor()
-        cursor.execute(self.sql_lookup_assy, (name,))
-        assy_result = cursor.fetchone()
-        if assy_result:
-            assy = AssemblyRecord(assy_result["a_id"], assy_result["name"], assy_result["class"])
-            cursor.execute(self.sql_get_assy, (name,))
-            assy_store_result = cursor.fetchall()
-            for r in assy_store_result:
-                s = Store(r["s_id"], r["type"], r["location"], [assy])
-                role = r["role"]
-                a_s = AssemblyStoreMap(r["a_s_id"], role, s, assy)
-                assy.stores[role] = a_s
-            return assy
-        else:
-            raise AssemblyLookupError("A DataAssembly named " + name + " was not found.")
-
-
-class PostgreSQLLookup(Lookup):
-    """A Lookup that uses a Postgres database.  """
-    def __init__(self):
-        super(PostgreSQLLookup, self).__init__()
-
-
-class WebServiceLookup(Lookup):
-    """A Lookup that uses a web service.  """
-    def __init__(self):
-        super(WebServiceLookup, self).__init__()
-
-
-class AssemblyRecord(object):
-    """An AssemblyRecord stores information about the canonical location where the data
-    for a DataAssembly is stored.  """
-    def __init__(self, db_id, name, cls, stores=None):
-        self.db_id = db_id
-        self.name = name
-        self.cls = cls
-        if stores is None:
-            stores = {}
-        self.stores = stores
-
-
-class AssemblyStoreMap(object):
-    """An AssemblyStoreMap links an AssemblyRecord to a Store.  """
-    def __init__(self, db_id, role, store, assembly_record):
-        self.db_id = db_id
-        self.role = role
-        self.store = store
-        self.assembly_record = assembly_record
-
-
-class Store(object):
-    """A Store stores the location of a DataAssembly data file.  """
-    def __init__(self, db_id, type, location, assemblies=None):
-        self.db_id = db_id
-        self.type = type
-        self.location = location
-        if assemblies is None:
-            assemblies = []
-        self.assemblies = assemblies
-
-
 class Loader(object):
     """
     Loads a DataAssembly from files.
@@ -205,10 +99,6 @@ class Loader(object):
         return result
 
 
-class AssemblyLookupError(Exception):
-    pass
-
-
 class AssemblyFetchError(Exception):
     pass
 
@@ -222,17 +112,6 @@ _fetcher_types = {
 
 def get_fetcher(type="S3", location=None, assembly_name=None):
     return _fetcher_types[type](location, assembly_name)
-
-
-_lookup_types = {
-    "SQLite": SQLiteLookup,
-    "PostgreSQL": PostgreSQLLookup,
-    "WebService": WebServiceLookup
-}
-
-
-def get_lookup(type="SQLite"):
-    return _lookup_types[type]()
 
 
 def fetch_assembly(assy_record):

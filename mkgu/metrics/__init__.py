98ef5615b2ae11aa52e7a47ce669914ff311d078
Martin Schrimpf <m4rtinsch@gmail.com>
2018-04-27 08:32:01 -0400

    make Similarity compute over all adjacent dims

--- mkgu/metrics/__init__.py
+++ mkgu/metrics/__init__.py
@@ -1,6 +1,11 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import itertools
+
+import numpy as np
 from abc import ABCMeta, abstractmethod
+
+from mkgu.assemblies import DataAssembly
 
 
 class Benchmark(object):
@@ -34,8 +39,52 @@ class Metric(object):
 
 
 class Similarity(object, metaclass=ABCMeta):
+    def __call__(self, source_assembly, target_assembly, similarity_dim='neuroid'):
+        """
+        :param mkgu.assemblies.NeuroidAssembly source_assembly:
+        :param mkgu.assemblies.NeuroidAssembly target_assembly:
+        :param str similarity_dim: the dimension in both assemblies along which the similarity is to be computed
+        :return: mkgu.assemblies.DataAssembly
+        """
+
+        # compute similarities
+        def iter_indices(assembly):
+            dim_index = np.where(np.array(assembly.dims) == similarity_dim)[0][0]
+
+            def insert_slice(combination):
+                combination.insert(dim_index, slice(None))
+                return combination
+
+            return [insert_slice(list(combination)) for combination in
+                    itertools.product(*[list(range(len(assembly[_dim])))
+                                        for _dim in filter(lambda _dim: _dim != similarity_dim, assembly.dims)])]
+
+        indices1, indices2 = iter_indices(source_assembly), iter_indices(target_assembly)
+        similarities = np.array([self.apply(source_assembly.values[ids1], target_assembly.values[ids2])
+                                 for ids1, ids2 in itertools.product(indices1, indices2)])
+
+        # package in assembly
+        coords = list(source_assembly.coords.keys()) + list(target_assembly.coords.keys())
+        duplicate_coords = [coord for coord in coords if coords.count(coord) > 1]
+        coords = {**self._collect_coords(source_assembly, duplicate_coords, ignore_coord=similarity_dim, kind='left'),
+                  **self._collect_coords(target_assembly, duplicate_coords, ignore_coord=similarity_dim, kind='right')}
+        dims = {**self._collect_dim_shapes(source_assembly, duplicate_coords, ignore_dim=similarity_dim, kind='left'),
+                **self._collect_dim_shapes(target_assembly, duplicate_coords, ignore_dim=similarity_dim, kind='right')}
+        similarities = similarities.reshape(list(itertools.chain(*dims.values())))
+        return DataAssembly(similarities, coords=coords, dims=dims.keys())
+
+    def _collect_coords(self, assembly, duplicate_coords, ignore_coord, kind):
+        coord_names = {coord: coord if coord not in duplicate_coords else coord + '-' + kind
+                       for coord in assembly.coords}
+        return {coord_names[coord]: (tuple(coord_names[dim] for dim in values.dims), values.values)
+                for coord, values in assembly.coords.items() if coord is not ignore_coord}
+
+    def _collect_dim_shapes(self, assembly, duplicate_dims, ignore_dim, kind):
+        return {dim if dim not in duplicate_dims else dim + '-' + kind:
+                    assembly[dim].shape for dim in assembly.dims if dim is not ignore_dim}
+
     @abstractmethod
-    def __call__(self, source_assembly, target_assembly):
+    def apply(self, source_assembly, target_assembly):
         raise NotImplementedError()
 
 
---
+++ tests/test_metrics/test___init__.py
@@ -0,0 +1,69 @@
+import itertools
+
+import numpy as np
+
+from mkgu.assemblies import NeuroidAssembly
+from mkgu.metrics import Similarity
+
+
+class SimilarityDummy(Similarity):
+    def __init__(self):
+        self.source_assemblies = []
+        self.target_assemblies = []
+
+    def apply(self, source_assembly, target_assembly):
+        self.source_assemblies.append(source_assembly)
+        self.target_assemblies.append(target_assembly)
+
+
+class TestSimilarity:
+    def test_no_adjacent(self):
+        assembly = np.random.rand(100)
+        assembly = NeuroidAssembly(assembly, coords={'neuroid': list(range(len(assembly)))}, dims=['neuroid'])
+        similarity = SimilarityDummy()
+        similarity(assembly, assembly)
+        assert 1 == len(similarity.source_assemblies) == len(similarity.target_assemblies)
+        assert assembly.equals(similarity.source_assemblies[0])
+        assert assembly.equals(similarity.target_assemblies[0])
+
+    def test_one_adjacent(self):
+        assembly = np.random.rand(100, 3)
+        assembly = NeuroidAssembly(
+            assembly,
+            coords={'neuroid': list(range(len(assembly))), 'adjacent_coord': list(range(assembly.shape[1]))},
+            dims=['neuroid', 'adjacent_coord'])
+        similarity = SimilarityDummy()
+        similarity(assembly, assembly)
+        assert np.power(assembly.shape[1], 2) == len(similarity.source_assemblies) == len(similarity.target_assemblies)
+        pairs = list(zip(similarity.source_assemblies, similarity.target_assemblies))
+        target_pairs = [(assembly.sel(adjacent_coord=i).rename({'adjacent_coord': 'adjacent_coord-left'}).values,
+                         assembly.sel(adjacent_coord=j).rename({'adjacent_coord': 'adjacent_coord-right'}).values)
+                        for i, j in itertools.product(*([list(range(assembly.shape[1]))] * 2))]
+        for source_values, target_values in target_pairs:
+            match = False
+            for source_actual, target_actual in pairs:
+                if all(source_values == source_actual) and all(target_values == target_actual):
+                    match = True
+                    break
+            assert match, "pair {} - {} not found".format(source_values, target_values)
+
+    def test_one_adjacent_similarity_dim_last(self):
+        assembly = np.random.rand(3, 100)
+        assembly = NeuroidAssembly(
+            assembly,
+            coords={'neuroid': list(range(assembly.shape[1])), 'adjacent_coord': list(range(assembly.shape[0]))},
+            dims=['adjacent_coord', 'neuroid'])
+        similarity = SimilarityDummy()
+        similarity(assembly, assembly)
+        assert np.power(assembly.shape[0], 2) == len(similarity.source_assemblies) == len(similarity.target_assemblies)
+        pairs = list(zip(similarity.source_assemblies, similarity.target_assemblies))
+        target_pairs = [(assembly.sel(adjacent_coord=i).rename({'adjacent_coord': 'adjacent_coord-left'}).values,
+                         assembly.sel(adjacent_coord=j).rename({'adjacent_coord': 'adjacent_coord-right'}).values)
+                        for i, j in itertools.product(*([list(range(assembly.shape[0]))] * 2))]
+        for source_values, target_values in target_pairs:
+            match = False
+            for source_actual, target_actual in pairs:
+                if all(source_values == source_actual) and all(target_values == target_actual):
+                    match = True
+                    break
+            assert match, "pair {} - {} not found".format(source_values, target_values)

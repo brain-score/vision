9ed303b958cec2ac3959a664e93c02a3c64f2c22
Martin Schrimpf <m4rtinsch@gmail.com>
2018-05-01 13:03:20 -0700

    proper ordering and grouping of physiology benchmark assembly

--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import numpy as np
 
 from mkgu import benchmarks
 
@@ -8,3 +9,13 @@ class TestAnatomyFelleman:
     def test_equal(self):
         benchmark = benchmarks.load(data_name='Felleman1991', metric_name='edge_ratio')
         assert 1 == benchmark(benchmark._target_assembly)
+
+
+class TestMajaj2015:
+    def test_target_assembly(self):
+        benchmark = benchmarks.load(data_name='dicarlo.Hong2011', metric_name='rdm')
+        np.testing.assert_array_equal(benchmark._target_assembly.dims, ['presentation', 'neuroid'])
+        assert len(benchmark._target_assembly['presentation']) == 2560
+        assert len(benchmark._target_assembly['neuroid']) == 296
+        assert len(benchmark._target_assembly.sel(region='IT')['neuroid']) == 168
+        assert len(benchmark._target_assembly.sel(region='V4')['neuroid']) == 128

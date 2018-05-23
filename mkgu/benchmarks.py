acb4fca5b3d0ff1e4eca29a295bab453fd84bb4a
Martin Schrimpf <m4rtinsch@gmail.com>
2018-05-23 12:11:21 -0400

    exclude last 40 V4 neuroids

--- mkgu/benchmarks.py
+++ mkgu/benchmarks.py
@@ -42,6 +42,17 @@ def load(data_name, metric_name, metric_kwargs=None):
         data = mkgu.get_assembly(name=data_name)
         data.load()  # TODO: should load lazy
         if data_name == 'dicarlo.Majaj2015':
+            err_neuroids = ['Tito_L_P_8_5', 'Tito_L_P_7_3', 'Tito_L_P_7_5', 'Tito_L_P_5_1', 'Tito_L_P_9_3',
+                            'Tito_L_P_6_3', 'Tito_L_P_7_4', 'Tito_L_P_5_0', 'Tito_L_P_5_4', 'Tito_L_P_9_6',
+                            'Tito_L_P_0_4', 'Tito_L_P_4_6', 'Tito_L_P_5_6', 'Tito_L_P_7_6', 'Tito_L_P_9_8',
+                            'Tito_L_P_4_1', 'Tito_L_P_0_5', 'Tito_L_P_9_9', 'Tito_L_P_3_0', 'Tito_L_P_0_3',
+                            'Tito_L_P_6_6', 'Tito_L_P_5_7', 'Tito_L_P_1_1', 'Tito_L_P_3_8', 'Tito_L_P_1_6',
+                            'Tito_L_P_3_5', 'Tito_L_P_6_8', 'Tito_L_P_2_8', 'Tito_L_P_9_7', 'Tito_L_P_6_7',
+                            'Tito_L_P_1_0', 'Tito_L_P_4_5', 'Tito_L_P_4_9', 'Tito_L_P_7_8', 'Tito_L_P_4_7',
+                            'Tito_L_P_4_0', 'Tito_L_P_3_9', 'Tito_L_P_7_7', 'Tito_L_P_4_3', 'Tito_L_P_9_5']
+            good_neuroids = [i for i, neuroid_id in enumerate(data['neuroid_id'].values)
+                             if neuroid_id not in err_neuroids]
+            data = data.isel(neuroid=good_neuroids)
             data = data.sel(variation=6)  # TODO: remove variation selection once part of name
             data = data.multi_groupby(['category_name', 'object_name', 'image_id']).mean(dim="presentation")
             data = data.squeeze("time_bin")

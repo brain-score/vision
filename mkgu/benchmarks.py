18ecd0f025be683e5fa313396597dc27fc43fa7a
Martin Schrimpf <m4rtinsch@gmail.com>
2018-06-26 12:07:40 -0400

    add function to build own benchmark instead of using pre-defined ones

--- mkgu/benchmarks.py
+++ mkgu/benchmarks.py
@@ -43,23 +43,14 @@ class Benchmark(object):
     def ceiling(self):
         return self._ceiling(self._target_assembly)
 
-    def _load_assembly(self, name):
-        assembly = mkgu.get_assembly(name=name)
-        assembly.load()
-        return assembly
-
     def __repr__(self):
         return fullname(self)
 
 
-class DicarloMajaj2015(Benchmark):
-    def __init__(self):
-        assembly = self._load_assembly('dicarlo.Majaj2015')
-        metric = metrics['neural_fit']()
-        ceiling = ceilings['splitrep'](metric, average_repetition=self.average_repetition)
-        super(DicarloMajaj2015, self).__init__(assembly, metric, ceiling)
-
-        self._target_splits = ('region',)
+class SplitBenchmark(Benchmark):
+    def __init__(self, *args, target_splits, **kwargs):
+        super(SplitBenchmark, self).__init__(*args, **kwargs)
+        self._target_splits = target_splits
         cartesian_product = CartesianProduct()
         self._target_dividers = cartesian_product.dividers(self._target_assembly, self._target_splits)
 
@@ -73,8 +64,7 @@ class DicarloMajaj2015(Benchmark):
     def _apply(self, source_assembly, source_splits=()):
         transformations = Transformations(cartesian_product_kwargs=dict(
             dividing_coord_names_source=source_splits, dividing_coord_names_target=self._target_splits))
-        target_assembly = self.average_repetition(self._target_assembly)
-        scores = transformations(source_assembly, target_assembly, metric=self._metric)
+        scores = transformations(source_assembly, self._target_assembly, metric=self._metric)
         return scores
 
     def _ceil(self, score, ceiling):
@@ -88,27 +78,6 @@ class DicarloMajaj2015(Benchmark):
             selector = {**divider, **dict(aggregation='center')}
             _score.loc[selector] = _score.loc[selector] / _ceiling.loc[selector]
         return ceiled_score
-
-    def average_repetition(self, assembly):
-        return assembly.multi_groupby(['category_name', 'object_name', 'image_id']).mean(dim='presentation')
-
-    def _load_assembly(self, name):
-        assembly = super()._load_assembly(name)
-        err_neuroids = ['Tito_L_P_8_5', 'Tito_L_P_7_3', 'Tito_L_P_7_5', 'Tito_L_P_5_1', 'Tito_L_P_9_3',
-                        'Tito_L_P_6_3', 'Tito_L_P_7_4', 'Tito_L_P_5_0', 'Tito_L_P_5_4', 'Tito_L_P_9_6',
-                        'Tito_L_P_0_4', 'Tito_L_P_4_6', 'Tito_L_P_5_6', 'Tito_L_P_7_6', 'Tito_L_P_9_8',
-                        'Tito_L_P_4_1', 'Tito_L_P_0_5', 'Tito_L_P_9_9', 'Tito_L_P_3_0', 'Tito_L_P_0_3',
-                        'Tito_L_P_6_6', 'Tito_L_P_5_7', 'Tito_L_P_1_1', 'Tito_L_P_3_8', 'Tito_L_P_1_6',
-                        'Tito_L_P_3_5', 'Tito_L_P_6_8', 'Tito_L_P_2_8', 'Tito_L_P_9_7', 'Tito_L_P_6_7',
-                        'Tito_L_P_1_0', 'Tito_L_P_4_5', 'Tito_L_P_4_9', 'Tito_L_P_7_8', 'Tito_L_P_4_7',
-                        'Tito_L_P_4_0', 'Tito_L_P_3_9', 'Tito_L_P_7_7', 'Tito_L_P_4_3', 'Tito_L_P_9_5']
-        good_neuroids = [i for i, neuroid_id in enumerate(assembly['neuroid_id'].values)
-                         if neuroid_id not in err_neuroids]
-        assembly = assembly.isel(neuroid=good_neuroids)
-        assembly = assembly.sel(variation=6)  # TODO: remove variation selection once part of name
-        assembly = assembly.squeeze("time_bin")
-        assembly = assembly.transpose('presentation', 'neuroid')
-        return assembly
 
     @property
     def ceiling(self):
@@ -131,33 +100,83 @@ class DicarloMajaj2015(Benchmark):
         return scores
 
 
-class DicarloMajaj2015RDM(DicarloMajaj2015):
+class DicarloMajaj2015(SplitBenchmark):
     def __init__(self):
-        super(DicarloMajaj2015, self).__init__(assembly_name='dicarlo.Majaj2015', metric_name='rdm',
-                                               ceiling_name='splitrep', target_splits=('region',))
+        assembly = DicarloMajaj2015Loader()()
+        metric = metrics['neural_fit']()
+        ceiling = ceilings['splitrep'](metric, average_repetition=self.average_repetition)
+        super(DicarloMajaj2015, self).__init__(assembly, metric, ceiling, target_splits=('region',))
+
+    def _apply(self, source_assembly, source_splits=()):
+        target_assembly_save = copy.deepcopy(self._target_assembly)
+        self._target_assembly = self.average_repetition(self._target_assembly)
+        scores = super(DicarloMajaj2015, self)._apply(source_assembly, source_splits)
+        self._target_assembly = target_assembly_save
+        return scores
+
+    def average_repetition(self, assembly):
+        return assembly.multi_groupby(['category_name', 'object_name', 'image_id']).mean(dim='presentation')
 
 
 class GallantDavid2004(Benchmark):
-    def _load_assembly(self, name):
-        assembly = super()._load_assembly(name)
-        assembly = assembly.rename({'neuroid': 'neuroid_id'})
-        assembly = assembly.stack(neuroid=('neuroid_id',))
-        assembly = assembly.transpose('presentation', 'neuroid')
-        return assembly
-
-    def _load_metric(self, name):
-        return metrics[name](regression='linear')
+    # work in progress
+    def __init__(self):
+        assembly = GallantDavid2004Loader()()
+        metric = metrics['neural_fit'](regression='linear')
+        ceiling = ceilings['cons']()
+        super().__init__(assembly, metric, ceiling)
 
 
 class FellemanVanEssen1991(Benchmark):
+    # work in progress
     def __init__(self):
         self._target_assembly = ventral_stream
         self._metric = EdgeRatioMetric()
 
 
+class AssemblyLoader(object):
+    def __call__(self):
+        raise NotImplementedError()
+
+
+class DicarloMajaj2015Loader(AssemblyLoader):
+    def __call__(self):
+        assembly = mkgu.get_assembly(name='dicarlo.Majaj2015')
+        assembly.load()
+        err_neuroids = ['Tito_L_P_8_5', 'Tito_L_P_7_3', 'Tito_L_P_7_5', 'Tito_L_P_5_1', 'Tito_L_P_9_3',
+                        'Tito_L_P_6_3', 'Tito_L_P_7_4', 'Tito_L_P_5_0', 'Tito_L_P_5_4', 'Tito_L_P_9_6',
+                        'Tito_L_P_0_4', 'Tito_L_P_4_6', 'Tito_L_P_5_6', 'Tito_L_P_7_6', 'Tito_L_P_9_8',
+                        'Tito_L_P_4_1', 'Tito_L_P_0_5', 'Tito_L_P_9_9', 'Tito_L_P_3_0', 'Tito_L_P_0_3',
+                        'Tito_L_P_6_6', 'Tito_L_P_5_7', 'Tito_L_P_1_1', 'Tito_L_P_3_8', 'Tito_L_P_1_6',
+                        'Tito_L_P_3_5', 'Tito_L_P_6_8', 'Tito_L_P_2_8', 'Tito_L_P_9_7', 'Tito_L_P_6_7',
+                        'Tito_L_P_1_0', 'Tito_L_P_4_5', 'Tito_L_P_4_9', 'Tito_L_P_7_8', 'Tito_L_P_4_7',
+                        'Tito_L_P_4_0', 'Tito_L_P_3_9', 'Tito_L_P_7_7', 'Tito_L_P_4_3', 'Tito_L_P_9_5']
+        good_neuroids = [i for i, neuroid_id in enumerate(assembly['neuroid_id'].values)
+                         if neuroid_id not in err_neuroids]
+        assembly = assembly.isel(neuroid=good_neuroids)
+        assembly = assembly.sel(variation=6)  # TODO: remove variation selection once part of name
+        assembly = assembly.squeeze("time_bin")
+        assembly = assembly.transpose('presentation', 'neuroid')
+        return assembly
+
+
+class GallantDavid2004Loader(AssemblyLoader):
+    def __call__(self):
+        assembly = mkgu.get_assembly(name='gallant.David2004')
+        assembly.load()
+        assembly = assembly.rename({'neuroid': 'neuroid_id'})
+        assembly = assembly.stack(neuroid=('neuroid_id',))
+        assembly = assembly.transpose('presentation', 'neuroid')
+        return assembly
+
+
+_assemblies = {
+    'dicarlo.Majaj2015': DicarloMajaj2015Loader(),
+    'gallant.David2004': GallantDavid2004Loader(),
+}
+
 _benchmarks = {
     'dicarlo.Majaj2015': DicarloMajaj2015,
-    'dicarlo.Majaj2015-rdm': DicarloMajaj2015RDM,
     'gallant.David2004': GallantDavid2004,
     'Felleman1991': FellemanVanEssen1991,
 }
@@ -166,3 +185,10 @@ _benchmarks = {
 def load(name):
     assert name in _benchmarks
     return _benchmarks[name]()
+
+
+def build(assembly_name, metric_name, ceiling_name, target_splits=()):
+    assembly = _assemblies[assembly_name]()
+    metric = metrics[metric_name]
+    ceiling = ceilings[ceiling_name]
+    return SplitBenchmark(assembly, metric, ceiling, target_splits=target_splits)

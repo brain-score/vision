a6c220de9ddd25227a2a74d1bf671d139115db13
Martin Schrimpf <m4rtinsch@gmail.com>
2018-06-27 16:58:17 -0400

    separate SplitCeiling from reps; add SplitNoCeiling

--- tests/test_metrics/test_ceiling.py
+++ tests/test_metrics/test_ceiling.py
@@ -1,5 +1,6 @@
+import numpy as np
 from mkgu.metrics import Score
-from mkgu.metrics.ceiling import NoCeiling
+from mkgu.metrics.ceiling import NoCeiling, SplitNoCeiling
 
 
 class TestNoCeiling:
@@ -7,3 +8,13 @@ class TestNoCeiling:
         ceiling = NoCeiling()
         ceiling = ceiling(None)
         assert isinstance(ceiling, Score)
+
+
+class TestSplitNoCeiling:
+    def test(self):
+        ceiling = SplitNoCeiling()
+        ceiling = ceiling(None)
+        assert isinstance(ceiling, Score)
+        np.testing.assert_array_equal(ceiling.values, [1] * 10)
+        assert ceiling.aggregation.sel(aggregation='center') == 1
+        assert ceiling.aggregation.sel(aggregation='error') == 0

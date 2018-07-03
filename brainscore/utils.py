81b91fbd001552def4fcc599eb90ec024ea8a506
Martin Schrimpf <m4rtinsch@gmail.com>
2018-07-02 21:46:13 -0400

    allow passing all transformation kwargs in SplitBenchmark.__call__

--- brainscore/utils.py
+++ brainscore/utils.py
@@ -1,3 +1,6 @@
+import copy
+
+
 def fullname(obj):
     return obj.__module__ + "." + obj.__class__.__name__
 
@@ -21,3 +24,17 @@ def combine_fields(objs, func):
         lambda field_name_values: (field_name_values[0], func(field_name_values[1])), field_values.items()))
     ctr = objs[0].__class__
     return ctr(**field_values)
+
+
+def recursive_dict_merge(dict1, dict2):
+    """
+    Merges dictionaries (of dictionaries).
+    Preference is given to the second dict, i.e. if a key occurs in both dicts, the value from `dict2` is used.
+    """
+    result = copy.deepcopy(dict1)
+    for key in dict2:
+        if key in dict1 and isinstance(dict1[key], dict) and isinstance(dict2[key], dict):
+            result[key] = recursive_dict_merge(dict1[key], dict2[key])
+        else:
+            result[key] = dict2[key]
+    return result
--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -4,6 +4,8 @@ import numpy as np
 from pytest import approx
 
 from brainscore import benchmarks
+from brainscore.benchmarks import SplitBenchmark, metrics
+from brainscore.metrics.ceiling import SplitNoCeiling
 
 
 class TestAnatomyFelleman:
@@ -32,3 +34,16 @@ class TestMajaj2015:
         score, unceiled_score = benchmark(source, return_unceiled=True)
         assert all(score.aggregation.sel(aggregation='error') == unceiled_score.aggregation.sel(aggregation='error'))
         np.testing.assert_array_almost_equal(score.aggregation.sel(aggregation='center'), [1., 1.], decimal=2)
+
+    def test_construct_kwargs(self):
+        assembly = benchmarks.load_assembly('dicarlo.Majaj2015')
+        assembly = assembly.rename({'presentation': 'stimulus'})
+        metric = metrics['rdm']()
+        dimensions = ('stimulus', 'neuroid')
+        benchmark = SplitBenchmark(target_assembly=assembly, metric=metric, ceiling=SplitNoCeiling(),
+                                   target_splits=['region'], target_splits_kwargs=dict(non_dividing_dims=dimensions))
+        score = benchmark(assembly, transformation_kwargs=dict(
+            alignment_kwargs=dict(order_dimensions=dimensions),
+            cartesian_product_kwargs=dict(non_dividing_dims=dimensions)))
+        np.testing.assert_array_equal(score.aggregation.sel(aggregation='center').dims, ['region'])
+        np.testing.assert_array_equal(score.values.dims, ['region', 'split'])

b3416d809f6bae3dd2d66d90470d7cfa0bcaece8
Martin Schrimpf <m4rtinsch@gmail.com>
2018-10-02 12:00:07 -0400

    implement multi-dimensional grouping for non-unique coords

--- brainscore/assemblies.py
+++ brainscore/assemblies.py
@@ -41,37 +41,59 @@ class DataAssembly(DataArray):
         return GroupbyBridge(result, self, dim, group_coord_names, delimiter, multi_group_name)
 
     def multi_dim_groupby(self, groups, apply):
+        # align
+        groups = sorted(groups, key=lambda group: self.dims.index(self[group].dims[0]))
         # build indices
         groups = {group: np.unique(self[group]) for group in groups}
         group_dims = {self[group].dims: group for group in groups}
         indices = defaultdict(lambda: defaultdict(list))
+        result_indices = defaultdict(dict)
         for group in groups:
             for index, value in enumerate(self[group].values):
                 indices[group][value].append(index)
+                if value not in result_indices[group]:  # if captured once, it will be "grouped away"
+                    index = max(result_indices[group].values()) + 1 if len(result_indices[group]) > 0 else 0
+                    result_indices[group][value] = index
 
         coords = {coord: (dims, value) for coord, dims, value in walk_coords(self)}
 
         def simplify(value):
             return value.item() if value.size == 1 else value
 
+        def indexify(dict_indices):
+            return [(i,) if isinstance(i, int) else tuple(i) for i in dict_indices.values()]
+
         # group and apply
         # making this a DataArray right away and then inserting through .loc would slow things down
-        result = np.zeros([len(group) for group in groups.values()])
+        result = np.zeros([len(indices) for indices in result_indices.values()])
+        result_coords = {coord: (dims, [None] * len(result_indices[group_dims[dims]]))
+                         for coord, (dims, value) in coords.items()}
         for values in itertools.product(*groups.values()):
             group_values = dict(zip(groups.keys(), values))
-            group_indices = {group: indices[group][value] for group, value in group_values.items()}
-            idx = [tuple(i) for i in group_indices.values()]
-            cells = self.values[idx]  # using DataArray here would slow things down. thus we pass coords as kwargs
+            self_indices = {group: indices[group][value] for group, value in group_values.items()}
+            values_indices = indexify(self_indices)
+            cells = self.values[values_indices]  # using DataArray would slow things down. thus we pass coords as kwargs
             cells = simplify(cells)
-            cell_coords = {coord: value[group_indices[group_dims[dims]]]
-                           for coord, (dims, value) in coords.items()}  # note that we are ignoring dims here
-            cell_coords = {coord: simplify(value) for coord, value in cell_coords.items()}
-            merge = apply(cells, **cell_coords)
-            result[idx] = merge
+            cell_coords = {coord: (dims, value[self_indices[group_dims[dims]]])
+                           for coord, (dims, value) in coords.items()}
+            cell_coords = {coord: (dims, simplify(np.unique(value))) for coord, (dims, value) in cell_coords.items()}
+
+            # ignore dims when passing to function
+            passed_coords = {coord: value for coord, (dims, value) in cell_coords.items()}
+            merge = apply(cells, **passed_coords)
+            result_idx = {group: result_indices[group][value] for group, value in group_values.items()}
+            result[indexify(result_idx)] = merge
+            for coord, (dims, value) in cell_coords.items():
+                if isinstance(value, np.ndarray):  # multiple values for coord -> ignore
+                    if coord in result_coords:  # delete from result coords if not yet deleted
+                        del result_coords[coord]
+                    continue
+                assert dims == result_coords[coord][0]
+                coord_index = result_idx[group_dims[dims]]
+                result_coords[coord][1][coord_index] = value
 
         # re-package
-        coords = {coord: (dims, value) for coord, (dims, value) in coords.items() if dims in group_dims}
-        result = type(self)(result, coords=coords, dims=list(itertools.chain(*group_dims.keys())))
+        result = type(self)(result, coords=result_coords, dims=list(itertools.chain(*group_dims.keys())))
         return result
 
     def _join_group_coords(self, dim, group_coord_names, delimiter, multi_group_name):

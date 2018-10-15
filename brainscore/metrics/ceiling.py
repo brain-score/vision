ace861fd7d83cc6f092c47584e399192a788ddf9
Martin Schrimpf <m4rtinsch@gmail.com>
2018-10-14 23:26:47 -0400

    get rid of sub-classing in ceiling; remove SplitRepMetricCeiling

--- brainscore/metrics/ceiling.py
+++ brainscore/metrics/ceiling.py
@@ -1,13 +1,10 @@
-import functools
-import logging
-
 import scipy.stats
 
-from brainscore.assemblies import merge_data_arrays, DataAssembly
-from brainscore.metrics import ParametricMetric, build_score
-from brainscore.metrics.transformations import CrossValidation, subset, Transformations
+from brainscore.assemblies import DataAssembly, walk_coords
+from brainscore.metrics import build_score
+from brainscore.metrics.transformations import Transformations, CrossValidationSingle
+from brainscore.metrics.xarray_utils import Defaults as XarrayDefaults
 from brainscore.metrics.xarray_utils import XarrayCorrelation
-from brainscore.utils import fullname
 
 
 class Ceiling(object):
@@ -17,116 +14,64 @@ class Ceiling(object):
 
 class NoCeiling(Ceiling):
     def __call__(self, assembly):
-        return build_score(DataAssembly(1), center=DataAssembly(1), error=DataAssembly(0))
+        return build_score(None, center=DataAssembly(1), error=DataAssembly(0))
 
 
-class SplitCeiling(Ceiling):
-    def __init__(self, n_splits=CrossValidation.Defaults.splits):
-        self._n_splits = n_splits
-        self._logger = logging.getLogger(fullname(self))
+class InternalConsistency(Ceiling):
+    """
+    Computes the consistency within an assembly with repetitions.
+    """
 
-    def __call__(self, assembly):
-        split_scores = []
-        # the following for loop is very similar to CrossValidation#__call__
-        # but it didn't seem straight-forward how to merge the two.
-        for split_iterator in range(self._n_splits):
-            self._logger.debug("split {}/{}".format(split_iterator + 1, self._n_splits))
-            split_score = self._compute_split(assembly)
-            # package
-            split_score = split_score.expand_dims('split')
-            split_score['split'] = [split_iterator]
-            split_scores.append(split_score)
-        split_scores = merge_data_arrays(split_scores)
-        # aggregate
-        center, error = self.aggregate(split_scores)
-        return build_score(split_scores, center, error)
+    class Defaults:
+        split_coord = 'repetition'
 
-    def _compute_split(self, assembly):
-        raise NotImplementedError()
-
-    def aggregate(self, scores):
-        raise NotImplementedError()
-
-
-class SplitNoCeiling(SplitCeiling):
-    def _compute_split(self, assembly):
-        return DataAssembly(1)
-
-    def aggregate(self, scores):
-        return DataAssembly(1), DataAssembly(0)
-
-
-class SplitRepCeiling(SplitCeiling):
-    def __init__(self, average_repetition, n_splits=CrossValidation.Defaults.splits,
-                 repetition_train_size=0.5, repetition_test_size=0.5, repetition_dim='repetition',
-                 stratification_coord=CrossValidation.Defaults.stratification_coord):
-        super(SplitRepCeiling, self).__init__(n_splits=n_splits)
-        self._average_repetition = average_repetition
-        self._repetition_test_size = 1 / repetition_test_size
-        self._rep_split = CrossValidation(train_size=repetition_train_size, test_size=repetition_test_size,
-                                          dim=repetition_dim, stratification_coord=stratification_coord,
-                                          splits=n_splits)
-        self._traintest_split = CrossValidation(splits=n_splits)
-        self._logger = logging.getLogger(fullname(self))
-        self._split_generator = None
-        self._repetition_values, self._traintest_values = None, None
-
-    def __call__(self, assembly):
-        repetition_values, repetition_splits = self._rep_split.build_splits(assembly)
-        traintest_values, traintest_splits = self._traintest_split.build_splits(assembly)
-        assert len(repetition_splits) == len(traintest_splits)
-        self._repetition_values, self._traintest_values = repetition_values, traintest_values
-        self._split_generator = zip(repetition_splits, traintest_splits)
-        result = super(SplitRepCeiling, self).__call__(assembly)
-        self._split_generator = None  # reset
-        self._repetition_values, self._traintest_values = None, None
-        return result
-
-    def _compute_split(self, assembly):
-        ((half1_indices, half2_indices), (train_indices, test_indices)) = next(self._split_generator)
-        half1_values, half2_values = self._repetition_values[half1_indices], self._repetition_values[half2_indices]
-        train_values, test_values = self._traintest_values[train_indices], self._traintest_values[test_indices]
-        _subset = functools.partial(subset, dims_must_match=False)
-        half1, half2 = _subset(assembly, half1_values), _subset(assembly, half2_values)
-        half1, half2 = self._average_repetition(half1), self._average_repetition(half2)
-        train_half1, test_half1 = _subset(half1, train_values), _subset(half1, test_values)
-        train_half2, test_half2 = _subset(half2, train_values), _subset(half2, test_values)
-
-        # compute
-        split_score = self.score(train_half1, train_half2, test_half1, test_half2)
-        split_score = spearman_brown_correction(split_score, n=self._repetition_test_size)
-        return split_score
-
-    def score(self, train_half1, train_half2, test_half1, test_half2):
-        raise NotImplementedError()
-
-    def aggregate(self, scores):
-        return self._rep_split.aggregate(scores, scores)
-
-
-class SplitRepMetricCeiling(SplitRepCeiling):
-    def __init__(self, metric, *args, **kwargs):
-        self._metric = metric
-        super().__init__(*args, **kwargs)
-
-    def score(self, train_half1, train_half2, test_half1, test_half2):
-        return self._metric(train_half1, train_half2, test_half1, test_half2)
-
-    def aggregate(self, scores):
-        scores = self._metric.aggregate(scores)
-        return super().aggregate(scores)
-
-
-class InternalConsistency:
-    def __init__(self, stimulus_coord, neuroid_dim, neuroid_coord):
-        correlation = scipy.stats.pearsonr
-        self._correlation = XarrayCorrelation(correlation, stimulus_coord=stimulus_coord, neuroid_coord=neuroid_coord)
-        self._neuroid_dim = neuroid_dim
-        cross_validation = CrossValidation()
+    def __init__(self, split_coord=Defaults.split_coord, stimulus_coord=XarrayDefaults.stimulus_coord,
+                 neuroid_dim=XarrayDefaults.neuroid_dim, neuroid_coord=XarrayDefaults.neuroid_coord):
+        consistency = SplitHalfConsistency(stimulus_coord=stimulus_coord, neuroid_dim=neuroid_dim,
+                                           neuroid_coord=neuroid_coord)
+        self._consistency = self.SplitHalfWrapper(split_coord=split_coord, consistency=consistency)
+        cross_validation = CrossValidationSingle(train_size=0.5, split_coord=split_coord)
         self._transformations = Transformations([cross_validation])
 
     def __call__(self, assembly):
-        return self._transformations(assembly, metric=self._correlation)
+        return self._transformations(assembly, metric=self._consistency)
+
+    class SplitHalfWrapper:
+        def __init__(self, split_coord, consistency):
+            self._split_coord = split_coord
+            self._consistency = consistency
+
+        def __call__(self, half1, half2):
+            half1, half2 = self._average_repetitions(half1), self._average_repetitions(half2)
+            return self._consistency(half1, half2)
+
+        def _average_repetitions(self, assembly):
+            repetition_dims = assembly[self._split_coord].dims
+            nonrepetition_coords = [coord for coord, dims, values in walk_coords(assembly)
+                                    if dims == repetition_dims and coord != self._split_coord]
+            average = assembly.multi_groupby(nonrepetition_coords).mean(dim=repetition_dims)
+            return average
+
+        def aggregate(self, scores):
+            return self._consistency.aggregate(scores)
+
+
+class SplitHalfConsistency:
+    """
+    Computes the consistency between two halves of an assembly.
+    """
+
+    def __init__(self, stimulus_coord=XarrayDefaults.stimulus_coord,
+                 neuroid_dim=XarrayDefaults.neuroid_dim, neuroid_coord=XarrayDefaults.neuroid_coord):
+        correlation = scipy.stats.pearsonr
+        self._correlation = XarrayCorrelation(correlation, stimulus_coord=stimulus_coord, neuroid_coord=neuroid_coord)
+        self._neuroid_dim = neuroid_dim
+
+    def __call__(self, half1, half2):
+        return self._correlation(half1, half2)
+
+    def aggregate(self, scores):
+        return scores.median(dim=self._neuroid_dim)
 
 
 def spearman_brown_correction(correlation, n):
@@ -134,7 +79,6 @@ def spearman_brown_correction(correlation, n):
 
 
 ceilings = {
-    'splitrep': SplitRepMetricCeiling,
     'cons': InternalConsistency,
     None: NoCeiling,
 }
--- tests/test_metrics/test_ceiling.py
+++ tests/test_metrics/test_ceiling.py
@@ -1,6 +1,10 @@
+from string import ascii_lowercase as alphabet
+
 import numpy as np
+
+from brainscore.assemblies import NeuroidAssembly, DataAssembly
 from brainscore.metrics import Score
-from brainscore.metrics.ceiling import NoCeiling, SplitNoCeiling
+from brainscore.metrics.ceiling import NoCeiling, InternalConsistency, SplitHalfConsistency
 
 
 class TestNoCeiling:
@@ -8,13 +12,35 @@ class TestNoCeiling:
         ceiling = NoCeiling()
         ceiling = ceiling(None)
         assert isinstance(ceiling, Score)
-
-
-class TestSplitNoCeiling:
-    def test(self):
-        ceiling = SplitNoCeiling()
-        ceiling = ceiling(None)
-        assert isinstance(ceiling, Score)
-        np.testing.assert_array_equal(ceiling.values, [1] * 10)
         assert ceiling.aggregation.sel(aggregation='center') == 1
-        assert ceiling.aggregation.sel(aggregation='error') == 0
+
+
+class TestInternalConsistency:
+    def test(self):
+        data = NeuroidAssembly(np.tile(np.arange(10)[:, np.newaxis], [5, 10]),
+                               coords={'image_id': ('presentation', np.tile(list(alphabet)[:10], 5)),
+                                       'image_meta': ('presentation', np.tile(list(alphabet)[:10], 5)),
+                                       'repetition': ('presentation', np.repeat(np.arange(5), 10)),
+                                       'neuroid_id': ('neuroid', np.arange(10)),
+                                       'neuroid_meta': ('neuroid', np.arange(10))},
+                               dims=['presentation', 'neuroid'])
+        ceiler = InternalConsistency()
+        ceiling = ceiler(data)
+        assert ceiling.aggregation.sel(aggregation='center') == 1
+
+
+class TestSplitHalfConsistency:
+    def test(self):
+        data = NeuroidAssembly(np.tile(np.arange(10)[:, np.newaxis], [5, 10]),
+                               coords={'image_id': ('presentation', np.tile(list(alphabet)[:10], 5)),
+                                       'image_meta': ('presentation', np.tile(list(alphabet)[:10], 5)),
+                                       'repetition': ('presentation', np.tile(np.arange(5), 10)),
+                                       'neuroid_id': ('neuroid', np.arange(10)),
+                                       'neuroid_meta': ('neuroid', np.arange(10))},
+                               dims=['presentation', 'neuroid'])
+        ceiler = SplitHalfConsistency()
+        ceiling = ceiler(data, data)
+        assert all(ceiling == DataAssembly(np.ones(10),
+                                           coords={'neuroid_id': ('neuroid', np.arange(10)),
+                                                   'neuroid_meta': ('neuroid', np.arange(10))},
+                                           dims=['neuroid']))

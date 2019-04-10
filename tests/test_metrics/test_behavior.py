3ac17a8533c83b9b94af2a9f9d6c5e3a3dc4543f
Martin Schrimpf <m4rtinsch@gmail.com>
2019-04-09 22:33:06 -0400

    use pre-packaged assemblies

--- tests/test_metrics/test_behavior.py
+++ tests/test_metrics/test_behavior.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 
 import numpy as np
 import pandas as pd
@@ -19,8 +20,7 @@ class TestI2N:
                              ])
     def test_model(self, model, expected_score):
         # assembly
-        _objectome = self.get_objectome()
-        fitting_objectome, testing_objectome = _objectome[~ _objectome['use']], _objectome[_objectome['use']]
+        fitting_objectome, testing_objectome = self.get_objectome('partial_trials'), self.get_objectome('full_trials')
 
         # features
         feature_responses = pd.read_pickle(os.path.join(os.path.dirname(__file__),
@@ -29,49 +29,30 @@ class TestI2N:
         feature_responses['image_id'] = 'stimulus_path', [os.path.splitext(os.path.basename(path))[0]
                                                           for path in feature_responses['stimulus_path'].values]
         feature_responses = feature_responses.stack(presentation=['stimulus_path'])
-        expected_images = set(fitting_objectome['id'].values) | set(testing_objectome['id'].values)
+        expected_images = set(fitting_objectome['image_id'].values) | set(testing_objectome['image_id'].values)
         assert expected_images.issuperset(set(feature_responses['image_id'].values))
         assert len(np.unique(feature_responses['layer'])) == 1  # only penultimate layer
         feature_responses = feature_responses.transpose('presentation', 'neuroid')
         fitting_features = self.separate_and_annotate_features(feature_responses, fitting_objectome)
         testing_features = self.separate_and_annotate_features(feature_responses, testing_objectome)
         # metric
-        testing_objectome = self.to_xarray(testing_objectome)
         i2n = I2n()
         score = i2n(fitting_features, testing_features, testing_objectome)
         score = score.sel(aggregation='center')
         assert score == approx(expected_score, abs=0.005), f"expected {expected_score}, but got {score}"
 
-    def get_objectome(self):
-        packaged_filepath = os.path.join(os.path.dirname(__file__), 'objectome240.pkl')
-        if not os.path.isfile(packaged_filepath):
-            # repackage subsampled
-            objectome = pd.read_pickle(os.path.join(os.path.dirname(__file__), 'objectome24s100_humanpool.pkl'))
-            subsample = pd.read_pickle(
-                os.path.join(os.path.dirname(__file__), 'objectome24s100_imgsubsampled240_pandas.pkl'))
-            objectome['use'] = objectome['id'].isin(subsample.values[:, 0])
-            objectome.to_pickle(packaged_filepath)
-        else:
-            objectome = pd.read_pickle(packaged_filepath)
-        objectome['correct'] = objectome['choice'] == objectome['sample_obj']
-        objectome['label'] = objectome['sample_obj']
+    def get_objectome(self, subtype):
+        with open(f'/braintree/home/msch/brainio_contrib/mkgu_packaging/dicarlo/dicarlo.Rajalingham2018.{subtype}.pkl',
+                  'rb') as f:
+            objectome = pickle.load(f)
         return objectome
 
     def separate_and_annotate_features(self, feature_responses, objectome):
-        id_rows = {row['id']: row for _, row in objectome[['id', 'label']].drop_duplicates().iterrows()}
-        image_ids = xr.DataArray(np.zeros(len(id_rows)), coords={'image_id': list(id_rows.keys())},
+        image_labels = {image_id: label for image_id, label in
+                        zip(objectome['image_id'].values, objectome['truth'].values)}
+        image_ids = xr.DataArray(np.zeros(len(image_labels)), coords={'image_id': list(image_labels.keys())},
                                  dims=['image_id']).stack(presentation=['image_id'])
         feature_responses = subset(feature_responses, image_ids)
-        labels = [id_rows[image_id]['label'] for image_id in feature_responses['image_id'].values]
+        labels = [image_labels[image_id] for image_id in feature_responses['image_id'].values]
         feature_responses['label'] = 'presentation', labels
         return feature_responses
-
-    def to_xarray(self, objectome):
-        columns = objectome.columns
-        objectome = xr.DataArray(objectome['choice'],
-                                 coords={column: ('presentation', objectome[column]) for column in columns},
-                                 dims=['presentation'])
-        objectome = objectome.rename({'id': 'image_id'})
-        objectome['truth'] = objectome['label']
-        objectome = objectome.set_index(presentation=[col if col != 'id' else 'image_id' for col in columns])
-        return objectome

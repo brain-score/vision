16893a1488e3f3c525ff37ed058d58577e17ee41
Martin Schrimpf <m4rtinsch@gmail.com>
2019-07-19 08:49:49 -0400

    add optional meta-information to benchmarks

--- brainscore/benchmarks/imagenet.py
+++ brainscore/benchmarks/imagenet.py
@@ -1,26 +1,27 @@
 import os
 
+import numpy as np
 import pandas as pd
 
 from brainio_base.stimuli import StimulusSet
-from brainscore.benchmarks import Benchmark
+from brainscore.benchmarks import BenchmarkBase
+from brainscore.metrics import Score
 from brainscore.metrics.accuracy import Accuracy
 from brainscore.model_interface import BrainModel
 
 
-class Imagenet2012(Benchmark):
+class Imagenet2012(BenchmarkBase):
     def __init__(self):
         stimulus_set = pd.read_csv(os.path.join(os.path.dirname(__file__), 'imagenet2012.csv'))
         stimulus_set = StimulusSet(stimulus_set)
         stimulus_set.image_paths = {row.image_id: row.filepath for row in stimulus_set.itertuples()}
         self._stimulus_set = stimulus_set
-
-        self._name = 'imagenet2012'
         self._similarity_metric = Accuracy()
-
-    @property
-    def name(self):
-        return self._name
+        ceiling = Score([1, np.nan], coords={'aggregation': ['center', 'error']}, dims=['aggregation'])
+        super(Imagenet2012, self).__init__(identifier='fei-fei.Deng2009-top1',
+                                           ceiling_func=lambda: ceiling,
+                                           parent='ImageNet',
+                                           paper_link="https://ieeexplore.ieee.org/abstract/document/5206848")
 
     def __call__(self, candidate):
         # the proper `fitting_stimuli` to pass to the candidate would be the imagenet training set.
@@ -30,7 +31,3 @@ class Imagenet2012(Benchmark):
         predictions = candidate.look_at(self._stimulus_set[list(set(self._stimulus_set.columns) - {'synset'})])
         score = self._similarity_metric(predictions, self._stimulus_set['synset'].values)
         return score
-
-    @property
-    def ceiling(self):
-        return None

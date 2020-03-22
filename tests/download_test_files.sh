c44ffcdea73edaf40cb5a87662c8691e667f205b
Martin Schrimpf <m4rtinsch@gmail.com>
2020-03-22 15:27:42 -0400

    fix test file download, add echo statement

--- tests/download_test_files.sh
+++ tests/download_test_files.sh
@@ -3,6 +3,8 @@
 # get directory of this script (i.e. tests), following https://stackoverflow.com/a/246128/2225200
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-aws --no-sign-request s3 cp s3://brain-score-tests/tests/test_benchmarks/alexnet-freemanziemba2013.aperture-private.pkl $(SCRIPT_DIR)/test_benchmarks/
-aws --no-sign-request s3 cp s3://brain-score-tests/tests/test_benchmarks/alexnet-majaj2015.private-features.12.pkl $(SCRIPT_DIR)/test_benchmarks/
-aws --no-sign-request s3 cp s3://brain-score-tests/tests/test_benchmarks/cornet_s-kar2019.pkl $(SCRIPT_DIR)/test_benchmarks/
+for f in alexnet-freemanziemba2013.aperture-private.pkl alexnet-majaj2015.private-features.12.pkl
+do
+  echo "Downloading ${f}"
+  aws --no-sign-request s3 cp s3://brain-score-tests/tests/test_benchmarks/${f} ${SCRIPT_DIR}/test_benchmarks/
+done

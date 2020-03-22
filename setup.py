740fe1718a6330957ac3bd51af4f8462f020bd0c
Martin Schrimpf <m4rtinsch@gmail.com>
2020-03-22 16:20:44 -0400

    fix previous commit: need at least 1.16

--- setup.py
+++ setup.py
@@ -7,10 +7,9 @@ with open('README.md') as readme_file:
     readme = readme_file.read()
 
 requirements = [
-    "numpy>1.15",
+    "numpy>=1.16",
     "brainio_base @ git+https://github.com/brain-score/brainio_base",
     "brainio_collection @ git+https://github.com/brain-score/brainio_collection",
-    # "tensorflow-gpu",  # for using mask-regression
     "scikit-learn",
     "h5py",
     "tqdm",

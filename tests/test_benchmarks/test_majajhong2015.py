cf8d94531890eb3c27fc0341ab6e21bd0d4e269c
Martin Schrimpf <m4rtinsch@gmail.com>
2020-07-30 14:33:27 -0400

    update unit tests for `stimulus_set_identifier`, round 2

--- tests/test_benchmarks/test_majajhong2015.py
+++ tests/test_benchmarks/test_majajhong2015.py
@@ -9,7 +9,7 @@ class TestAssembly:
     def test_majajhong2015_V4(self):
         assembly = load_assembly(average_repetitions=True, region='V4')
         check_standard_format(assembly)
-        assert assembly.attrs['stimulus_set_name'] == 'dicarlo.hvm-private'
+        assert assembly.attrs['stimulus_set_identifier'] == 'dicarlo.hvm-private'
         assert set(assembly['region'].values) == {'V4'}
         assert len(assembly['presentation']) == 2560
         assert len(assembly['neuroid']) == 88
@@ -17,7 +17,7 @@ class TestAssembly:
     def test_majajhong2015_IT(self):
         assembly = load_assembly(average_repetitions=True, region='IT')
         check_standard_format(assembly)
-        assert assembly.attrs['stimulus_set_name'] == 'dicarlo.hvm-private'
+        assert assembly.attrs['stimulus_set_identifier'] == 'dicarlo.hvm-private'
         assert set(assembly['region'].values) == {'IT'}
         assert len(assembly['presentation']) == 2560
         assert len(assembly['neuroid']) == 168

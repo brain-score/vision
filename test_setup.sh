7f210a153e00ac2ee1fe20d104f9d3d78622f267
franzigeiger <franzigeiger94@gmail.com>
2020-08-05 15:27:38 +0200

    Fix long failing tests due to missing files

--- test_setup.sh
+++ test_setup.sh
@@ -5,5 +5,5 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 for f in alexnet-freemanziemba2013.aperture-private.pkl alexnet-majaj2015.private-features.12.pkl CORnetZ-rajalingham2018public.pkl cornet_s-kar2019.pkl
 do
-  aws --no-sign-request s3 cp s3://brain-score-tests/tests/test_benchmarks/${f} ${SCRIPT_DIR}/test_benchmarks/
+  aws --no-sign-request s3 cp s3://brain-score-tests/tests/test_benchmarks/${f} ${SCRIPT_DIR}/tests/test_benchmarks/
 done

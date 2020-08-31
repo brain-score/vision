0ae94481c665bbbb53da1541e68cc712e46a5f84
franzigeiger <franzigeiger94@gmail.com>
2020-08-31 15:28:53 +0200

    Temporary change samples

--- brainscore/benchmarks/majajhong2015.py
+++ brainscore/benchmarks/majajhong2015.py
@@ -29,7 +29,7 @@ def _DicarloMajajHong2015Region(region, identifier_metric_suffix, similarity_met
     assembly = LazyLoad(lambda region=region: load_assembly(average_repetitions=True, region=region))
     return NeuralBenchmark(identifier=f'dicarlo.MajajHong2015.{region}-{identifier_metric_suffix}', version=3,
                            assembly=assembly, similarity_metric=similarity_metric,
-                           visual_degrees=VISUAL_DEGREES, number_of_trials=NUMBER_OF_TRIALS,
+                           visual_degrees=VISUAL_DEGREES, number_of_trials=2,
                            ceiling_func=lambda: ceiler(assembly_repetition),
                            parent=region,
                            bibtex=BIBTEX)

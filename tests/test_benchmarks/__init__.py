064e4828855dda581121c6389819d694419f58b6
Martin Schrimpf <m4rtinsch@gmail.com>
2020-09-23 08:34:09 -0400

    update PrecomputedFeatures' look_at to include number_of_trials

--- tests/test_benchmarks/__init__.py
+++ tests/test_benchmarks/__init__.py
@@ -18,7 +18,7 @@ class PrecomputedFeatures(BrainModel):
     def start_recording(self, region, *args, **kwargs):
         pass
 
-    def look_at(self, stimuli):
+    def look_at(self, stimuli, number_of_trials):
         assert set(self.features['image_id'].values) == set(stimuli['image_id'].values)
         features = self.features.isel(presentation=[np.where(self.features['image_id'].values == image_id)[0][0]
                                                     for image_id in stimuli['image_id'].values])

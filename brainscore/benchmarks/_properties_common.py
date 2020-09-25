Corrected naming of variables according to convention Updated stimulus and assembly information according to updates to brainio_contrib Removed commented code related to repeat_trials and average_trials Moved functions to calculate individual properties to benchmarks._properties_common Updated MLBrainPool to allow using StochasticModelCommitment (from candidate_models)

brainscore/benchmarks/_properties_common.py
@@ -5,11 +5,10 @@ from brainscore.benchmarks import BenchmarkBase, ceil_score
 from brainscore.model_interface import BrainModel
 from brainscore.benchmarks.screen import place_on_screen
 from brainio_collection.fetch import get_stimulus_set
-# from brainscore.benchmarks.trials import repeat_trials, average_trials
 from brainio_base.assemblies import DataAssembly
 
 BLANK_STIM_NAME = 'dicarlo.Marques2020_blank'
-RF_STIM_NAME = 'dicarlo.Marques2020_rf'
+RF_STIM_NAME = 'dicarlo.Marques2020_receptive_field'
 ORIENTATION_STIM_NAME = 'dicarlo.Marques2020_orientation'
 
 RF_NUMBER_OF_TRIALS = 10
@@ -68,19 +67,17 @@ def get_firing_rates(model_identifier, model, region, stimulus_identifier, numbe
 def get_activations(model: BrainModel, stimulus_identifier, number_of_trials):
     stimulus_set = get_stimulus_set(stimulus_identifier)
     stimulus_set = place_on_screen(stimulus_set, target_visual_degrees=model.visual_degrees())
-    # stimulus_set = repeat_trials(stimulus_set, number_of_trials=number_of_trials) # coment
-    activations = model.look_at(stimulus_set, number_of_trials) #, number_of_trials)
+    activations = model.look_at(stimulus_set, number_of_trials)
     if 'time_bin' in activations.dims:
         activations = activations.squeeze('time_bin')  # static case for these benchmarks
-    # activations = average_trials(activations)  # coment
     return activations
 
 
-def get_stim_pos(activations):
-    pos_y = np.array(sorted(set(activations.pos_y.values)))
-    pos_x = np.array(sorted(set(activations.pos_x.values)))
-    assert len(pos_x) == 1 and len(pos_y) == 1
-    return np.array([pos_y[0], pos_x[0]])
+def get_stim_pos(stimulus_set):
+    position_y = np.array(sorted(set(stimulus_set.position_y.values)))
+    position_x = np.array(sorted(set(stimulus_set.position_x.values)))
+    assert len(position_x) == 1 and len(position_y) == 1
+    return np.array([position_y[0], position_x[0]])
 
 
 def filter_receptive_fields(model_identifier, model, region, pos, rf_delta=RF_DELTA):
@@ -101,14 +98,14 @@ def map_receptive_field_locations(model_identifier, model: BrainModel, region):
 
     _assert_grating_activations(rf_activations)
 
-    pos_y = np.array(sorted(set(rf_activations.pos_y.values)))
-    pos_x = np.array(sorted(set(rf_activations.pos_x.values)))
+    position_y = np.array(sorted(set(rf_activations.position_y.values)))
+    position_x = np.array(sorted(set(rf_activations.position_x.values)))
     n_neuroids = rf_activations.values.shape[0]
     neuroid_ids = rf_activations.neuroid.values
     rf_activations = rf_activations.values
     rf_activations[rf_activations < 0] = 0
 
-    rf_activations = rf_activations.reshape(n_neuroids, len(pos_y), len(pos_x), -1)
+    rf_activations = rf_activations.reshape(n_neuroids, len(position_y), len(position_x), -1)
     rf_activations = rf_activations - np.reshape(blank_activations, [n_neuroids] +
                                                  [1] * (len(rf_activations.shape) - 1))
 
@@ -130,12 +127,12 @@ def map_receptive_field_locations(model_identifier, model: BrainModel, region):
                 np.argwhere(exc_pos) * np.repeat(np.expand_dims(rf_map[n, exc_pos], axis=1), 2, axis=1),
                 axis=0) / np.sum(np.repeat(np.expand_dims(rf_map[n, exc_pos], axis=1), 2, axis=1), axis=0)
             # interpolates pos of rf centroid
-            rf_pos[n, 0] = np.interp(rf_coord[0], np.arange(len(pos_y)), pos_y)
-            rf_pos[n, 1] = np.interp(rf_coord[1], np.arange(len(pos_x)), pos_x)
+            rf_pos[n, 0] = np.interp(rf_coord[0], np.arange(len(position_y)), position_y)
+            rf_pos[n, 1] = np.interp(rf_coord[1], np.arange(len(position_x)), position_x)
 
     rf_pos = DataAssembly(rf_pos, coords={'neuroid': neuroid_ids, 'axis': ['y', 'x']}, dims=['neuroid', 'axis'])
-    rf_map = DataAssembly(rf_map, coords={'neuroid': neuroid_ids, 'pos_y': pos_y, 'pos_x': pos_x},
-                          dims=['neuroid', 'pos_y', 'pos_x'])
+    rf_map = DataAssembly(rf_map, coords={'neuroid': neuroid_ids, 'position_y': position_y, 'position_x': position_x},
+                          dims=['neuroid', 'position_y', 'position_x'])
 
     return rf_pos, rf_map
 
@@ -188,26 +185,77 @@ def firing_rates_affine(model_identifier, model: BrainModel, region):
 
 
 def _assert_grating_activations(activations):
-    pos_y = np.array(sorted(set(activations.pos_y.values)))
-    pos_x = np.array(sorted(set(activations.pos_x.values)))
+    position_y = np.array(sorted(set(activations.position_y.values)))
+    position_x = np.array(sorted(set(activations.position_x.values)))
     contrast = np.array(sorted(set(activations.contrast.values)))
     radius = np.array(sorted(set(activations.radius.values)))
-    sf = np.array(sorted(set(activations.sf.values)))
+    spatial_frequency = np.array(sorted(set(activations.spatial_frequency.values)))
     orientation = np.array(sorted(set(activations.orientation.values)))
     phase = np.array(sorted(set(activations.phase.values)))
     nStim = activations.values.shape[1]
 
-    assert np.sum(np.tile(phase, len(pos_y) * len(pos_x) * len(contrast) * len(radius) * len(sf) * len(orientation)) ==
-                  activations.phase.values) == nStim
-    assert np.sum(np.tile(np.repeat(orientation, len(phase)), len(pos_y) * len(pos_x) * len(contrast) * len(radius) *
-                          len(sf)) == activations.orientation.values) == nStim
-    assert np.sum(np.tile(np.repeat(sf, len(phase) * len(orientation)), len(pos_y) * len(pos_x) * len(contrast) *
-                          len(radius)) == activations.sf.values) == nStim
-    assert np.sum(np.tile(np.repeat(radius, len(phase) * len(orientation) * len(sf)), len(pos_y) * len(pos_x) *
-                          len(contrast)) == activations.radius.values) == nStim
-    assert np.sum(np.tile(np.repeat(contrast, len(phase) * len(orientation) * len(sf) * len(radius)), len(pos_y) *
-                          len(pos_x)) == activations.contrast.values) == nStim
-    assert np.sum(np.tile(np.repeat(pos_x, len(phase) * len(orientation) * len(sf) * len(radius) * len(contrast)),
-                          len(pos_y)) == activations.pos_x.values) == nStim
-    assert np.sum(np.repeat(pos_y, len(phase) * len(orientation) * len(sf) * len(radius) * len(contrast) * len(pos_x))
-                  == activations.pos_y.values) == nStim
+    assert np.sum(np.tile(phase, len(position_y) * len(position_x) * len(contrast) * len(radius) *
+                          len(spatial_frequency) * len(orientation)) == activations.phase.values) == nStim
+    assert np.sum(np.tile(np.repeat(orientation, len(phase)), len(position_y) * len(position_x) * len(contrast) *
+                          len(radius) * len(spatial_frequency)) == activations.orientation.values) == nStim
+    assert np.sum(np.tile(np.repeat(spatial_frequency, len(phase) * len(orientation)), len(position_y) *
+                          len(position_x) * len(contrast) * len(radius)) == activations.sf.values) == nStim
+    assert np.sum(np.tile(np.repeat(radius, len(phase) * len(orientation) * len(spatial_frequency)), len(position_y) *
+                          len(position_x) * len(contrast)) == activations.radius.values) == nStim
+    assert np.sum(np.tile(np.repeat(contrast, len(phase) * len(orientation) * len(spatial_frequency) * len(radius)),
+                          len(position_y) * len(position_x)) == activations.contrast.values) == nStim
+    assert np.sum(np.tile(np.repeat(position_x, len(phase) * len(orientation) * len(spatial_frequency) * len(radius) *
+                                    len(contrast)), len(position_y)) == activations.position_x.values) == nStim
+    assert np.sum(np.repeat(position_y, len(phase) * len(orientation) * len(spatial_frequency) * len(radius) *
+                            len(contrast) * len(position_x)) == activations.position_y.values) == nStim
+
+
+def calc_circular_variance(orientation_curve, orientation):
+    vect_sum = orientation_curve.dot(np.exp(1j * 2 * orientation / 180 * np.pi))
+    osi = np.absolute(vect_sum) / np.sum(np.absolute(orientation_curve))
+    return 1 - osi
+
+
+def calc_bandwidth(orientation_curve, orientation, filt_type='hanning', thrsh=0.5, mode='full'):
+    from scipy.interpolate import UnivariateSpline
+    or_ext = np.hstack((orientation - 180, orientation, orientation + 180))
+    or_curve_ext = np.tile(orientation_curve, (1, 3))
+
+    if filt_type == 'hanning':
+        w = np.array([0, 2/5, 1, 2/5, 0])
+    elif filt_type == 'flat':
+        w = np.array([1, 1, 1, 1, 1])
+    elif filt_type == 'smooth':
+        w = np.array([0, 1/5, 1, 1/5, 0])
+
+    if filt_type is not None:
+        or_curve_ext = np.convolve(w / w.sum(), np.squeeze(or_curve_ext), mode='same')
+    or_curve_spl = UnivariateSpline(or_ext, or_curve_ext, s=0.)
+
+    or_full = np.linspace(-180, 359, 540)
+    or_curve_full = or_curve_spl(or_full)
+    pref_or_fit = np.argmax(or_curve_full[181:360])
+    or_curve_max = or_curve_full[pref_or_fit + 181]
+
+    try:
+        less = np.where(or_curve_full <= or_curve_max * thrsh)[0][:]
+        p1 = or_full[less[np.where(less < pref_or_fit + 181)[0][-1]]]
+        p2 = or_full[less[np.where(less > pref_or_fit + 181)[0][0]]]
+        bw = (p2 - p1)
+        if bw > 180:
+            bw = np.nan
+    except:
+        bw = np.nan
+    if mode is 'half':
+        bw = bw / 2
+    return bw, pref_or_fit, or_full[181:360], or_curve_full[181:360]
+
+
+def calc_orthogonal_preferred_ratio(orientation_curve, orientation):
+    pref_orientation = np.argmax(orientation_curve)
+    orth_orientation = pref_orientation + int(len(orientation)/2)
+    if orth_orientation >= len(orientation):
+        orth_orientation -= len(orientation)
+    opr = orientation_curve[orth_orientation] / orientation_curve[pref_orientation]
+    return opr
+

brainscore/benchmarks/marques2020_ringach2002.py
@@ -1,25 +1,27 @@
 import brainscore
-from brainscore.benchmarks._properties_common import PropertiesBenchmark
+from brainscore.benchmarks._properties_common import PropertiesBenchmark, _assert_grating_activations
+from brainscore.benchmarks._properties_common import calc_circular_variance, calc_bandwidth, \
+    calc_orthogonal_preferred_ratio
 from brainscore.metrics.ceiling import NeuronalPropertyCeiling
 from brainio_base.assemblies import DataAssembly
-from brainscore.metrics.distribution_similarity import BootstrapDistributionSimilarity, ks_sim
-from brainscore.utils.v1_properties_utils import calc_cv, calc_bw, calc_opr
+from brainscore.metrics.distribution_similarity import BootstrapDistributionSimilarity, ks_similarity
 from result_caching import store
 import numpy as np
 
-ASSEMBLY_NAME = 'dicarlo.Marques2020_Ringach2002'
+ASSEMBLY_NAME = 'shapley.Ringach2002'
 REGION = 'V1'
 PARENT_ORIENTATION = 'V1-orientation'
 PARENT_MAGNITUDE = 'V1-magnitude'
 
-PROPERTY_NAMES = ['baseline', 'maxdc', 'mindc', 'maxac', 'mod_ratio', 'circ_var', 'bandwidth', 'orth_pref_ratio',
-                  'or_sel', 'cv_bw_ratio', 'opr_cv_diff', 'opr_bw_ratio']
+PROPERTY_NAMES = ['baseline', 'max_dc', 'min_dc', 'max_ac', 'modulation_ratio', 'circular_variance', 'bandwidth',
+                  'orthogonal_preferred_ratio', 'orientation_selective', 'circular_variance_bandwidth_ratio',
+                  'orthogonal_preferred_ratio_circular_variance_difference',
+                  'orthogonal_preferred_ratio_bandwidth_ratio']
 
 BIBTEX = """@article{Ringach2002,
             abstract = {We studied the steady-state orientation selectivity of single neurons in macaque primary visual cortex (V1). To analyze the data, two measures of orientation tuning selectivity, circular variance and orientation bandwidth, were computed from the tuning curves. Circular variance is a global measure of the shape of the tuning curve, whereas orientation bandwidth is a local measure of the sharpness of the tuning curve around its peak. Circular variance in V1 was distributed broadly, indicating a great diversity of orientation selectivity. This diversity was also reflected in the individual cortical layers. However, there was a tendency for neurons with high circular variance, meaning low selectivity for orientation, to be concentrated in layers 4C, 3B, and 5. The relative variation of orientation bandwidth across the cortical layers was less than for circular variance, but it showed a similar laminar dependence. Neurons with large orientation bandwidth were found predominantly in layers 4C and 3B. There was a weak correlation between orientation selectivity and the level of spontaneous activity of the neurons. We also assigned a response modulation ratio for each cell, which is a measure of the linearity of spatial summation. Cells with low modulation ratios tended to have higher circular variance and bandwidth than those with high modulation ratios. These findings suggest a revision to the classical view that nonoriented receptive fields are principally found in layer 4C and the cytochrome oxidase-rich blobs in layer 2/3. Instead, a broad distribution of tuning selectivity is found in all cortical layers, and neurons that are weakly tuned for orientation are ubiquitous in V1 cortex.},
             author = {Ringach, Dario L and Shapley, Robert M and Hawken, Michael J},
             doi = {20026567},
-            file = {:Users/tiago/Documents/Mendeley Desktop/Ringach, Shapley, Hawken/Ringach, Shapley, Hawken{\_}2002.pdf:pdf},
             issn = {1529-2401},
             journal = {The Journal of Neuroscience},
             keywords = {1981,1998,bandwidth,circular variance,cortical layer,findings is that there,great diversity of circular,is a,ity,one of our main,orientation selectiv-,primate vision,striate cortex,swindale,variance in v1},
@@ -37,83 +39,83 @@ def _MarquesRingach2002V1Property(property_name, similarity_metric, ceil_func, p
     assembly = brainscore.get_assembly(ASSEMBLY_NAME)
     return PropertiesBenchmark(identifier=f'dicarlo.Marques_ringach2002.V1-{property_name}', assembly=assembly,
                                neuronal_property=ringach2002_properties, similarity_metric=similarity_metric,
-                               parent=parent, ceiling_func=ceil_func, bibtex=BIBTEX, version=0.1)
+                               parent=parent, ceiling_func=ceil_func, bibtex=BIBTEX, version=1)
 
 
-def MarquesRingach2002V1CV():
-    property_name = 'circ_var'
+def MarquesRingach2002V1CircularVariance():
+    property_name = 'circular_variance'
     parent = PARENT_ORIENTATION
     similarity_metric = BootstrapDistributionSimilarity(similarity_func=ks_sim, property_name=property_name)
-    ceil_func = NeuronalPropertyCeiling(BootstrapDistributionSimilarity(similarity_func=ks_sim,
+    ceil_func = NeuronalPropertyCeiling(BootstrapDistributionSimilarity(similarity_func=ks_similarity,
                                                                         property_name=property_name))
     return _MarquesRingach2002V1Property(property_name=property_name, similarity_metric=similarity_metric,
                                          ceil_func=ceil_func, parent=parent)
 
 
-def MarquesRingach2002V1BW():
+def MarquesRingach2002V1Bandwidth():
     property_name = 'bandwidth'
     parent = PARENT_ORIENTATION
     similarity_metric = BootstrapDistributionSimilarity(similarity_func=ks_sim, property_name=property_name)
-    ceil_func = NeuronalPropertyCeiling(BootstrapDistributionSimilarity(similarity_func=ks_sim,
+    ceil_func = NeuronalPropertyCeiling(BootstrapDistributionSimilarity(similarity_func=ks_similarity,
                                                                         property_name=property_name))
     return _MarquesRingach2002V1Property(property_name=property_name, similarity_metric=similarity_metric,
                                          ceil_func=ceil_func, parent=parent)
 
 
-def MarquesRingach2002V1OPR():
-    property_name = 'orth_pref_ratio'
+def MarquesRingach2002V1OrthogonalPreferredRatio():
+    property_name = 'orthogonal_preferred_ratio'
     parent = PARENT_ORIENTATION
     similarity_metric = BootstrapDistributionSimilarity(similarity_func=ks_sim, property_name=property_name)
-    ceil_func = NeuronalPropertyCeiling(BootstrapDistributionSimilarity(similarity_func=ks_sim,
+    ceil_func = NeuronalPropertyCeiling(BootstrapDistributionSimilarity(similarity_func=ks_similarity,
                                                                         property_name=property_name))
     return _MarquesRingach2002V1Property(property_name=property_name, similarity_metric=similarity_metric,
                                          ceil_func=ceil_func, parent=parent)
 
-def MarquesRingach2002V1OS():
-    property_name = 'or_sel'
+def MarquesRingach2002V1OrientationSelective():
+    property_name = 'orientation_selective'
     parent = PARENT_ORIENTATION
     similarity_metric = BootstrapDistributionSimilarity(similarity_func=ks_sim, property_name=property_name)
-    ceil_func = NeuronalPropertyCeiling(BootstrapDistributionSimilarity(similarity_func=ks_sim,
+    ceil_func = NeuronalPropertyCeiling(BootstrapDistributionSimilarity(similarity_func=ks_similarity,
                                                                         property_name=property_name))
     return _MarquesRingach2002V1Property(property_name=property_name, similarity_metric=similarity_metric,
                                          ceil_func=ceil_func, parent=parent)
 
 
-def MarquesRingach2002V1MAXDC():
-    property_name = 'maxdc'
+def MarquesRingach2002V1MaxDC():
+    property_name = 'max_dc'
     parent = PARENT_MAGNITUDE
     similarity_metric = BootstrapDistributionSimilarity(similarity_func=ks_sim, property_name=property_name)
-    ceil_func = NeuronalPropertyCeiling(BootstrapDistributionSimilarity(similarity_func=ks_sim,
+    ceil_func = NeuronalPropertyCeiling(BootstrapDistributionSimilarity(similarity_func=ks_similarity,
                                                                         property_name=property_name))
     return _MarquesRingach2002V1Property(property_name=property_name, similarity_metric=similarity_metric,
                                          ceil_func=ceil_func, parent=parent)
 
 
-def MarquesRingach2002V1MAXAC():
-    property_name = 'maxac'
+def MarquesRingach2002V1MaxAC():
+    property_name = 'max_ac'
     parent = PARENT_MAGNITUDE
     similarity_metric = BootstrapDistributionSimilarity(similarity_func=ks_sim, property_name=property_name)
-    ceil_func = NeuronalPropertyCeiling(BootstrapDistributionSimilarity(similarity_func=ks_sim,
+    ceil_func = NeuronalPropertyCeiling(BootstrapDistributionSimilarity(similarity_func=ks_similarity,
                                                                         property_name=property_name))
     return _MarquesRingach2002V1Property(property_name=property_name, similarity_metric=similarity_metric,
                                          ceil_func=ceil_func, parent=parent)
 
 
-def MarquesRingach2002V1MR():
-    property_name = 'mod_ratio'
+def MarquesRingach2002V1ModulationRatio():
+    property_name = 'modulation_ratio'
     parent = PARENT_MAGNITUDE
     similarity_metric = BootstrapDistributionSimilarity(similarity_func=ks_sim, property_name=property_name)
-    ceil_func = NeuronalPropertyCeiling(BootstrapDistributionSimilarity(similarity_func=ks_sim,
+    ceil_func = NeuronalPropertyCeiling(BootstrapDistributionSimilarity(similarity_func=ks_similarity,
                                                                         property_name=property_name))
     return _MarquesRingach2002V1Property(property_name=property_name, similarity_metric=similarity_metric,
                                          ceil_func=ceil_func, parent=parent)
 
 
-def MarquesRingach2002V1BSLN():
+def MarquesRingach2002V1Baseline():
     property_name = 'baseline'
     parent = PARENT_MAGNITUDE
     similarity_metric = BootstrapDistributionSimilarity(similarity_func=ks_sim, property_name=property_name)
-    ceil_func = NeuronalPropertyCeiling(BootstrapDistributionSimilarity(similarity_func=ks_sim,
+    ceil_func = NeuronalPropertyCeiling(BootstrapDistributionSimilarity(similarity_func=ks_similarity,
                                                                         property_name=property_name))
     return _MarquesRingach2002V1Property(property_name=property_name, similarity_metric=similarity_metric,
                                          ceil_func=ceil_func, parent=parent)
@@ -121,6 +123,7 @@ def MarquesRingach2002V1BSLN():
 
 @store(identifier_ignore=['responses', 'baseline'])
 def ringach2002_properties(model_identifier, responses, baseline):
+    _assert_grating_activations(responses)
     radius = np.array(sorted(set(responses.radius.values)))
     sf = np.array(sorted(set(responses.sf.values)))
     orientation = np.array(sorted(set(responses.orientation.values)))
@@ -138,37 +141,37 @@ def ringach2002_properties(model_identifier, responses, baseline):
     responses_fft = np.absolute(np.fft.fft(responses)) / len(phase)
     responses_ac = responses_fft[:, :, :, 1]
 
-    maxdc = np.zeros((n_neuroids, 1))
-    maxac = np.zeros((n_neuroids, 1))
-    mindc = np.zeros((n_neuroids, 1))
-    circ_var = np.zeros((n_neuroids, 1))
+    max_dc = np.zeros((n_neuroids, 1))
+    max_ac = np.zeros((n_neuroids, 1))
+    min_dc = np.zeros((n_neuroids, 1))
+    circular_variance = np.zeros((n_neuroids, 1))
     bandwidth = np.zeros((n_neuroids, 1))
-    orth_pref_ratio = np.zeros((n_neuroids, 1))
-    or_sel = np.ones((n_neuroids, 1))
+    orthogonal_preferred_ratio = np.zeros((n_neuroids, 1))
+    orientation_selective = np.ones((n_neuroids, 1))
 
     for neur in range(n_neuroids):
         pref_sf, pref_orientation = np.unravel_index(np.argmax(responses_dc[neur, :, :]), (len(sf), len(orientation)))
 
-        maxdc[neur] = responses_dc[neur, pref_sf, pref_orientation]
-        maxac[neur] = responses_ac[neur, pref_sf, pref_orientation]
+        max_dc[neur] = responses_dc[neur, pref_sf, pref_orientation]
+        max_ac[neur] = responses_ac[neur, pref_sf, pref_orientation]
 
         orientation_curve = responses_dc[neur, pref_sf]
-        mindc[neur] = orientation_curve.min()
+        min_dc[neur] = orientation_curve.min()
 
-        circ_var[neur] = calc_cv(orientation_curve, orientation)
+        circular_variance[neur] = calc_circular_variance(orientation_curve, orientation)
         bandwidth[neur],  pref_or_fit, or_full, or_curve_full = \
-            calc_bw(orientation_curve, orientation, filt_type='hanning', thrsh=0.707, mode='half')
-        orth_pref_ratio[neur] = calc_opr(orientation_curve, orientation)
+            calc_bandwidth(orientation_curve, orientation, filt_type='hanning', thrsh=0.707, mode='half')
+        orthogonal_preferred_ratio[neur] = calc_orthogonal_preferred_ratio(orientation_curve, orientation)
 
-    or_sel[np.isnan(bandwidth)] = 0
-    mod_ratio = maxac / maxdc
-    cv_bw_ratio = circ_var / bandwidth
-    opr_cv_diff = orth_pref_ratio - circ_var
-    opr_bw_ratio = orth_pref_ratio/bandwidth
+    orientation_selective[np.isnan(bandwidth)] = 0
+    modulation_ratio = max_ac / max_dc
+    circular_variance_bandwidth_ratio = circular_variance / bandwidth
+    orthogonal_preferred_ratio_circular_variance_difference = orthogonal_preferred_ratio - circular_variance
+    orthogonal_preferred_ratio_bandwidth_ratio = orthogonal_preferred_ratio/bandwidth
 
-    properties_data = np.concatenate((baseline, maxdc, mindc, maxac, mod_ratio, circ_var, bandwidth, orth_pref_ratio,
-                                      or_sel, cv_bw_ratio, opr_cv_diff, opr_bw_ratio), axis=1)
-    good_neuroids = maxdc > baseline + 5
+    properties_data = np.concatenate((baseline, max_dc, min_dc, max_ac, modulation_ratio, circular_variance, bandwidth, orthogonal_preferred_ratio,
+                                      orientation_selective, circular_variance_bandwidth_ratio, orthogonal_preferred_ratio_circular_variance_difference, orthogonal_preferred_ratio_bandwidth_ratio), axis=1)
+    good_neuroids = max_dc > baseline + 5
     properties_data = properties_data[np.argwhere(good_neuroids)[:, 0], :]
 
     properties_data = DataAssembly(properties_data, coords={'neuroid_id': ('neuroid', range(properties_data.shape[0])),

brainscore/metrics/distribution_similarity.py
@@ -43,7 +43,7 @@ class BootstrapDistributionSimilarity(Metric):
         return score
 
 
-def ks_sim(p, q):
+def ks_similarity(p, q):
     z1 = np.zeros_like(p)
     z2 = np.zeros_like(p)
     z1[0] = 1
8fe3cc4cadba370bd1683f999012cfeb86297ad3
Martin Schrimpf <m4rtinsch@gmail.com>
2020-09-28 09:53:30 -0400

    add number_of_trials to test-BrainModels

--- tests/test_benchmarks/test_imagenet_c.py
+++ tests/test_benchmarks/test_imagenet_c.py
@@ -3,9 +3,10 @@ import pytest
 from pytest import approx
 
 from brainio_base.assemblies import BehavioralAssembly
-from brainscore.benchmarks.imagenet import Imagenet2012
-from brainscore.benchmarks.imagenet_c import Imagenet_C_Individual, Imagenet_C_Noise, Imagenet_C_Blur, Imagenet_C_Weather, Imagenet_C_Digital
+from brainscore.benchmarks.imagenet_c import Imagenet_C_Individual, Imagenet_C_Noise, Imagenet_C_Blur, \
+    Imagenet_C_Weather, Imagenet_C_Digital
 from brainscore.model_interface import BrainModel
+
 
 @pytest.mark.slow
 class TestImagenetC:
@@ -22,7 +23,7 @@ class TestImagenetC:
                 assert task == BrainModel.Task.label
                 assert fitting_stimuli == 'imagenet'  # shortcut
 
-            def look_at(self, stimuli):
+            def look_at(self, stimuli, number_of_trials=1):
                 labels = -np.ones_like(stimuli['image_id'].values)
                 return BehavioralAssembly([labels], coords={
                     **{column: ('presentation', stimuli[column].values) for column in stimuli.columns},
@@ -31,6 +32,7 @@ class TestImagenetC:
         candidate = Static()
         scores = [benchmark(candidate) for benchmark in benchmarks]
         assert all([np.mean(score) == approx(0) for score in scores])
+
 
 class TestImagenetCIndividual:
     def test_groundtruth(self, name='dietterich.Hendrycks2019.gaussian_noise_1', noise_type='gaussian_noise'):
@@ -42,7 +44,7 @@ class TestImagenetCIndividual:
                 assert task == BrainModel.Task.label
                 assert fitting_stimuli == 'imagenet'  # shortcut
 
-            def look_at(self, stimuli):
+            def look_at(self, stimuli, number_of_trials=1):
                 source_image_ids = source['image_id'].values
                 stimuli_image_ids = stimuli['image_id'].values
                 sorted_x = source_image_ids[np.argsort(source_image_ids)]

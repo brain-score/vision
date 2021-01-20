3638025e97042c98085c36af5b8542024eefe148
Martin Schrimpf <m4rtinsch@gmail.com>
2021-01-20 18:15:31 -0500

    install submitted repository locally

--- brainscore/submission/repository.py
+++ brainscore/submission/repository.py
@@ -49,7 +49,7 @@ def find_correct_dir(work_dir):
 def install_project(repo, package):
     logger.info('Start installing the submitted repository')
     try:
-        subprocess.check_output([sys.executable, "-m", "pip", "install", "-v", str(repo)], env=os.environ,
+        subprocess.check_output([sys.executable, "-m", "pip", "install", "-v", str(repo), "--user"], env=os.environ,
                                 stderr=subprocess.STDOUT)
         sys.path.insert(0, str(repo))
         logger.info(f'System paths {sys.path}')

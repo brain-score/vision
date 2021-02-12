- Added CKA metric - Added O1, O2, I1, I2, and I1n metrics - Added CKA benchmarks for V1 FreemanZiemba datasets - Corrected bug in the calculation of the AC responses to the grating stimuli (multiplied the amplitude of the FFT harmonic by a factor of 2 to account for the "negative frequency" component) - DeValois1982b and Ringach2002 assemblies -

brainscore/benchmarks/marques2020_devalois1982b.py
@@ -57,7 +57,7 @@ def devalois1982b_properties(model_identifier, responses, baseline):
     responses = responses.reshape((n_neuroids, len(radius), len(spatial_frequency), len(orientation), len(phase)))
     responses_dc = responses.mean(axis=4) - baseline.reshape((-1, 1, 1, 1))
     responses_ac = np.absolute(np.fft.fft(responses)) / len(phase)
-    responses_ac = responses_ac[:, :, :, :, 1]
+    responses_ac = responses_ac[:, :, :, :, 1] * 2
     responses = np.zeros((n_neuroids, len(radius), len(spatial_frequency), len(orientation), 2))
     responses[:, :, :, :, 0] = responses_dc
     responses[:, :, :, :, 1] = responses_ac

brainscore/benchmarks/marques2020_ringach2002.py
@@ -124,7 +124,7 @@ def ringach2002_properties(model_identifier, responses, baseline):
     responses = responses.reshape((n_neuroids, n_cycles, len(spatial_frequency), len(orientation), len(phase)))
     responses_dc = responses.mean(axis=4)
     responses_ac = np.absolute(np.fft.fft(responses)) / len(phase)
-    responses_ac = responses_ac[:, :, :, :, 1]
+    responses_ac = responses_ac[:, :, :, :, 1] * 2
     del responses
 
     max_dc = np.zeros((n_neuroids, 1))
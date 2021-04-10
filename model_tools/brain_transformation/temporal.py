8cec1cd83c94c561c73d4ffc3a159f6fb2e5b20c
Martin Schrimpf <mschrimpf@users.noreply.github.com>
2021-04-10 12:01:24 -0400

    fix time_bin naming (level_0/1 -> start/end) (#53)
    * fix time_bin naming (level_0/1 -> start/end)
    
    * note xarray bug leading to merge mis-naming

--- model_tools/brain_transformation/temporal.py
+++ model_tools/brain_transformation/temporal.py
@@ -1,11 +1,9 @@
-import logging
-
-import numpy as np
-
 from typing import List, Tuple
 
-from brainio_base.assemblies import merge_data_arrays
+import logging
+import numpy as np
 
+from brainio_base.assemblies import merge_data_arrays, walk_coords
 from brainscore.model_interface import BrainModel
 from model_tools.utils import fullname
 
@@ -46,8 +44,24 @@ class TemporalIgnore(BrainModel):
         responses = merge_data_arrays(time_responses)
         if len(self._time_bins) == 1:
             responses = responses.squeeze('time_bin')
+        responses = fix_timebin_naming(responses)
         return responses
 
     @property
     def identifier(self):
         return self._layer_model.identifier
+
+
+def fix_timebin_naming(assembly):
+    """
+    renames coordinate time_bin_level_0 to time_bin_start and time_bin_level_1 to time_bin_end
+    to work around bug introduced in xarray 0.16.2 (and still present in 0.17.0).
+    """
+    # jjpr had found that xarray 0.16.2 introduced a bug where xarray.core.alignment._get_joiner assumes Index when the
+    # object is a MultiIndex.
+    # xarray.rename for some reason does not work for some reason, it cannot find the coords
+    rename = dict(time_bin_level_0='time_bin_start', time_bin_level_1='time_bin_end')
+    assembly = type(assembly)(assembly.values, coords={
+        rename[coord] if coord in rename else coord: (dims, values)
+        for coord, dims, values in walk_coords(assembly)}, dims=assembly.dims)
+    return assembly

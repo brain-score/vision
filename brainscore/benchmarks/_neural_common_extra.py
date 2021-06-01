1ae2205a7deeaaa263483cbc630916f02aeadc9a
LoreGoetschalckx <lore.ne.goetschalckx@gmail.com>
2021-06-01 14:35:10 -0400

    adding dummy benchmark to use to cache features

--- brainscore/benchmarks/_neural_common_extra.py
+++ brainscore/benchmarks/_neural_common_extra.py
@@ -53,6 +53,47 @@ class NeuralBenchmarkCovariate(BenchmarkBase):
         raw_score = self._similarity_metric(source_assembly, covariate_assembly, self._assembly)
         return explained_variance(raw_score, self.ceiling)
 
+class CacheFeaturesCovariate(BenchmarkBase):
+    def __init__(self, identifier, assembly, covariate_image_dir, similarity_metric, visual_degrees, number_of_trials, **kwargs):
+        super(CacheFeaturesCovariate, self).__init__(identifier=identifier, **kwargs)
+        self._assembly = assembly
+        self._similarity_metric = similarity_metric
+        region = np.unique(self._assembly['region'])
+        assert len(region) == 1
+        self.region = region[0]
+        timebins = timebins_from_assembly(self._assembly)
+        self.timebins = timebins
+        self._visual_degrees = visual_degrees
+        self._number_of_trials = number_of_trials
+        self.covariate_image_dir = covariate_image_dir
+
+    def __call__(self, candidate: BrainModel):
+        candidate.start_recording(self.region, time_bins=self.timebins)
+        stimulus_set = place_on_screen(self._assembly.stimulus_set, target_visual_degrees=candidate.visual_degrees(),
+                                       source_visual_degrees=self._visual_degrees)
+
+        # Find 'twin' image set whose model activations will serve as covariate
+        brainio_dir = os.getenv('BRAINIO_HOME', os.path.join(os.path.expanduser('~'), '.brainio'))
+        covariate_stimulus_set = pd.DataFrame(stimulus_set)
+        covariate_stimulus_set = StimulusSet(covariate_stimulus_set)
+        #covariate_stimulus_set = stimulus_set.copy(deep=True)
+        covariate_stimulus_set.identifier = stimulus_set.identifier + '_' + self.covariate_image_dir
+        covariate_stimulus_set.image_paths = {
+            k: os.path.join(brainio_dir,
+                            self.covariate_image_dir,
+                            os.path.basename(v)) for k, v in stimulus_set.image_paths.items()}
+
+        source_assembly = candidate.look_at(stimulus_set, number_of_trials=self._number_of_trials)
+        source_assembly.attrs['stimulus_set_identifier'] = stimulus_set.identifier
+        covariate_assembly = candidate.look_at(covariate_stimulus_set, number_of_trials=self._number_of_trials)
+        covariate_assembly.attrs['stimulus_set_identifier'] = covariate_stimulus_set.identifier
+
+        if 'time_bin' in source_assembly.dims:
+            source_assembly = source_assembly.squeeze('time_bin')  # static case for these benchmarks
+            covariate_assembly = covariate_assembly.squeeze('time_bin')  # static case for these benchmarks
+        #raw_score = self._similarity_metric(source_assembly, covariate_assembly, self._assembly)
+        return 0
+
 class NeuralBenchmarkCovariateGram(BenchmarkBase):
     def __init__(self, identifier, assembly, covariate_image_dir, similarity_metric, visual_degrees, number_of_trials, gram, **kwargs):
         super(NeuralBenchmarkCovariateGram, self).__init__(identifier=identifier, **kwargs)
--- brainscore/tolerance.py
+++ brainscore/tolerance.py
@@ -1,6 +1,6 @@
 from brainscore.utils import LazyLoad
 from brainscore.benchmarks.majajhong2015_combined import _DicarloMajajHong2015Region_lmh, \
-    _DicarloMajajHong2015Region_lmh_covariate, _DicarloMajajHong2015Region_lmh_toleranceceiling, _DicarloMajajHong2015Region_lmh_imagedir, _DicarloMajajHong2015Region_lmh_covariate_gram
+    _DicarloMajajHong2015Region_lmh_covariate, _DicarloMajajHong2015Region_lmh_toleranceceiling, _DicarloMajajHong2015Region_lmh_imagedir, _DicarloMajajHong2015Region_lmh_covariate_gram, _DicarloMajajHong2015Region_lmh_covariate_cache_features
 from brainscore.benchmarks.public_benchmarks import _standard_benchmark
 from brainscore.metrics.ceiling import InternalConsistency, RDMConsistency, ToleranceConsistency
 from brainscore.metrics.rdm import RDMCrossValidated
@@ -142,6 +142,56 @@ def get_tol_99(crossvalidation_kwargs, **kwargs):
     return LazyLoad(top_function)
 
 
+def get_tol_extract_features(crossvalidation_kwargs, **kwargs):
+    '''
+    Similar to tol_drew, except the control regression is PCA + linear regression instead of PLS, and with all components
+    so we make sure all of the control variable is regressed out
+    '''
+
+    assert (kwargs.get('covariate_image_dir', None) is not None)
+    assert (kwargs.get('control', None) is not None)
+    assert (kwargs.get('gram', None) is not None)
+
+    similarity_metric_kwargs = dict(
+        covariate_control=kwargs['control'],
+        control_regression=gram_linear(gram=kwargs['gram'], pca_kwargs={'n_components':0.99}),
+        main_regression=pls_regression(),
+        correlation=pearsonr_correlation(),
+        crossvalidation_kwargs=crossvalidation_kwargs,
+        fname=kwargs.get('explained_variance_fname', None),
+        tag=kwargs.get('csv_file', None)
+    )
+
+    top_function_kwargs = dict(
+        covariate_image_dir=kwargs['covariate_image_dir'],
+        region=kwargs.get('region'),
+        identifier_metric_suffix='tol_99',
+        similarity_metric=CrossRegressedCorrelationDrew(**similarity_metric_kwargs),
+        ceiler=InternalConsistency(),
+        assembly_name=kwargs.get('assembly_name')
+    )
+
+
+
+    def top_function():
+        return _DicarloMajajHong2015Region_lmh_covariate_cache_features(**top_function_kwargs)
+        # return _DicarloMajajHong2015Region_lmh_covariate(
+        #     covariate_image_dir=kwargs['covariate_image_dir'],
+        #     region='IT', identifier_metric_suffix='Drew',
+        #     similarity_metric=CrossRegressedCorrelationDrew(
+        #         covariate_control=kwargs['control'],
+        #         control_regression=gram_linear(gram=kwargs['gram'], pca_kwargs={'n_components': 0.99}),
+        #         main_regression=pls_regression(),
+        #         correlation=pearsonr_correlation(),
+        #         crossvalidation_kwargs=crossvalidation_kwargs,
+        #         fname=kwargs.get('explained_variance_fname', None),
+        #         tag=kwargs.get('csv_file', None)),
+        #     ceiler=InternalConsistency()
+        # )
+
+    return LazyLoad(top_function)
+
+
 def get_tol_99_gram(crossvalidation_kwargs, **kwargs):
     '''
     Similar to tol_drew, except the control regression is PCA + linear regression instead of PLS, and with all components
@@ -172,19 +222,19 @@ def get_tol_99_gram(crossvalidation_kwargs, **kwargs):
 
     return LazyLoad(top_function)
 
-def get_tol_99_(crossvalidation_kwargs, **kwargs):
-    '''
-    This should use the the same NeuralBenchMark class as the one you'd get with the dicarlo.MajajHong2015public.IT-pls
-    identifier, except with more flexibility in arguments you can pass
-    '''
-
-    def top_function():
-        return _standard_benchmark('dicarlo.MajajHong2015.IT.public',
-                            load_assembly=functools.partial(load_majajhong2015, region='IT', access='public'),
-                            visual_degrees=majajhong2015_degrees, number_of_trials=majajhong2015_trials,
-                            stratification_coord='object_name', bibtex=majajhong2015_bibtex)
-
-    return LazyLoad(top_function)
+# def get_tol_99_(crossvalidation_kwargs, **kwargs):
+#     '''
+#     This should use the the same NeuralBenchMark class as the one you'd get with the dicarlo.MajajHong2015public.IT-pls
+#     identifier, except with more flexibility in arguments you can pass
+#     '''
+#
+#     def top_function():
+#         return _standard_benchmark('dicarlo.MajajHong2015.IT.public',
+#                             load_assembly=functools.partial(load_majajhong2015, region='IT', access='public'),
+#                             visual_degrees=majajhong2015_degrees, number_of_trials=majajhong2015_trials,
+#                             stratification_coord='object_name', bibtex=majajhong2015_bibtex)
+#
+#     return LazyLoad(top_function)
 
 
 

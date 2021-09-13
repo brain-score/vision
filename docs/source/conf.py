772f5bcff863582580551b6673b550b9b919f993
Martin Schrimpf <m4rtinsch@gmail.com>
2021-09-13 12:19:46 -0400

    try to add more to the sys.path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))  # to include tests/
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))  # to include tests/
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'tests')))  # to include tests/
 
 # -- Project information -----------------------------------------------------
 

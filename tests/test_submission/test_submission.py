19e46a0c3d86bf4ce606f3efa7bbea78e552f43b
Martin Schrimpf <m4rtinsch@gmail.com>
2021-10-07 19:31:58 -0400

    use dev database for testing

--- tests/test_submission/test_submission.py
+++ tests/test_submission/test_submission.py
@@ -17,7 +17,7 @@ from tests.test_submission import base_model
 from tests.test_submission.test_db import clear_schema, init_user
 
 logger = logging.getLogger(__name__)
-database = 'brainscore-ohio-test'
+database = 'brainscore-1-ohio-cred'  # dev database
 
 
 @pytest.mark.memory_intense

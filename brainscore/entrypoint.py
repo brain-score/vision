6f1ff8437251219643f9f1f5579d8350011f861b
jjpr <jjpr@mit.edu>
2021-10-15 21:55:11 -0400

    Add path for lookup.csv to lookup DataFrame so packaging can write to it. (#305)

--- brainscore/entrypoint.py
+++ brainscore/entrypoint.py
@@ -7,11 +7,15 @@ from pathlib import Path
 
 import pandas as pd
 
-_logger = logging.getLogger(__name__)
+from brainio.lookup import CATALOG_PATH_KEY
 
+_logger = logging.getLogger(__name__)
 
 def brainio_brainscore():
     path = Path(__file__).parent / "lookup.csv"
     _logger.debug(f"Loading lookup from {path}")
     print(f"Loading lookup from {path}")  # print because logging usually isn't set up at this point during import
-    return pd.read_csv(path)
+    df = pd.read_csv(path)
+    df.attrs[CATALOG_PATH_KEY] = str(path)
+    return df
+

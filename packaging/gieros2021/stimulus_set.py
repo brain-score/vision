30943fe08691c9810841c33198e8f5df7b9211df
Michael Ferguson <mef8dd@virginia.edu>
2022-03-15 13:58:40 -0400

    Added Images, stimulus_set for geiros_2021_sketch

---
+++ packaging/gieros2021/stimulus_set.py
@@ -0,0 +1,68 @@
+from pathlib import Path
+from brainio.stimuli import StimulusSet
+from brainio.packaging import package_stimulus_set
+
+stimuli = []  # collect meta
+image_paths = {}  # collect mapping of image_id to filepath
+stimuli_directory = 'Geiros_Behavioral/sketch/dnn/session-1'
+
+
+'''
+Dataset Meta Info (from https://github.com/rgeirhos/generalisation-humans-DNNs)
+
+Sample image from dataset:
+3841_eid_dnn_1-0-10_knife_10_n03041632_32377.JPEG
+
+This is a concatenation of the following information (separated by '_'):
+
+    1) a four-digit number starting with 0000 for the first image in an experiment; 
+       the last image therefore has the number n-1 if n is the number of images in a certain experiment
+    2) short code for experiment name, e.g. 'eid' for eidolon-experiment
+    3) either e.g. 's01' for 'subject-01', or 'dnn' for DNNs
+    4) condition
+    5) category (ground truth)
+    6) a number (just ignore it)
+    7) image identifier in the form a_b.JPEG (or a_b.png), with a being the 
+       WNID (WordNet ID) of the corresponding synset and b being an integer.
+'''
+
+for filepath in Path(stimuli_directory).glob('*.png'):
+
+    # entire name of image file:
+    image_id = filepath.stem
+    split_name = filepath.stem.split('_')
+
+    # ensure proper metadata length per image in set
+    assert len(split_name) == 7
+
+    # Dataset image data, 1-7 from above:
+    image_number = split_name[0]
+    experiment_code = split_name[1]
+    subject = split_name[2]
+    condition = split_name[3]
+    category_ground_truth = split_name[4]
+    random_number = split_name[5]
+    wordnetID_integer = split_name[6]
+
+    image_paths[image_id] = filepath
+    stimuli.append({
+        'image_number': image_number,
+        'experiment_code': experiment_code,
+        'subject': subject,
+        'condition': condition,
+        'category_ground_truth': category_ground_truth,
+        'random_number': random_number,
+        'wordnetID_integer': wordnetID_integer,
+
+        # optionally you can set 'image_path_within_store' to define the filename in the packaged stimuli
+    })
+
+
+stimuli = StimulusSet(stimuli)
+stimuli.image_paths = image_paths
+stimuli.name = 'geiros2021_sketch'  # give the StimulusSet an identifier name
+
+# 800 images in dataset
+assert len(stimuli) == 800  # make sure the StimulusSet is what you would expect
+
+package_stimulus_set(stimuli, stimulus_set_identifier=stimuli.name)  # upload to S3

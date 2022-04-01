d2f4a62a1693041557bc1635de554fe6781dd976
Mike Ferguson <mef8dd@virginia.edu>
2022-04-01 11:49:25 -0400

    Removed extra dim on assembly, address git comments

--- packaging/geirhos2021/sketch/sketch_data_assembly.py
+++ packaging/geirhos2021/sketch/sketch_data_assembly.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from brainio.assemblies import BehavioralAssembly
 from brainio.packaging import package_data_assembly
 import pandas as pd
@@ -30,32 +32,29 @@ all_subjects = pd.concat([subject_1, subject_2, subject_3, subject_4, subject_5,
 all_subjects['image_lookup_id'] = all_subjects['imagename'].str.split("_").str[-1]
 
 # construct the assembly
-assembly = BehavioralAssembly(all_subjects.to_numpy(),
+assembly = BehavioralAssembly(all_subjects['object_response'].to_numpy().flatten(),
                               coords={
-                                  'image_id_long': ('presentation', all_subjects['imagename'].to_numpy()),
-                                  'image_lookup_id': ('presentation', all_subjects['image_lookup_id'].to_numpy()),
-                                  'subject_response': ('presentation', all_subjects['object_response'].to_numpy()),
-                                  'ground_truth': ('presentation', all_subjects['category'].to_numpy()),
-                                  'condition': ('presentation', all_subjects['condition'].to_numpy()),
-                                  'response_time': ('presentation', all_subjects['rt'].to_numpy()),
-                                  'trial': ('presentation', all_subjects['trial'].to_numpy()),
-                                  'subject': ('presentation', all_subjects['subj'].to_numpy()),
-                                  'session': ('presentation', all_subjects['session'].to_numpy()),
+                                  'image_id': ('presentation', all_subjects['imagename'].to_numpy().flatten()),
+                                  'image_lookup_id': ('presentation', all_subjects['image_lookup_id'].to_numpy().flatten()),
+                                  'ground_truth': ('presentation', all_subjects['category'].to_numpy().flatten()),
+                                  'condition': ('presentation', all_subjects['condition'].to_numpy().flatten()),
+                                  'response_time': ('presentation', all_subjects['rt'].to_numpy().flatten()),
+                                  'trial': ('presentation', all_subjects['trial'].to_numpy().flatten()),
+                                  'subject': ('presentation', all_subjects['subj'].to_numpy().flatten()),
+                                  'session': ('presentation', all_subjects['session'].to_numpy().flatten()),
                               },
-                              dims=['presentation', 'info']
+                              dims=['presentation']
                               )
 
 # give the assembly an identifier name
-assembly.name = 'Geirhos2021_Sketch'
+assembly.name = 'brendel.Geirhos2021_sketch'
 
 # make sure assembly dims are correct length
 assert len(assembly['presentation']) == 5600
-assert len(assembly['info']) == 8
 
 # make sure assembly coords are correct length
-assert len(assembly['image_id_long']) == 5600
+assert len(assembly['image_id']) == 5600
 assert len(assembly['image_lookup_id']) == 5600
-assert len(assembly['subject_response']) == 5600
 assert len(assembly['ground_truth']) == 5600
 assert len(assembly['condition']) == 5600
 assert len(assembly['response_time']) == 5600
@@ -65,17 +64,16 @@ assert len(assembly['session']) == 5600
 
 
 # make sure there are 800 unique images (shown 1 time for each  of 7 subjects, total of 7 * 800 = 5600 images shown)
-assert len(all_subjects['image_lookup_id'].unique()) == 800
+assert len(np.unique(assembly['image_lookup_id'].values)) == 800
 
 # make sure there are 7 unique subjects
-assert len(all_subjects['subj'].unique()) == 7
+assert len(np.unique(assembly['subject'].values)) == 7
 
 # make sure there are 16 unique object categories (ground truths)
-assert len(all_subjects['category'].unique()) == 16
+assert len(np.unique(assembly['ground_truth'].values)) == 16
 
 # make sure there is only one condition (i.e. no image variations added after presentation)
-assert len(all_subjects['condition'].unique()) == 1
-
+assert len(np.unique(assembly['condition'].values)) == 1
 
 # upload to S3
 # package_data_assembly(assembly, assembly_identifier=assembly.name, ,

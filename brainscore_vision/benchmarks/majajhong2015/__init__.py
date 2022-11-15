3cab21b3cadcd6108c64d18212cfc27507ad175b
Martin Schrimpf <m4rtinsch@gmail.com>
2022-11-14 22:19:03 -0500

    fix pool -> benchmark_registry

--- brainscore_vision/benchmarks/majajhong2015/__init__.py
+++ brainscore_vision/benchmarks/majajhong2015/__init__.py
@@ -8,13 +8,13 @@ benchmark_registry['dicarlo.MajajHong2015.IT-pls'] = DicarloMajajHong2015ITPLS
 from .benchmark import DicarloMajajHong2015V4Mask, DicarloMajajHong2015ITMask, \
     DicarloMajajHong2015V4RDM, DicarloMajajHong2015ITRDM
 
-pool['dicarlo.MajajHong2015.V4-mask'] = DicarloMajajHong2015V4Mask
-pool['dicarlo.MajajHong2015.IT-mask'] = DicarloMajajHong2015ITMask
-pool['dicarlo.MajajHong2015.V4-rdm'] = DicarloMajajHong2015V4RDM
-pool['dicarlo.MajajHong2015.IT-rdm'] = DicarloMajajHong2015ITRDM
+benchmark_registry['dicarlo.MajajHong2015.V4-mask'] = DicarloMajajHong2015V4Mask
+benchmark_registry['dicarlo.MajajHong2015.IT-mask'] = DicarloMajajHong2015ITMask
+benchmark_registry['dicarlo.MajajHong2015.V4-rdm'] = DicarloMajajHong2015V4RDM
+benchmark_registry['dicarlo.MajajHong2015.IT-rdm'] = DicarloMajajHong2015ITRDM
 
 # public
 from .benchmark import MajajHongV4PublicBenchmark, MajajHongITPublicBenchmark
 
-pool['dicarlo.MajajHong2015public.V4-pls'] = MajajHongV4PublicBenchmark
-pool['dicarlo.MajajHong2015public.IT-pls'] = MajajHongITPublicBenchmark
+benchmark_registry['dicarlo.MajajHong2015public.V4-pls'] = MajajHongV4PublicBenchmark
+benchmark_registry['dicarlo.MajajHong2015public.IT-pls'] = MajajHongITPublicBenchmark

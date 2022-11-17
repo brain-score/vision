acfbe9d60c0af45226bf7587ce471748aac0d973
Martin Schrimpf <m4rtinsch@gmail.com>
2022-11-16 21:02:35 -0500

    use core plugin-management for scoring

--- brainscore_vision/__init__.py
+++ brainscore_vision/__init__.py
@@ -4,8 +4,9 @@ from typing import Dict, Any, Union, Callable
 from brainio.assemblies import DataAssembly
 from brainscore_core.benchmarks import Benchmark
 from brainscore_core.metrics import Metric
+from brainscore_core.metrics import Score
+from brainscore_core.plugin_management.conda_score import wrap_score
 from brainscore_core.plugin_management.import_plugin import import_plugin
-from result_caching import store
 
 from brainscore_vision.benchmarks import benchmark_pool
 from brainscore_vision.model_interface import BrainModel
@@ -49,36 +50,35 @@ def load_model(identifier: str) -> BrainModel:
     return model_registry[identifier]()
 
 
-@store(identifier_ignore=['model', 'benchmark'])
-def score_model(model_identifier, benchmark_identifier, model):
+def _run_score(model_identifier: str, benchmark_identifier: str) -> Score:
     """
-    Score a given model on a given benchmark.
-    The model needs to implement the :class:`~brainscore.model_interface.BrainModel` interface so that the benchmark can
-    interact with it.
-    The benchmark will be looked up from the :data:`~brainscore.benchmarks.benchmark_pool` and evaluates the model
-    on how brain-like it is under that benchmark's experimental paradigm, primate measurements, comparison metric, and
-    ceiling. This results in a quantitative :class:`~brainscore.metrics.Score` ranging from 0 (least brain-like) to 1
-    (most brain-like under this benchmark).
-
-    The results of this method are cached by default (according to the identifiers), calling it twice with the same
-    identifiers will only invoke once.
-
-    :param model_identifier: a unique identifier for this model
-    :param model: the model implementation following the :class:`~brainscore.model_interface.BrainModel` interface
-    :param benchmark_identifier: the identifier of the benchmark to test the model against
-    :return: a :class:`~brainscore.metrics.Score` of how brain-like the candidate model is under this benchmark. The
-                score is normalized by this benchmark's ceiling such that 1 means the model matches the data to ceiling
-                level.
-
-    :seealso: :class:`brainscore.benchmarks.Benchmark`
-    :seealso: :class:`brainscore.model_interface.BrainModel`
+    Score the model referenced by the `model_identifier` on the benchmark referenced by the `benchmark_identifier`.
     """
-    # model_identifier variable is not unused, the result caching component uses it to identify the cached results
-    assert model is not None
-    _logger.debug("retrieving benchmark")
-    benchmark = benchmark_pool[benchmark_identifier]
-    _logger.debug("scoring model")
-    score = benchmark(model)
+    model: BrainModel = load_model(model_identifier)
+    benchmark: Benchmark = load_benchmark(benchmark_identifier)
+    score: Score = benchmark(model)
     score.attrs['model_identifier'] = model_identifier
     score.attrs['benchmark_identifier'] = benchmark_identifier
     return score
+
+
+def score(model_identifier: str, benchmark_identifier: str) -> Score:
+    """
+    Score the model referenced by the `model_identifier` on the benchmark referenced by the `benchmark_identifier`.
+    The model needs to implement the :class:`~brainscore.model_interface.BrainModel` interface
+    so that the benchmark can interact with it.
+    The benchmark will be looked up from the :data:`~brainscore_vision.benchmarks` and evaluates the model
+    (looked up from :data:`~brainscore_vision.models`) on how brain-like it is under that benchmark's
+    experimental paradigm, primate measurements, comparison metric, and ceiling.
+    This results in a quantitative
+    `Score <https://brain-score-core.readthedocs.io/en/latest/modules/metrics.html#brainscore_core.metrics.Score>`_
+    ranging from 0 (least brain-like) to 1 (most brain-like under this benchmark).
+
+    :param model_identifier: the identifier for the model
+    :param benchmark_identifier: the identifier for the benchmark to test the model against
+    :return: a Score of how brain-like the candidate model is under this benchmark. The score is normalized by
+        this benchmark's ceiling such that 1 means the model matches the data to ceiling level.
+    """
+    return wrap_score(__file__,
+                      model_identifier=model_identifier, benchmark_identifier=benchmark_identifier,
+                      score_function=_run_score)

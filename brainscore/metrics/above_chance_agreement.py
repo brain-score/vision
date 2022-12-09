6b1873bcea1f9fa21f464a91e432ab52ba8edbca
Mike Ferguson <mef8dd@virginia.edu>
2022-12-09 10:16:32 -0500

    Ceiling prototype 1

--- brainscore/metrics/above_chance_agreement.py
+++ brainscore/metrics/above_chance_agreement.py
@@ -10,11 +10,10 @@ from brainscore.metrics.transformations import apply_aggregate
 
 class AboveChanceAgreement(Metric):
 
-    def __call__(self, source, target):
+    def __call__(self, source, target, image_type):
         assert len(set(source['stimulus_id'].values)) == len(set(target['stimulus_id'].values))
         # https://github.com/bethgelab/model-vs-human/blob/745046c4d82ff884af618756bd6a5f47b6f36c45/modelvshuman/plotting/analyses.py#L161
         subject_scores = []
-        image_type = "w"
         for subject in self.extract_subjects(target):
             for category in sorted(set(target['animal'].values)):
                 this_target = target.sel(subject=1, animal=category, image_type=image_type)
@@ -40,63 +39,54 @@ class AboveChanceAgreement(Metric):
         center = np.abs(center)
         return Score([center, error], coords={'aggregation': ['center', 'error']}, dims=['aggregation'])
 
-    def ceiling(self, assembly):
+    def ceiling(self, assembly, image_type):
         """
         Computes subject consistency by comparing each subject to all other subjects.
         """
-        subjects = [1, 2, 3, 4, 5, 6, 7]
+        subjects = sorted(set(assembly['subject'].values))
         subject_scores = []
-        image_type = "w"
-        for subject in subjects:
+        for subject1, subject2 in itertools.combinations(subjects, 2):
             for category in sorted(set(assembly['animal'].values)):
-                single_subject = assembly.sel(subject=subject, animal=category, image_type=image_type)
-                all_subjects = assembly.sel(animal=category, image_type=image_type)
 
-                single_subject_images = single_subject["stimulus_id"].values
-                mask = all_subjects["stimulus_id"].isin(single_subject_images)
-                all_subjects = all_subjects.where(mask).dropna("presentation")
+                subject1_assembly = assembly.sel(subject=subject1, animal=category, image_type=image_type)
+                subject2_assembly = assembly.sel(subject=subject2, animal=category, image_type=image_type)
 
-                # get mode of subject responses by image:
-                all_subjects = all_subjects.groupby("stimulus_id").mean()
+                trials_in_common = list(set(subject1_assembly["stimulus_id"].values).intersection(subject2_assembly["stimulus_id"].values))
 
-                # test = []
-                # for image in single_subject_images:
-                #     mode = scipy.stats.mode(all_subjects[all_subjects['stimulus_id'] == image])[0][0]
-                #     test.append(mode)
+                # if the two subjects did not see the same images, pass
+                if len(trials_in_common) == 0:
+                    break
 
+                # get the images where the subjects are in common:
+                mask1 = subject1_assembly["stimulus_id"].isin(trials_in_common)
+                mask2 = subject2_assembly["stimulus_id"].isin(trials_in_common)
 
-                subject_score = self.compare_single_subject(single_subject, all_subjects)
-                subject_score = subject_score.expand_dims('subject').expand_dims('category')
-                subject_score['subject'] = [subject]
-                subject_score['category'] = [category]
-                subject_scores.append(subject_score)
+                sub1 = subject1_assembly.where(mask1).dropna("presentation")
+                sub2 = subject2_assembly.where(mask2).dropna("presentation")
+                pairwise_score = self.compare_single_subject(sub1, sub2, is_ceiling=True)
+                pairwise_score = pairwise_score.expand_dims('subject').expand_dims('category')
+                pairwise_score['subject_left'] = 'subject', [subject1]
+                pairwise_score['subject_right'] = 'subject', [subject2]
+                pairwise_score['category'] = [category]
+                subject_scores.append(Score(pairwise_score))
         subject_scores = Score.merge(*subject_scores)
         subject_scores = apply_aggregate(aggregate_fnc=self.aggregate, values=subject_scores)
         return subject_scores
-        # for subject1, subject2 in itertools.combinations(subjects, 2):
-        #     for category in sorted(set(assembly['nimal'].values)):
-        #         subject1_assembly = assembly.sel(subject=subject1, animal=category)
-        #         subject2_assembly = assembly.sel(subject=subject2, animal=category)
-        #         pairwise_score = self.compare_single_subject(subject1_assembly, subject2_assembly)
-        #         pairwise_score = pairwise_score.expand_dims('subject').expand_dims('category')
-        #         pairwise_score['subject_left'] = 'subject', [subject1]
-        #         pairwise_score['subject_right'] = 'subject', [subject2]
-        #         pairwise_score['category'] = [category]
-        #         subject_scores.append(Score(pairwise_score))
-        # subject_scores = Score.merge(*subject_scores)
-        # subject_scores = apply_aggregate(aggregate_fnc=self.aggregate, values=subject_scores)
-        # return subject_scores
 
     def extract_subjects(self, assembly):
         return list(sorted(set(assembly['subject'].values)))
 
-    def compare_single_subject(self, source, target):
+    def compare_single_subject(self, source, target, is_ceiling=False):
         assert len(source['presentation']) == len(target['presentation'])
         source = source.sortby('stimulus_id')
         target = target.sortby('stimulus_id')
         assert all(source['stimulus_id'].values == target['stimulus_id'].values)
-        correct_source = source.values == source['animal'].values
-        correct_target = target['correct'].values
+        if is_ceiling:
+            correct_source = source['correct'].values
+            correct_target = target['correct'].values
+        else:
+            correct_source = source.values == source['animal'].values
+            correct_target = target['correct'].values
         accuracy_source = np.mean(correct_source)
         accuracy_target = np.mean(correct_target)
         expected_consistency = accuracy_source * accuracy_target + (1 - accuracy_source) * (1 - accuracy_target)

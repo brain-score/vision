559264d16b995e7311ec3675daeb50eadf196339
Mike Ferguson <mef8dd@virginia.edu>
2023-01-17 16:28:08 -0500

    Inverted and Normal now work

--- brainscore/metrics/accuracy_delta.py
+++ brainscore/metrics/accuracy_delta.py
@@ -3,6 +3,7 @@ import itertools
 import numpy as np
 
 from brainscore.metrics import Metric, Score
+import random
 from brainscore.metrics.ceiling import SplitHalvesConsistencyBaker
 
 
@@ -10,19 +11,29 @@ class AccuracyDelta(Metric):
 
     def __call__(self, source, target, image_types, isCeiling=False):
 
+        # if ceiling, then use already existing human delta code on two halves:
         if isCeiling:
-            model_delta = self.get_human_delta(source, image_types)
+            half_1_delta = self.get_human_delta(source, image_types)
+            half_2_delta = self.get_human_delta(target, image_types)
+            score = max((1 - ((np.abs(half_1_delta - half_2_delta)) / half_2_delta)), 0)
+            return score
         else:
             model_delta = self.get_model_delta(source, image_types)
-        human_delta = self.get_human_delta(target, image_types)
+            scores = []
 
-        # calculate metric score:
-        score = max((1 - (np.abs(human_delta - model_delta)) / human_delta), 0)
+            # calculate score over average of 100 sub splits of human delta
+            for i in range(100):
+                human_delta = self.get_human_delta(target, image_types)
+                score = max((1 - ((np.abs(human_delta - model_delta)) / human_delta)), 0)
+                scores.append(score)
 
-        # calculate error
-        error = 0.005
+            # calculate mean:
+            score = np.mean(scores)
 
-        return score
+            # calculate error
+            error = np.std(scores)
+
+            return Score([score, error], coords={'aggregation': ['center', 'error']}, dims=('aggregation',))
 
     def extract_subjects(self, assembly):
         return list(sorted(set(assembly['subject'].values)))
@@ -30,35 +41,50 @@ class AccuracyDelta(Metric):
     def get_human_delta(self, target, image_types):
         # calculate human accuracies for [whole, condition]
         condition_scores_human = []
+
+        # pull half of subjects. This is outside loop to make sure
+        # the same half of subjects are used in w and f conditions.
+        num_subjects = len(set(target["subject"].values))
+        half1_subjects = random.sample(range(1, num_subjects), num_subjects // 2)
+        half1 = target[{'presentation': [subject in half1_subjects for subject in target['subject'].values]}]
+
+        # for whole condition, and other condition (frank or frag)
         for image_type in image_types:
             scores = []
-            for subject in self.extract_subjects(target):
+
+            # get per subject accuracy
+            for subject in self.extract_subjects(half1):
                 this_target = target.sel(image_type=image_type, subject=subject)
                 correct_count = np.count_nonzero(this_target.values)
                 accuracy = correct_count / len(this_target)
                 scores.append(accuracy)
+            condition_scores_human.append(scores)
 
-            overall = np.mean(scores)
-            condition_scores_human.append(overall)
+        # calculate 16-human pairwise accuracy:
+        delta_vector = [a_i - b_i for a_i, b_i in zip(condition_scores_human[0], condition_scores_human[1])]
 
-        # calculate deltas:
-        human_delta = condition_scores_human[0] - condition_scores_human[1]
-        return human_delta
+        # return mean of delta vector. This is equal to the mean of half of subject deltas (random half)
+        return np.mean(delta_vector)
 
     def get_model_delta(self, source, image_types):
-        # calculate model accuracies for [whole, condition]
         condition_scores_model = []
+
+        # for whole condition, and other condition (frank or frag)
         for image_type in image_types:
-            # raw network accuracy per category, whole condition
+
+            # raw network accuracy per category
             scores = []
             for category in sorted(set(source['animal'].values)):
                 this_source = source.sel(animal=category, image_type=image_type)
                 correct_count = (this_source.values == category).sum()
                 accuracy = correct_count / len(this_source[0])
                 scores.append(accuracy)
+
             # overall accuracy, averaged over 9 categories
             overall = np.mean(scores)
             condition_scores_model.append(overall)
+
+        # return difference between whole and condition
         model_delta = condition_scores_model[0] - condition_scores_model[1]
         return model_delta
 

0a1d632334483a6a2fa6d44189126132378d297f
Mike Ferguson <mef8dd@virginia.edu>
2023-01-27 11:46:55 -0500

    Initial Commit for Jacob2020 Benchmarks. First draft of 3DPI Stimulus_Set

---
+++ packaging/jacob2020/3d_processing_assembly.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+from brainio.assemblies import BehavioralAssembly
+from brainio.packaging import package_data_assembly
+import pandas as pd
+
+'''
+Experiment Information:
+ ******* PRELIMINARY - NEED TODO FOR JACOB 2020 ********
+
+ - 32 subjects 
+ - 40 images for each condition (whole, fragmented, frankenstein) = 120 images/subject shown
+ - 120 * 32 = 3840 total images shown 
+ - However there are only 3706 trials total, for data issues (according to Baker)
+ - 9-way AFC, from set: {bear, bunny, cat, elephant, frog, lizard, tiger, turtle, wolf}
+ 
+'''
+
+# initial csv to dataframe processing:
+all_subjects = pd.read_csv('human_data/inverted.csv')
+
+
+# construct the assembly
+assembly = BehavioralAssembly(all_subjects['RSP'],
+                              coords={
+                                  'stimulus_id': ('presentation', all_subjects['FileName']),
+                                  'subject': ('presentation', all_subjects['Subj']),
+                                  'orientation': ('presentation', all_subjects['Inv']),
+                                  'condition': ('presentation', all_subjects['Config']),
+                                  'truth': ('presentation', all_subjects['Animal']),
+                                  'correct': ('presentation', all_subjects['RSP']),
+                              },
+                              dims=['presentation']
+                              )
+
+# give the assembly an identifier name
+assembly.name = 'Baker2022_inverted_distortion'
+
+# upload to S3
+package_data_assembly('brainio_brainscore', assembly, assembly_identifier=assembly.name,
+                      stimulus_set_identifier='Baker2022_inverted_distortion',
+                      assembly_class_name="BehavioralAssembly", bucket_name="brainio-brainscore")
---
+++ packaging/jacob2020/3d_processing_stimulus_set.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+from brainio.stimuli import StimulusSet
+from brainio.packaging import package_stimulus_set
+
+stimuli = []
+image_paths = {}
+stimuli_directory = 'images'
+
+
+'''
+Dataset Information:
+- From Jacob 2020: https://www.nature.com/articles/s41467-021-22078-3
+
+- This stimulus set is only six (6) images - two cubes, 2 "y's, and 2 squares.
+- Naming Convention - sample name: cube_1.jpg
+    
+
+Fields:
+1) shape: string in the set {"cube", "y", "square"}
+2) number - int in the set {1, 2} indicating shape variation 
+'''
+
+for filepath in Path(stimuli_directory).glob('*.jpg'):
+
+    # entire name of image file:
+    image_id = filepath.stem
+
+    # parse the image id:
+    items = image_id.split("_")
+
+    # get image_shape
+    image_shape = items[0]
+
+    # get image_number:
+    image_number = items[1]
+
+    # combine into stimulus set
+    image_paths[image_id] = filepath
+    stimuli.append({
+        'stimulus_id': image_id,
+        'image_shape': image_shape,
+        'image_number': image_number,
+    })
+
+# package stimulus_set
+stimuli = StimulusSet(stimuli)
+stimuli.stimulus_paths = image_paths
+
+# give the StimulusSet an identifier name
+stimuli.name = 'Jacob2020_3d_processing'
+
+# upload to S3
+package_stimulus_set("brainio_brainscore", stimuli, stimulus_set_identifier=stimuli.name,
+                     bucket_name="brainio-brainscore")

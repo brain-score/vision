9e9cb46536a1da8bd7d78523ac26ad9042437369
Jerry2001 <ngohongphuc2001@gmail.com>
2023-03-28 18:56:57 -0400

    Add the occlusion depth stimulus set code

--- packaging/jacob2020/occlusion_depth_ordering/occlusion_depth_ordering_stimulus_set.py
+++ packaging/jacob2020/occlusion_depth_ordering/occlusion_depth_ordering_stimulus_set.py
@@ -4,7 +4,7 @@ from brainio.packaging import package_stimulus_set
 
 stimuli = []
 image_paths = {}
-stimuli_directory = 'occlusion_depth_ordering/images'
+stimuli_directory = 'images'
 
 
 '''
@@ -15,9 +15,9 @@ Dataset Information:
         - occlusion_notched
         - occlusion_adjacent
         - occlusion_occluded
-        - depth_ordering_notched
-        - depth_ordering_adjacent
-        - depth_ordering_occluded
+        - depth_notched
+        - depth_adjacent
+        - depth_occluded
         These six images correspond to Figure 5C in Jacob 2020. The first three are the top row from the figure, right 
         to left in that order. The second three are the bottom row from the figure, right to left, in that order.
 
@@ -25,14 +25,9 @@ Dataset Information:
   
     3) Sample name: occlusion_notched.jpg
     4) Fields:
-        - experiment: string in the set {"occlusion", "depth_ordering"}, indicating the sub-experiment 
-        - image - string in the set {"notched", "adjacent", "occluded"} indicating shape variation 
-'''
-
-
-'''
-TODO JERRY, BELOW:
-
+        - experiment_type: string in the set {"occlusion", "depth"}, indicating the sub-experiment 
+        - image_variation - string in the set {"notched", "adjacent", "occluded"} indicating shape variation
+         
 '''
 
 for filepath in Path(stimuli_directory).glob('*.png'):
@@ -43,18 +38,18 @@ for filepath in Path(stimuli_directory).glob('*.png'):
     # parse the image id:
     items = image_id.split("_")
 
-    # get image_shape
-    image_shape = items[0]
+    # get the type of experiment:
+    experiment_type = items[0]
 
-    # get image_number:
-    image_number = items[1]
+    # get the type of image variation:
+    image_variation = items[1]
 
     # combine into stimulus set
     image_paths[image_id] = filepath
     stimuli.append({
         'stimulus_id': image_id,
-        'experiment_': image_shape,
-        'image_number': image_number,
+        'experiment_type': experiment_type,
+        'image_variation': image_variation,
     })
 
 # package stimulus_set
@@ -62,7 +57,7 @@ stimuli = StimulusSet(stimuli)
 stimuli.stimulus_paths = image_paths
 
 # give the StimulusSet an identifier name
-stimuli.name = 'Jacob2020_3dpi'
+stimuli.name = 'Jacob2020_occlusion_depth_ordering'
 
 # upload to S3
 package_stimulus_set("brainio_brainscore", stimuli, stimulus_set_identifier=stimuli.name,

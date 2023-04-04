659718a0c5cfc80f41f72784c84097beec663fcc
Mike Ferguson <mef8dd@virginia.edu>
2023-04-04 13:46:43 -0400

    added more test stuff

--- tests/test_benchmarks/test_jacob20203DPI.py
+++ tests/test_benchmarks/test_jacob20203DPI.py
@@ -1,33 +1,10 @@
 from pathlib import Path
-
 import pytest
 from pytest import approx
-
 from brainio.assemblies import BehavioralAssembly
 from brainscore import benchmark_pool
 from tests.test_benchmarks import PrecomputedFeatures
 
-
-'''
-TODO:
-
-1) Fill in correct values for ceilings, model scores
-2) Capture models .nc files: locate the cached (pickled) activations 
-   (likely in ~/.result_caching/model_tools.activations.core.ActionsExtractorHelper._from_paths_stored/....pkl
-3) Convert those activations to netCDF: convert pickled activations into netcdf:
-
-   import pickle
-   from brainio.packaging import write_netcdf
-
-    with open('~/.result_caching/.../<filename>.pkl', 'rb') as f:
-        activations = pickle.load(f)
-        write_netcdf(activations, '<path/to/tests/<file>.nc')
-
-3) Upload those to S3
-4) Update test__init__ files
-5) Add filename to test_setup.sh
-
-'''
 
 class TestJacob20203DPI:
 
@@ -49,13 +26,15 @@ class TestJacob20203DPI:
 
     @pytest.mark.parametrize('benchmark, model, expected_raw_score', [
         ('Jacob2020-3dpi_square', 'alexnet', approx(0.5908, abs=0.0001)),
-        ('Jacob2020-3dpi_y', 'alexnet', approx(0.6344, abs=0.0001)),
-        ('Jacob2020-3dpi_square', 'resnet-18', approx(0.2460, abs=0.0001)),
-        ('Jacob2020-3dpi_y', 'resnet-18', approx(0.3695, abs=0.0001)),
+        # ('Jacob2020-3dpi_y', 'alexnet', approx(0.6344, abs=0.0001)),
+        # ('Jacob2020-3dpi_square', 'resnet-18', approx(0.2460, abs=0.0001)),
+        # ('Jacob2020-3dpi_y', 'resnet-18', approx(0.3695, abs=0.0001)),
     ])
     def test_model_raw_score(self, benchmark, model, expected_raw_score):
         # load features
-        precomputed_features = Path(__file__).parent / f'{model}-{benchmark}.nc'
+        shape = benchmark.split("_")[-1]
+        precomputed_features = Path(__file__).parent / \
+                               f"model_identifier={model},benchmark_identifier={shape}-IT,visual_degrees=8.nc"
         benchmark = benchmark_pool[benchmark]
         precomputed_features = BehavioralAssembly.from_files(file_path=precomputed_features)
         precomputed_features = PrecomputedFeatures(precomputed_features,
@@ -86,3 +65,70 @@ class TestJacob20203DPI:
         score = benchmark(precomputed_features)
         assert score[0] == expected_ceiled_score
 
+# from pathlib import Path
+#
+# import pytest
+# from pytest import approx
+#
+# from brainio.assemblies import BehavioralAssembly
+# from brainscore import benchmark_pool
+# from tests.test_benchmarks import PrecomputedFeatures
+#
+#  Jerry: Anything marked with FILL needs to be filled in!
+# class TestJacob2020OcclusionDepthOrdering:
+#
+#     @pytest.mark.parametrize('benchmark', [
+#         'FILL',
+#         'FILL',
+#     ])
+#     def test_in_pool(self, benchmark):
+#         FILL
+#
+#     @pytest.mark.parametrize('benchmark, expected_ceiling', [
+#         ('FILL', approx(FILL, abs=0.0001)),
+#         ('FILL', approx(FILL, abs=0.001)),
+#     ])
+#     def test_benchmark_ceiling(self, benchmark, expected_ceiling):
+#         benchmark = benchmark_pool[benchmark]
+#         ceiling = benchmark.ceiling
+#         assert ceiling.sel(aggregation='center') == expected_ceiling
+#
+#     @pytest.mark.parametrize('benchmark, model, expected_raw_score', [
+#         ('FILL', 'alexnet', approx(FILL, abs=0.0001)),
+#         ('FILL', 'alexnet', approx(FILL, abs=0.0001)),
+#         ('FILL', 'resnet-18', approx(FILL, abs=0.0001)),
+#         ('FILL', 'resnet-18', approx(FILL, abs=0.0001)),
+#     ])
+#     def test_model_raw_score(self, benchmark, model, expected_raw_score):
+#         # load features
+#         precomputed_features = Path(__file__).parent / f'{model}-{benchmark}.nc'
+#         benchmark = benchmark_pool[benchmark]
+#         precomputed_features = BehavioralAssembly.from_files(file_path=precomputed_features)
+#         precomputed_features = PrecomputedFeatures(precomputed_features,
+#                                                    visual_degrees=8.0,  # doesn't matter, features are already computed
+#                                                    )
+#         score = benchmark(precomputed_features)
+#         raw_score = score.raw
+#
+#         # division by ceiling <= 1 should result in higher score
+#         assert score.sel(aggregation='center') >= raw_score.sel(aggregation='center')
+#         assert raw_score.sel(aggregation='center') == expected_raw_score
+#
+#     @pytest.mark.parametrize('benchmark, model, expected_ceiled_score', [
+#         ('FILL', 'alexnet', approx(0.FILL, abs=0.0001)),
+#         ('FILL', 'alexnet', approx(0.FILL, abs=0.0001)),
+#         ('FILL', 'resnet-18', approx(FILL, abs=0.0001)),
+#         ('FILL', 'resnet-18', approx(FILL, abs=0.0001)),
+#     ])
+#     def test_model_ceiled_score(self, benchmark, model, expected_ceiled_score):
+#         # load features
+#         precomputed_features = Path(__file__).parent / f'{model}-{benchmark}.nc'
+#         benchmark = benchmark_pool[benchmark]
+#         precomputed_features = BehavioralAssembly.from_files(file_path=precomputed_features)
+#         precomputed_features = PrecomputedFeatures(precomputed_features,
+#                                                    visual_degrees=8.0,
+#                                                    # doesn't matter, features are already computed
+#                                                    )
+#         score = benchmark(precomputed_features)
+#         assert score[0] == expected_ceiled_score
+#

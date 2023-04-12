d2aaf5268f558196bf0fb9fefff78a8a690e2b9d
Ben Lonnqvist <ben.lonnqvist@epfl.ch>
2023-04-12 11:57:31 +0200

    clean up threshold metric file

--- brainscore/metrics/threshold.py
+++ brainscore/metrics/threshold.py
@@ -12,11 +12,13 @@ from brainio.assemblies import PropertyAssembly, BehavioralAssembly, DataAssembl
 from brainio.stimuli import StimulusSet
 
 
-def cumulative_gaussian(x, mu, sigma):
+def cumulative_gaussian(x: np.array, mu: float, sigma: float) -> float:
+    """The cumulative gaussian function."""
     return 0.5 * (1 + erf((x - mu) / (sigma * np.sqrt(2))))
 
 
-def inverse_cumulative_gaussian(x_point, mu, sigma):
+def inverse_cumulative_gaussian(x_point: float, mu: float, sigma: float) -> float:
+    """Inverts the cumulative_gaussian function."""
     return np.sqrt(2) * sigma * erfinv(2 * x_point - 1) + mu
 
 
@@ -41,23 +43,24 @@ class Threshold(Metric):
         self.threshold_accuracy = threshold_accuracy
         self.scoring = scoring
 
-    def __call__(self, source: Union[np.array, float], target: Union[list, PropertyAssembly]) -> Score:
+    def __call__(self, source: Union[BehavioralAssembly, float], target: Union[list, PropertyAssembly]) -> Score:
         """
-        :param source: Either a np.array containing model responses to individual stimuli, or a pre-computed threshold
-                        as a float.
+        :param source: Either a BehavioralAssembly containing model responses to individual stimuli, or a pre-computed
+                        threshold as a float.
         :param target: Either a list containing human thresholds (for the ceiling function & ThresholdElevation),
                         or a PropertyAsssembly.
-        :return: A Score containing the evaluated model's distance to target thresholds in units of multiples of
-                  the human score.
+        :return: A Score containing the evaluated model's ceiling-adjusted distance to target thresholds.
         """
         # compute threshold from measurements if the input is not a threshold already
-        if not isinstance(source, float):
-            source_threshold = self.compute_threshold(source, self._independent_variable)
-        else:
+        if isinstance(source, float):
             source_threshold = source
-
-        if source_threshold == 'fit_fail':
-            return Score([0.], coords={'aggregation': ['center', ]}, dims=['aggregation'])
+        elif isinstance(source, BehavioralAssembly):
+            source_threshold = self.compute_threshold(source, self._independent_variable)
+            # check whether the psychometric function fit was successful - if not, return a score of 0
+            if source_threshold == 'fit_fail':
+                return Score([0.], coords={'aggregation': ['center', ]}, dims=['aggregation'])
+        else:
+            raise TypeError(f'source is type {type(source)}, but type BehavioralAssembly or float is required.')
 
         # compare threshold to target thresholds
         if self.scoring == 'pool':
@@ -67,12 +70,14 @@ class Threshold(Metric):
         else:
             raise ValueError(f'Scoring method {self.scoring} is not a valid scoring method.')
 
-    def ceiling(self, assembly: PropertyAssembly):
+    def ceiling(self, assembly: Union[PropertyAssembly, Dict[str, PropertyAssembly]]) -> Score:
         """
-        :param assembly: the human PropertyAssembly containing human responses
+        Selects the appropriate ceiling to be computed from target assembly data.
+
+        :param assembly: the human PropertyAssembly containing human responses, or a dict containing the
+                          PropertyAssemblies of the ThresholdElevation metric.
         :return: Score object with coords center (ceiling) and error (STD)
         """
-        # compare threshold to target thresholds
         if self.scoring == 'pool':
             return self.pool_ceiling(assembly)
         elif self.scoring == 'individual':
@@ -95,9 +100,6 @@ class Threshold(Metric):
 
         Currently copied with modification from 'https://github.com/brain-score/brain-score/blob/
         jacob2020_occlusion_depth_ordering/brainscore/metrics/data_cloud_comparision.py#L54'.
-
-        :param assembly:
-        :return:
         """
         human_thresholds: list = assembly.values.tolist()
         scores = []
@@ -116,7 +118,8 @@ class Threshold(Metric):
         ceiling = Score([ceiling, ceiling_error], coords={'aggregation': ['center', 'error']}, dims=['aggregation'])
         return ceiling
 
-    def compute_threshold(self, source, independent_variable):
+    def compute_threshold(self, source: BehavioralAssembly, independent_variable: str) -> float:
+        """Converts the source BehavioralAssembly to a threshold float value."""
         assert len(source.values) == len(source[independent_variable].values)
 
         x_points = source[independent_variable].values
@@ -129,10 +132,16 @@ class Threshold(Metric):
             self.threshold_accuracy = self.inflection_accuracy(x_points, fit_params)
 
         threshold = self.find_threshold(self.threshold_accuracy, fit_params)
-        #plot_psychometric_curve(fit_params[0], fit_params[1], scatter=(x_points, accuracies))
         return threshold
 
-    def fit_threshold_function(self, x_points, y_points):
+    def fit_threshold_function(self, x_points: np.array, y_points: np.array) -> Union[np.array, str]:
+        """
+        A function that takes the x and y-points of the measured variable and handles the fitting of the
+        psychometric threshold function.
+
+        Returns either the fit parameters for self.fit_function or a string tag that indicates the failure
+        of the psychometric curve fit.
+        """
         initial_guess = [np.mean(x_points), np.mean(x_points)]
         try:
             fit = curve_fit(self.fit_function, x_points, y_points, p0=initial_guess)
@@ -144,11 +153,15 @@ class Threshold(Metric):
                   'for every input.')
             return 'fit_fail'
 
-    def find_threshold(self, threshold_accuracy, fit_params):
+    def find_threshold(self, threshold_accuracy: float, fit_params: Tuple[float, ...]) -> float:
+        """
+        A function that uses the inverse fit function to find the value of the threshold in terms of
+        the independent variable (self._independent_variable).
+        """
         threshold = self.fit_inverse_function(threshold_accuracy, *fit_params)
         return threshold
 
-    def inflection_accuracy(self, x_points, fit_params):
+    def inflection_accuracy(self, x_points: np.array, fit_params: np.array) -> float:
         """
         A function that finds the accuracy at the inflection point of the fit function. Useful if you do not care
         about the specific threshold accuracy, but rather about e.g. the elevation at the inflection point.
@@ -159,10 +172,15 @@ class Threshold(Metric):
         return threshold_accuracy
 
     @staticmethod
-    def individual_score(source_threshold, target):
+    def individual_score(source: float, target: Union[list, PropertyAssembly]) -> Score:
+        """
+        Computes the average distance of the source from each of the individual targets in units of the
+        individual targets. This is generally a more stringent scoring method than pool_score, aimed
+        to measure the average of the individual target effects.
+        """
         raw_scores = []
-        for human_threshold in target:
-            raw_score = max((1 - ((np.abs(human_threshold - source_threshold)) / human_threshold)), 0)
+        for target_value in target:
+            raw_score = max((1 - ((np.abs(target_value - source)) / target_value)), 0)
             raw_scores.append(raw_score)
 
         raw_score, model_error = np.mean(raw_scores), np.std(raw_scores)
@@ -170,17 +188,23 @@ class Threshold(Metric):
         return raw_score
 
     @staticmethod
-    def pool_score(source_threshold, target):
+    def pool_score(source: float, target: Union[list, PropertyAssembly]) -> Score:
+        """
+        Computes the distance of the source from the average of the target in units of the target average.
+        This is generally a less stringent scoring method than individual_score, aimed to measure the average
+        target effect.
+        """
         if not isinstance(target, list):
-            human_mean = np.mean(target.values)
+            target_mean = np.mean(target.values)
         else:
-            human_mean = np.mean(target)
-        raw_score = max((1 - ((np.abs(human_mean - source_threshold)) / human_mean)), 0)
+            target_mean = np.mean(target)
+        raw_score = max((1 - ((np.abs(target_mean - source)) / target_mean)), 0)
         raw_score = Score([raw_score], coords={'aggregation': ['center', ]}, dims=['aggregation'])
         return raw_score
 
     @staticmethod
-    def convert_proba_to_correct(source):
+    def convert_proba_to_correct(source: BehavioralAssembly) -> np.array:
+        """Converts the probability values returned by models doing probability tasks to behavioral choices."""
         decisions = np.argmax(source.values, axis=1)
         correct = []
         for presentation, decision in enumerate(decisions):
@@ -190,15 +214,14 @@ class Threshold(Metric):
                 correct.append(0)
         return np.array(correct)
 
-    @staticmethod
-    def remove_data_after_asymptote(x):
-        """NOTE: CURRENTLY NOT IN USE"""
-        # reverse array to get the last occurrence of the max in case of duplicate maxes
-        last_max_index = np.argmax(x[::-1])
-        return x[:last_max_index]
-
 
 class ThresholdElevation(Threshold):
+    """
+    Computes a threshold elevation from two conditions: a baseline condition and a test condition by dividing
+    the threshold of the test condition by the baseline condition. In other words,
+
+    `threshold_elevation = test_condition_threshold / baseline_condition_threshold`.
+    """
     def __init__(self,
                  independent_variable: str,
                  baseline_condition: str,
@@ -217,38 +240,47 @@ class ThresholdElevation(Threshold):
         self.scoring = scoring
 
     def __call__(self,
-                 source: Union[float, Dict[str, np.array]],
+                 source: Union[float, Dict[str, BehavioralAssembly]],
                  target: Union[list, Dict[str, PropertyAssembly]]
                  ) -> Score:
-        if isinstance(source, Dict):
+        """
+        :param source: Either a dictionary containing the BehavioralAssemblies for the test condition and the
+                        baseline condition, or a pre-computed float threshold elevation. If Dict, Dict
+                        keys should be 'condition_assembly' and 'baseline_assembly' respectively.
+        :param target: Either a dictionary containing the PropertyAssemblies for the test condition and the
+                        baseline condition, or a list of pre-computed threshold elevations. If Dict, Dict
+                        keys should be 'condition_assembly' and 'baseline_assembly' respectively.
+        :return: A score containing the evaluated model's ceiling-adjusted distance to target threshold elevations.
+        """
+        # check whether source is a threshold elevation already - if not, compute it.
+        if isinstance(source, float):
+            raw_source_threshold_elevation = source
+        elif isinstance(source, Dict):
             source_baseline_threshold = self.baseline_threshold_metric.compute_threshold(source[self.baseline_condition],
                                                                                          self._independent_variable)
+            # if using the inflection accuracy, get the inflection point from the baseline condition, and use that
+            # for the test condition.
             if self.threshold_accuracy == 'inflection':
                 self.test_threshold_metric.threshold_accuracy = self.baseline_threshold_metric.threshold_accuracy
             source_test_threshold = self.test_threshold_metric.compute_threshold(source[self.test_condition],
                                                                                  self._independent_variable)
             raw_source_threshold_elevation = source_test_threshold / source_baseline_threshold
         else:
-            raw_source_threshold_elevation = source
+            raise TypeError(f'source is type {type(source)}, but type BehavioralAssembly or float is required.')
 
-        if isinstance(target, Dict):
+        # check whether the targets are threshold elevations already - if not, compute them
+        if isinstance(target, list):
+            target_threshold_elevations = target
+        elif isinstance(target, Dict):
             target_threshold_elevations = self.compute_threshold_elevations(target)
         else:
-            target_threshold_elevations = target
+            raise TypeError(f'target is type {type(target)}, but type PropertyAssembly or list is required.')
 
-        # compare threshold to target thresholds
+        # compare threshold elevation to target threshold elevations
         if self.scoring == 'pool':
             return self.pool_score(raw_source_threshold_elevation, target_threshold_elevations)
         elif self.scoring == 'individual':
             return self.individual_score(raw_source_threshold_elevation, target_threshold_elevations)
-        else:
-            raise ValueError(f'Scoring method {self.scoring} is not a valid scoring method.')
-
-    def ceiling(self, assemblies: Dict[str, PropertyAssembly]):
-        if self.scoring == 'pool':
-            return self.pool_ceiling(assemblies)
-        elif self.scoring == 'individual':
-            return self.individual_ceiling(assemblies)
         else:
             raise ValueError(f'Scoring method {self.scoring} is not a valid scoring method.')
 
@@ -267,13 +299,8 @@ class ThresholdElevation(Threshold):
 
         Currently copied with modification from 'https://github.com/brain-score/brain-score/blob/
         jacob2020_occlusion_depth_ordering/brainscore/metrics/data_cloud_comparision.py#L54'.
-
-        :param assemblies:
-        :return:
         """
-        baseline_assembly = assemblies['baseline_assembly']
-        condition_assembly = assemblies['condition_assembly']
-        human_threshold_elevations = list(condition_assembly.values / baseline_assembly.values)
+        human_threshold_elevations = self.compute_threshold_elevations(assemblies)
         scores = []
         for i in range(len(human_threshold_elevations)):
             random_state = np.random.RandomState(i)
@@ -290,7 +317,13 @@ class ThresholdElevation(Threshold):
         ceiling = Score([ceiling, ceiling_error], coords={'aggregation': ['center', 'error']}, dims=['aggregation'])
         return ceiling
 
-    def compute_threshold_elevations(self, assemblies: Dict[str, PropertyAssembly]) -> list:
+    @staticmethod
+    def compute_threshold_elevations(assemblies: Dict[str, PropertyAssembly]) -> list:
+        """
+        Computes the threshold elevations of a baseline condition and a test condition:
+
+        `threshold_elevation = test_condition_threshold / baseline_condition_threshold`.
+        """
         baseline_assembly = assemblies['baseline_assembly']
         condition_assembly = assemblies['condition_assembly']
         threshold_elevations = []
@@ -298,4 +331,3 @@ class ThresholdElevation(Threshold):
             condition_threshold = condition_assembly[i]
             threshold_elevations.append(condition_threshold / baseline_threshold)
         return threshold_elevations
-

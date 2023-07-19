b3fc29490433edeb226e516c7edc2c073c9b6037
Linus <95619282+linus-md@users.noreply.github.com>
2023-07-19 13:32:37 +0200

    Updating the benchmark

--- brainscore/benchmarks/hebart2023.py
+++ brainscore/benchmarks/hebart2023.py
@@ -1,6 +1,10 @@
 import numpy as np
+
+import brainscore
 from brainscore.benchmarks import BenchmarkBase
 from brainscore.model_interface import BrainModel
+from brainscore.utils import LazyLoad
+
 
 
 BIBTEX = """@article{10.7554/eLife.82580,
@@ -14,39 +18,32 @@ BIBTEX = """@article{10.7554/eLife.82580,
           }"""
 
 class Hebart2023Accuracy(BenchmarkBase):
-    def __init__(self):
+    def __init__(self, similarity_measure):
         self._metric = None
-        self._assembly = None
         self._visual_degrees = None
-
         self._number_of_trials = None
+        self.validation_data = None  # https://osf.io/b2a4j -> host on S3 
+
+        #  What is the difference here? What should I use?
+        self._assembly = LazyLoad(lambda: load_assembly("Hebart2023"))     # <- this is from Geirhos er al.
+        def load_assembly(dataset):                                        # 
+            pass                                                           #
+        self.stimulus_set  = brainscore.get_stimulus_set("Hebart2023")     # <- this is from Islam2021
         
-        #TODO I am not sure where this goes , but it should be in the benchmark right?
-        self.validation_data = None  # https://osf.io/b2a4j we'll need to convert this to BehavioralAssembly right?
-
-        # TODO do we put the similarity matrix inside look_at?
-        #self.validation_data_pairs = [[self.validation_data[:, 0], self.validation_data[:, 1]], # All pairs  
-        #                        [self.validation_data[:, 0], self.validation_data[:, 2]], # TODO change to BehavioralAssembly
-        #                        [self.validation_data[:, 1], self.validation_data[:, 2]]]
-
         super(Hebart2023Accuracy, self).__init__(
-            identifier=None, version=1,
-            ceiling_func=None,
-            parent=None,
+            identifier=f'Hebart2023Accuracy_{similarity_measure}', version=1,
+            ceiling_func=None,                                             # Will need the noise ceiling from Martin H.
+            parent='Hebart2023',
             bibtex=BIBTEX)
 
     def __call__(self, candidate: BrainModel):
-        candidate.start_task(BrainModel.Task.odd_one_out, None) 
-        validation_data = self.validation_data
+        candidate.start_task(BrainModel.Task.odd_one_out, self.similarity_measure) # Is it ok to pass the similarity_measure here?
         predicted_odd_one_outs = candidate.look_at(self._assembly.stimulus_set, number_of_trials=self._number_of_trials)
-        raw_score = self._metric(predicted_odd_one_outs, self._assembly)
+        raw_score = self._metric(predicted_odd_one_outs, self._assembly.validation_data)
         ceiling = self.ceiling
         score = (raw_score - 1/3) / (ceiling - 1/3)
         score.attrs['raw'] = raw_score
         score.attrs['ceiling'] = ceiling
         return score
+    
 
-    # TODO do we put the similarity matrix inside look_at?
-    #def sub2ind(array_shape, rows, cols): 
-    #    ind = np.ix_(rows, cols)
-    #    return np.ravel_multi_index(ind, array_shape)

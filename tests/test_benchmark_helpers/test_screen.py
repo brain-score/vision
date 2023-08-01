92b02259795024b900c4b61a3e0522609bccd1bc
Martin Schrimpf <mschrimpf@users.noreply.github.com>
2023-08-01 19:38:48 +0200

    use pyproject.toml instead of setup.py (#383)
    * use pyproject instead of setup
    * explicitly set setuptools py-modules
    * add networkx dependency again
    * add scikit-learn dependency again
    * update screen gray tests: lossless png and more flexible amount gray

--- tests/test_benchmark_helpers/test_screen.py
+++ tests/test_benchmark_helpers/test_screen.py
@@ -1,17 +1,15 @@
-import os
-from tempfile import TemporaryDirectory
-
 import numpy as np
 from PIL import Image
 from pathlib import Path
 from pytest import approx
+from tempfile import TemporaryDirectory
 
 from brainscore_vision.benchmark_helpers.screen import ImageConverter
 
 
 class TestImageConverter:
     def test_identical(self):
-        image_path = Path(os.path.dirname(__file__)) / "rgb1.jpg"
+        image_path = Path(__file__).parent / "rgb1.jpg"
         source_image = np.array(Image.open(image_path))
         with TemporaryDirectory('screen') as target_dir:
             image_converter = ImageConverter(target_dir=target_dir)
@@ -21,7 +19,7 @@ class TestImageConverter:
         assert (source_image == converted_image).all()
 
     def test_crop_shape(self):
-        image_path = Path(os.path.dirname(__file__)) / "rgb1.jpg"
+        image_path = Path(__file__).parent / "rgb1.jpg"
         with TemporaryDirectory('screen') as target_dir:
             image_converter = ImageConverter(target_dir=target_dir)
             converted_image_path = image_converter.convert_image(image_path, source_degrees=10, target_degrees=8)
@@ -29,7 +27,7 @@ class TestImageConverter:
         np.testing.assert_array_equal(converted_image.size, [1855 * .8, 1855 * .8])
 
     def test_enlarge_shape(self):
-        image_path = Path(os.path.dirname(__file__)) / "rgb1.jpg"
+        image_path = Path(__file__).parent / "rgb1.jpg"
         with TemporaryDirectory('screen') as target_dir:
             image_converter = ImageConverter(target_dir=target_dir)
             converted_image_path = image_converter.convert_image(image_path, source_degrees=10, target_degrees=12)
@@ -37,16 +35,16 @@ class TestImageConverter:
         np.testing.assert_array_equal(converted_image.size, [1855 * 1.2, 1855 * 1.2])
 
     def test_enlarge_match(self):
-        image_path = Path(os.path.dirname(__file__)) / "rgb1.jpg"
+        image_path = Path(__file__).parent / "rgb1.jpg"
         with TemporaryDirectory('screen') as target_dir:
             image_converter = ImageConverter(target_dir=target_dir)
             converted_image_path = image_converter.convert_image(image_path, source_degrees=10, target_degrees=12)
             converted_image = Image.open(converted_image_path)
-        target_image = Image.open(Path(os.path.dirname(__file__)) / "rgb1-10to12.jpg")
-        np.testing.assert_array_equal(np.array(converted_image), np.array(target_image))
+        target_image = Image.open(Path(__file__).parent / "rgb1-10to12.png")
+        np.testing.assert_array_equal(converted_image, target_image)
 
     def test_enlarge_gray_background(self):
-        image_path = Path(os.path.dirname(__file__)) / "rgb1.jpg"
+        image_path = Path(__file__).parent / "rgb1.jpg"
         with TemporaryDirectory('screen') as target_dir:
             image_converter = ImageConverter(target_dir=target_dir)
             converted_image_path = image_converter.convert_image(image_path, source_degrees=8, target_degrees=10)
@@ -60,10 +58,10 @@ class TestImageConverter:
             if (color == gray).all():
                 amount_gray += 1
         assert amount_gray / converted_image.size == approx(.11658, abs=.0001)
-        assert amount_gray == 1880753
+        assert amount_gray == approx(1_880_000, rel=.0005)  # depending on Pillow version for some reason
 
     def test_enlarge_image_centered(self):
-        image_path = Path(os.path.dirname(__file__)) / "rgb1.jpg"
+        image_path = Path(__file__).parent / "rgb1.jpg"
         with TemporaryDirectory('screen') as target_dir:
             image_converter = ImageConverter(target_dir=target_dir)
             converted_image_path = image_converter.convert_image(image_path, source_degrees=8, target_degrees=10)

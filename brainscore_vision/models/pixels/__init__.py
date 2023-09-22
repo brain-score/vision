6bdcfe215321f0c89da6b38165c7cf3a83a93bc8
Mike Ferguson <mef8dd@virginia.edu>
2023-09-22 09:50:13 -0400

    added partial fix of lambda

--- brainscore_vision/models/pixels/__init__.py
+++ brainscore_vision/models/pixels/__init__.py
@@ -2,4 +2,4 @@ from brainscore_vision import model_registry
 from brainscore_vision.model_helpers.brain_transformation import ModelCommitment
 from .model import get_model, get_layers
 
-model_registry['pixels'] = ModelCommitment(identifier='pixels', activations_model=get_model('pixels'), layers=get_layers('pixels'))
+model_registry['pixels'] = lambda: ModelCommitment(identifier='pixels', activations_model=get_model('pixels'), layers=get_layers('pixels'))

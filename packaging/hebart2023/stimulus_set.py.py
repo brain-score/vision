4c775cbdcb5ee92c389a6773d832817c2e17b7f9
Linus <95619282+linus-md@users.noreply.github.com>
2023-09-22 13:40:41 +0200

    finish stimulus set script

--- packaging/hebart2023/stimulus_set.py.py
+++ packaging/hebart2023/stimulus_set.py.py
@@ -4,35 +4,35 @@ from brainio.packaging import package_stimulus_set
 import pandas as pd
 import re
 
+
 stimuli = []
 image_paths = {}
-stimuli_directory = 'images'
-
+stimulus_path = '/Users/linussommer/Desktop/Brain-Score-Data/StimulusSet/'
 
 '''
 Dataset Information:
 
-- From Hebart 2023: link TODO
+- From Hebart 2023: https://elifesciences.org/articles/82580
 - 1854 images in total
 
 Fields:
 - image_number: a number {0,1...1853} indicates the image category
-
 '''
 
-for filepath in Path(stimuli_directory).glob('*.jpg'):
+df = pd.read_csv(stimulus_path + 'metadata.csv')
 
+for i in range(len(df)):
     # entire name of image file:
-    image_id = filepath.stem
-    image_paths[image_id] = filepath
+    image_id = df.iloc[i][0]
+    image_paths[image_id] = stimulus_path + 'reference_images/' + df.iloc[i][1][1:]
     stimuli.append({
         'stimulus_id': image_id,
-        'label': image_id[:-4],
-        'number': None # TODO this might be better done manually from the csv
+        'number': i
     })
 
+print(stimuli)
 stimuli = StimulusSet(stimuli)
-
+stimuli.name = 'Hebart2023'  
 stimuli.stimulus_paths = image_paths
 
 # upload to S3

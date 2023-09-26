4c3a7602bd52326d25ae6f6176e37d8a5e86847b
Mike Ferguson <mef8dd@virginia.edu>
2023-09-26 10:13:10 -0400

    changed test to look for correct benchmark identifier (will change benchmark in system soon)

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -8,7 +8,7 @@ from brainscore_vision import score
 
 
 @pytest.mark.parametrize("model_identifier, benchmark_identifier, expected_score", [
-    ("pixels", "MajajHong2015.IT-pls", approx(0.36144805, abs=0.0005)),
+    ("pixels", "dicarlo.MajajHong2015.IT-pls", approx(0.36144805, abs=0.0005)),
 ])
 def test_score(model_identifier, benchmark_identifier, expected_score):
     actual_score = score(model_identifier=model_identifier, benchmark_identifier=benchmark_identifier)
@@ -22,7 +22,7 @@ def test_commandline_score():
             "brainscore_vision",
             "score",
             "--model_identifier=pixels",
-            "--benchmark_identifier=MajajHong2015.IT-pls",
+            "--benchmark_identifier=dicarlo.MajajHong2015.IT-pls",
         ],
         cwd=Path(__file__).parent.parent,
         capture_output=True,
@@ -35,4 +35,4 @@ def test_commandline_score():
     assert "0.0285" in output
     assert "<xarray.Score ()>\narray(0.0285022)" in output
     assert "model_identifier:      pixels" in output
-    assert "benchmark_identifier:  MajajHong2015.IT-pls" in output
+    assert "benchmark_identifier:  dicarlo.MajajHong2015.IT-pls" in output

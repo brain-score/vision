c17ec6e6287f066cae9c753e0a9faf7e51e3a2ee
Mike Ferguson <mef8dd@virginia.edu>
2023-09-26 14:02:08 -0400

    Changed default generic_test model to be pixels

--- tests/test_models/test_generic.py
+++ tests/test_models/test_generic.py
@@ -5,7 +5,8 @@ import brainscore_vision
 
 @pytest.mark.parametrize('identifier',
                          # TODO: replace with generic collection of all models
-                         ['sketch_model-4o-ep10'])
+                         # ['sketch_model-4o-ep10'])
+                         ['pixels'])
 def test_has_identifier(identifier):
     model = brainscore_vision.load_model(identifier)
     assert model.identifier == identifier

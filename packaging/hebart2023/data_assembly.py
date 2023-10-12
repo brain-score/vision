36df19b0204859c27f1b61baeb06cb034103d2b5
Linus <95619282+linus-md@users.noreply.github.com>
2023-10-12 12:04:37 +0200

    add documentation

--- packaging/hebart2023/data_assembly.py
+++ packaging/hebart2023/data_assembly.py
@@ -7,8 +7,10 @@ import pandas as pd
 Experiment Information:
  - https://elifesciences.org/articles/82580
 
- - odd one out task
- - 453.642 total triplets shown 
+ - Participants were shown three images and had to choose the odd one out.
+ - The dataset consisted of 12,340 workers and 4,699,160 triplets, of which 4,574,059 
+   triplets comprised the training and test data for computational modeling and 125,101 
+   triplets the four datasets used for computing noise ceilings
 '''
 
 # initial csv to dataframe processing:
@@ -16,18 +18,18 @@ df = pd.read_csv('/Users/linussommer/Desktop/Brain-Score-Data/DataAssembly/valid
 
 assert len(df) == 453642
 
+# the last image is the odd one in the human trial
 assembly = BehavioralAssembly(df['ooo'],
                                coords={
                                    'stimulus_id': ('presentation', [f"triplet_{i}" for i in range(len(df))]),
                                    'image_1': ('presentation', df['img1']),
                                    'image_2': ('presentation', df['img2']),
-                                   'image_3': ('presentation', df['ooo']),
+                                   'image_3': ('presentation', df['ooo']),  
                                },
                                dims=['presentation'])
 
 # give the assembly an identifier name
 assembly.name = 'Hebart2023'
-
 
 # upload to S3
 package_data_assembly('brainio_brainscore', assembly, assembly_identifier=assembly.name,

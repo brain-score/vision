c7630a9c63b72ac01941c1ab00416ff54c3fa216
Khaled K Shehada <shehadak@mit.edu>
2023-12-07 09:18:44 -0500

    Fixed benchmark id in test_integration

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -12,7 +12,7 @@ def test_can_import():
 
 
 @pytest.mark.parametrize("model_identifier, benchmark_identifier, expected_score", [
-    ("pixels", "MajajHong2015.IT-pls", approx(0.36144805, abs=0.0005)),
+    ("pixels", "dicarlo.MajajHong2015.IT-pls", approx(0.01538053, abs=0.0005)),
 ])
 def test_model_benchmark_score(model_identifier, benchmark_identifier, expected_score):
     from brainscore_vision import score
@@ -27,7 +27,7 @@ def test_model_benchmark_commandline_score():
             "brainscore_vision",
             "score",
             "--model_identifier=pixels",
-            "--benchmark_identifier=MajajHong2015.IT-pls",
+            "--benchmark_identifier=dicarlo.MajajHong2015.IT-pls",
         ],
         cwd=Path(__file__).parent.parent,
         capture_output=True,
@@ -37,7 +37,7 @@ def test_model_benchmark_commandline_score():
     assert "error" not in process.stderr.lower()
     output = process.stdout
     assert "Score" in output
-    assert "0.0285" in output
-    assert "<xarray.Score ()>\narray(0.0285022)" in output
+    assert "0.10192326" in output
+    assert "<xarray.Score ()>\narray(0.01538053)" in output
     assert "model_identifier:      pixels" in output
-    assert "benchmark_identifier:  MajajHong2015.IT-pls" in output
+    assert "benchmark_identifier:  dicarlo.MajajHong2015.IT-pls" in output

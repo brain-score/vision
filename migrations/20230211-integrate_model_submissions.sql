36770be3eeb6987f1969ef7cf1fd42f2007a7ac1
Khaled K Shehada <shehadak@mit.edu>
2023-12-07 09:47:16 -0500

    removed unnecessary migration csv files

--- migrations/20230211-integrate_model_submissions.sql
+++
@@ -1,5 +0,0 @@
--- unique submission ids of all models with a score
-select distinct(bs.id)
-from brainscore_score
-         join brainscore_model bm on brainscore_score.model_id = bm.id
-         join brainscore_submission bs on bm.submission_id = bs.id;

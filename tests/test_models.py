cce169123c6dbdeaf313116a76de09da26127c90
Khaled K Shehada <shehadak@mit.edu>
2023-12-12 19:38:11 -0500

    Updated test_models to test all existing models

--- tests/test_models.py
+++ tests/test_models.py
@@ -9,7 +9,7 @@ import brainscore_vision
 
 @pytest.mark.travis_slow
 @pytest.mark.parametrize('identifier',
-                         ['pixels'])
+                         ['pixels', 'alexnet'])
 def test_has_identifier(identifier):
     model = brainscore_vision.load_model(identifier)
     assert model.identifier == identifier

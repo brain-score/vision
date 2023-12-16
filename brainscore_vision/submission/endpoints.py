53dee2cc798ff42f60498ff00eb30d8a36ccb9cf
Martin Schrimpf <m4rtinsch@gmail.com>
2023-12-16 21:32:17 +0100

    format indent

--- brainscore_vision/submission/endpoints.py
+++ brainscore_vision/submission/endpoints.py
@@ -25,13 +25,13 @@ run_scoring_endpoint = RunScoringEndpoint(vision_plugins, db_secret=config.get_d
 
 def run_scoring(args_dict: Dict[str, Union[str, List]]):
     model_ids, benchmark_ids = resolve_models_benchmarks(domain="vision", args_dict=args_dict)
-    
+
     for benchmark in benchmark_ids:
         for model in model_ids:
             run_scoring_endpoint(domain="vision", jenkins_id=args_dict["jenkins_id"],
-                                model_identifier=model, benchmark_identifier=benchmark,
-                                user_id=args_dict["user_id"], model_type="brainmodel",
-                                public=args_dict["public"], competition=args_dict["competition"])
+                                 model_identifier=model, benchmark_identifier=benchmark,
+                                 user_id=args_dict["user_id"], model_type="brainmodel",
+                                 public=args_dict["public"], competition=args_dict["competition"])
 
 
 def send_email_to_submitter(uid: int, domain: str, pr_number: str,
@@ -49,7 +49,7 @@ if __name__ == '__main__':
     if 'user_id' not in args_dict or args_dict['user_id'] is None:
         user_id = get_user_id(args_dict['author_email'], db_secret=config.get_database_secret())
         args_dict['user_id'] = user_id
-    
+
     if args.fn == 'run_scoring':
         run_scoring(args_dict)
     elif args.fn == 'resolve_models_benchmarks':

9ffd0aa0ac0a9677f13ef716f61bebf2c2950cf6
Martin Schrimpf <m4rtinsch@gmail.com>
2023-12-16 21:31:50 +0100

    integrate submission tests
    tests adapted:
    * test_competition_field_set (from test_integration.test_competition_field)
    * test_competition_field_not_set (from test_integration.test_competition_field_none)
    * test_one_model_multiple_benchmarks (from test_integration.test_rerun_evaluation)
    * add assertion for score comment (from test_integration.test_evaluation)
    
    tests deleted because already present:
    * test_integration.test_failure_evaluation
    * all tests in test_submission.py
    
    tests not incorporated:
    * test_model_failure_evaluation -- this could be interesting to add in the future, when the scoring fails in the middle, leaving an in-progress database state

--- tests/test_submission/test_endpoints.py
+++ tests/test_submission/test_endpoints.py
@@ -1,4 +1,5 @@
 import logging
+
 import numpy as np
 import pytest
 from pytest import approx
@@ -10,7 +11,6 @@ from brainscore_core.submission import database_models
 from brainscore_core.submission.database import connect_db
 from brainscore_core.submission.database_models import clear_schema
 from brainscore_vision.submission.endpoints import run_scoring
-
 
 logger = logging.getLogger(__name__)
 
@@ -31,33 +31,69 @@ class TestRunScoring:
     def teardown_method(self):
         logger.info('Clean database')
         clear_schema()
-        
+
     def test_successful_run(self):
-        args_dict = {'jenkins_id': 62, 'user_id': 1, 'model_type': 'brainmodel', 
-                    'public': True, 'competition': 'None', 'new_models': ['alexnet'], 
-                    'new_benchmarks': ['dicarlo.MajajHong2015public.IT-pls'], 'specified_only': True}
+        args_dict = {'jenkins_id': 62, 'user_id': 1, 'model_type': 'brainmodel',
+                     'public': True, 'competition': 'None', 'new_models': ['alexnet'],
+                     'new_benchmarks': ['dicarlo.MajajHong2015public.IT-pls'], 'specified_only': True}
         run_scoring(args_dict)
         score_entries = database_models.Score.select()
         score_entries = list(score_entries)
         assert len(score_entries) == 1
         score_entry = score_entries[0]
-        assert score_entry.score_ceiled == approx(.5079817, abs=0.005)
+        assert score_entry.score_ceiled == approx(.5079817, abs=0.0005)
+        assert score_entry.comment.startswith('layers:')
 
     @pytest.mark.travis_slow
     def test_multiple_models(self):
-        args_dict = {'jenkins_id': 62, 'user_id': 1, 'model_type': 'brainmodel', 
-                    'public': True, 'competition': 'None', 'new_models': ['pixels', 'alexnet'], 
-                    'new_benchmarks': ['dicarlo.MajajHong2015public.IT-pls'], 'specified_only': True}
+        args_dict = {'jenkins_id': 62, 'user_id': 1, 'model_type': 'brainmodel',
+                     'public': True, 'competition': 'None', 'new_models': ['pixels', 'alexnet'],
+                     'new_benchmarks': ['dicarlo.MajajHong2015public.IT-pls'], 'specified_only': True}
         run_scoring(args_dict)
         score_entries = database_models.Score.select()
         assert len(score_entries) == 2
         score_values = [entry.score_ceiled for entry in score_entries]
         assert all(np.array(score_values) > 0)
 
+    @pytest.mark.travis_slow
+    def test_one_model_multiple_benchmarks(self):
+        args_dict = {'jenkins_id': 62, 'user_id': 1, 'model_type': 'brainmodel',
+                     'public': True, 'competition': 'None',
+                     'new_models': ['pixels', 'alexnet'],
+                     'new_benchmarks': ['dicarlo.MajajHong2015public.IT-pls', 'dicarlo.Rajalingham2018-i2n'],
+                     'specified_only': True}
+        run_scoring(args_dict)
+        score_entries = database_models.Score.select()
+        assert len(score_entries) == 2
+        score_values = [entry.score_ceiled for entry in score_entries]
+        assert all(np.array(score_values) > 0)
+        score_MajajHong = database_models.Score.select(dict(benchmark__identifier='dicarlo.MajajHong2015public.IT-pls'))
+        assert score_MajajHong.score_ceiled == approx(.5079817, abs=0.0005)
+        score_Rajalingham = database_models.Score.select(dict(benchmark__identifier='dicarlo.Rajalingham2018-i2n'))
+        assert score_Rajalingham.score_ceiled == approx(.3701702, abs=0.0005)
+
     def test_benchmark_does_not_exist(self):
-        args_dict = {'jenkins_id': 62, 'user_id': 1, 'model_type': 'brainmodel', 
-                    'public': True, 'competition': 'None', 'new_models': ['alexnet'], 
-                    'new_benchmarks': ['idonotexist'], 'specified_only': True}
+        args_dict = {'jenkins_id': 62, 'user_id': 1, 'model_type': 'brainmodel',
+                     'public': True, 'competition': 'None', 'new_models': ['alexnet'],
+                     'new_benchmarks': ['idonotexist'], 'specified_only': True}
         run_scoring(args_dict)
         score_entries = database_models.Score.select()
         assert len(score_entries) == 0
+
+    def test_competition_field_set(self):
+        args_dict = {'jenkins_id': 62, 'user_id': 1, 'model_type': 'brainmodel',
+                     'public': True, 'competition': 'cosyne2022', 'new_models': ['alexnet'],
+                     'new_benchmarks': ['dicarlo.MajajHong2015public.IT-pls'], 'specified_only': True}
+        run_scoring(args_dict)
+        model_entries = database_models.Model.select()
+        assert len(model_entries) == 1
+        assert model_entries[0].competition == "cosyne2022"
+
+    def test_competition_field_not_set(self):
+        args_dict = {'jenkins_id': 62, 'user_id': 1, 'model_type': 'brainmodel',
+                     'public': True, 'competition': 'None', 'new_models': ['alexnet'],
+                     'new_benchmarks': ['dicarlo.MajajHong2015public.IT-pls'], 'specified_only': True}
+        run_scoring(args_dict)
+        model_entries = database_models.Model.select()
+        assert len(model_entries) == 1
+        assert model_entries[0].competition is None

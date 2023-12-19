c615da18863fe8c55f656a6aa19079fd32598e5c
Martin Schrimpf <m4rtinsch@gmail.com>
2023-12-19 08:11:26 +0100

    rename `BarbuMayo2019` -> `ObjectNet`

--- brainscore_vision/benchmarks/objectnet/__init__.py
+++ brainscore_vision/benchmarks/objectnet/__init__.py
@@ -1,4 +1,4 @@
 from brainscore_vision import benchmark_registry
 from .benchmark import Objectnet
 
-benchmark_registry['BarbuMayo2019-top1'] = Objectnet
+benchmark_registry['ObjectNet-top1'] = Objectnet

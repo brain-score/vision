82322e3df9e4693fd30b4c48c570a3a1633d5263
Martin Schrimpf <m4rtinsch@gmail.com>
2023-12-28 19:54:12 +0100

    fix test for number of columns and replace with set

--- tests/test_benchmarks/test_hebart2023.py
+++ tests/test_benchmarks/test_hebart2023.py
@@ -1,7 +1,9 @@
-import pytest
 import numpy as np
+import pytest
+
 import brainscore
 from brainio.stimuli import StimulusSet
+
 
 @pytest.mark.memory_intense
 @pytest.mark.private_access
@@ -17,10 +19,14 @@ class TestHebart2023:
         image_1 = self.assembly.coords["image_1"]
         image_2 = self.assembly.coords["image_2"]
         image_3 = self.assembly.coords["image_3"]
-        assert len(image_1) == len(image_2) == len(image_3) ==453642
+        assert len(image_1) == len(image_2) == len(image_3) == 453642
 
     def test_stimulus_set(self):
         stimulus_set = self.assembly.attrs['stimulus_set']
         assert len(stimulus_set) == 1854
-        assert len(stimulus_set.columns) == 18
+        assert set(stimulus_set.columns) == {
+            'stimulus_id', 'image_no', 'filename',
+            'WordNet_ID', 'Wordnet_ID2', 'Wordnet_ID3', 'Wordnet_ID4', 'WordNet_Synonyms',
+            'freq_1', 'freq_2', 'top_down_1', 'top_down_2', 'bottom_up', 'word_freq', 'word_freq_online',
+            'example_image', 'dispersion', 'dominant_part', 'rank'}
         assert isinstance(stimulus_set, StimulusSet)

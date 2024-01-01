3f790030eb27bceb9f8f25f7ccc8f2f6fc6d450c
Martin Schrimpf <m4rtinsch@gmail.com>
2024-01-01 22:37:42 +0100

    simplify tests

--- brainscore_vision/data/bashivankar2019/test.py
+++ brainscore_vision/data/bashivankar2019/test.py
@@ -5,20 +5,11 @@ import brainscore_vision
 
 
 @pytest.mark.private_access
-@pytest.mark.parametrize('assembly_identifier', [
-    'BashivanKar2019.naturalistic',
-    'BashivanKar2019.synthetic',
-])
-def test_existence(assembly_identifier):
-    assert brainscore_vision.load_dataset(assembly_identifier) is not None
-
-
-@pytest.mark.private_access
-@pytest.mark.parametrize('assembly,shape,nans', [
+@pytest.mark.parametrize('identifier,shape,nans', [
     ('BashivanKar2019.naturalistic', (24320, 233, 1), 309760),
     ('BashivanKar2019.synthetic', (21360, 233, 1), 4319940),
 ])
-def test_assembly_shape(assembly, shape, nans):
-    assy = brainscore_vision.load_dataset(assembly)
+def test_assembly_shape(identifier, shape, nans):
+    assy = brainscore_vision.load_dataset(identifier)
     assert assy.shape == shape
     assert np.count_nonzero(np.isnan(assy)) == nans

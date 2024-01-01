523488486c64ae046d339886df02ab21d959e95b
Martin Schrimpf <m4rtinsch@gmail.com>
2024-01-01 22:36:59 +0100

    fix loader function

--- brainscore_vision/data/bashivankar2019/test.py
+++ brainscore_vision/data/bashivankar2019/test.py
@@ -1,22 +1,24 @@
-import brainscore_vision
-import brainio
-import pytest
 import numpy as np
+import pytest
+
+import brainscore_vision
 
 
+@pytest.mark.private_access
 @pytest.mark.parametrize('assembly_identifier', [
-    pytest.param('BashivanKar2019.naturalistic', marks=[pytest.mark.private_access]),
-    pytest.param('BashivanKar2019.synthetic', marks=[pytest.mark.private_access]),
+    'BashivanKar2019.naturalistic',
+    'BashivanKar2019.synthetic',
 ])
 def test_existence(assembly_identifier):
     assert brainscore_vision.load_dataset(assembly_identifier) is not None
 
 
+@pytest.mark.private_access
 @pytest.mark.parametrize('assembly,shape,nans', [
-    pytest.param('BashivanKar2019.naturalistic', (24320, 233, 1), 309760, marks=[pytest.mark.private_access]),
-    pytest.param('BashivanKar2019.synthetic', (21360, 233, 1), 4319940, marks=[pytest.mark.private_access]),
+    ('BashivanKar2019.naturalistic', (24320, 233, 1), 309760),
+    ('BashivanKar2019.synthetic', (21360, 233, 1), 4319940),
 ])
 def test_assembly_shape(assembly, shape, nans):
-    assy = brainio.get_assembly(assembly)
+    assy = brainscore_vision.load_dataset(assembly)
     assert assy.shape == shape
     assert np.count_nonzero(np.isnan(assy)) == nans

4ff210976569f8814b46bec49f465eac61e2e2e7
Martin Schrimpf <m4rtinsch@gmail.com>
2024-01-03 11:30:49 +0100

    fix prefix

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -12,7 +12,7 @@ def test_can_import():
 
 @pytest.mark.memory_intense
 @pytest.mark.parametrize("model_identifier, benchmark_identifier, expected_score", [
-    ("pixels", "dicarlo.MajajHong2015public.IT-pls", approx(0.07637264, abs=0.0005)),
+    ("pixels", "MajajHong2015public.IT-pls", approx(0.07637264, abs=0.0005)),
 ])
 def test_model_benchmark_score(model_identifier, benchmark_identifier, expected_score):
     from brainscore_vision import score
@@ -27,7 +27,7 @@ def test_model_benchmark_commandline_score():
             "brainscore_vision",
             "score",
             "--model_identifier=pixels",
-            "--benchmark_identifier=dicarlo.MajajHong2015public.IT-pls",
+            "--benchmark_identifier=MajajHong2015public.IT-pls",
         ],
         cwd=Path(__file__).parent.parent,
         capture_output=True,
@@ -40,4 +40,4 @@ def test_model_benchmark_commandline_score():
     assert "0.00548197" in output
     assert "<xarray.Score ()>\narray(0.07637264)" in output
     assert "model_identifier:      pixels" in output
-    assert "benchmark_identifier:  dicarlo.MajajHong2015public.IT-pls" in output
+    assert "benchmark_identifier:  MajajHong2015public.IT-pls" in output

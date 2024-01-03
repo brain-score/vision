ae468b78578ee42506001f54960bea98df2fd0ee
Martin Schrimpf <m4rtinsch@gmail.com>
2024-01-03 11:29:40 +0100

    allow maximum of 30 minutes per cell

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -15,7 +15,7 @@ def run_notebook(notebook_path):
     with open(notebook_path, encoding="utf-8") as f:
         nb = nbformat.read(f, as_version=4)
 
-    proc = ExecutePreprocessor(timeout=600, kernel_name='python3')
+    proc = ExecutePreprocessor(timeout=30 * 60, kernel_name='python3')
     proc.allow_errors = True
     proc.preprocess(nb, {'metadata': {'path': '/'}})
 

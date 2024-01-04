a0e0627cc4ebe614ef2846056bb13d7d37499fa3
Martin Schrimpf <m4rtinsch@gmail.com>
2024-01-04 23:52:58 +0100

    add tests for test_supports_behavior_or_neural

--- tests/test_model_helpers/test_generic_plugin_tests.py
+++ tests/test_model_helpers/test_generic_plugin_tests.py
@@ -2,6 +2,7 @@ from collections import namedtuple
 
 import pytest
 
+from brainscore_vision import BrainModel
 # Note that we cannot import `from brainscore_vision.model_helpers.generic_plugin_tests import test_*` directly
 # since this would expose the `test_*` methods during pytest test collection
 from brainscore_vision.model_helpers import generic_plugin_tests
@@ -47,3 +48,60 @@ class TestHasVisualDegrees:
         load_mock.return_value = object()
         with pytest.raises(AttributeError):
             generic_plugin_tests.test_has_visual_degrees(identifier='dummy')
+
+
+class TestSupportsBehaviorOrNeural:
+    ModelClass = namedtuple("DummyModel", field_names=['start_task', 'start_recording'], defaults=[None, None])
+
+    def test_supports_all(self, mocker):
+        load_mock = mocker.patch('brainscore_vision.model_helpers.generic_plugin_tests.load_model')
+        load_mock.return_value = self.ModelClass(start_task=lambda task, fitting_stimuli=None: None,
+                                                 start_recording=lambda recording_target, time_bins: None)
+        generic_plugin_tests.test_supports_behavior_or_neural('dummy')
+
+    def test_supports_behavior_only(self, mocker):
+        load_mock = mocker.patch('brainscore_vision.model_helpers.generic_plugin_tests.load_model')
+        load_mock.return_value = self.ModelClass(start_task=lambda task, fitting_stimuli=None: None)
+        generic_plugin_tests.test_supports_behavior_or_neural('dummy')
+
+    def test_supports_behavior_label_only(self, mocker):
+        load_mock = mocker.patch('brainscore_vision.model_helpers.generic_plugin_tests.load_model')
+
+        def start_task(task: BrainModel.Task, fitting_stimuli):
+            if task != BrainModel.Task.label:
+                raise NotImplementedError()
+
+        load_mock.return_value = self.ModelClass(start_task=start_task)
+        generic_plugin_tests.test_supports_behavior_or_neural('dummy')
+
+    def test_supports_neural_only(self, mocker):
+        load_mock = mocker.patch('brainscore_vision.model_helpers.generic_plugin_tests.load_model')
+        load_mock.return_value = self.ModelClass(start_recording=lambda recording_target, time_bins: None)
+        generic_plugin_tests.test_supports_behavior_or_neural('dummy')
+
+    def test_supports_neural_V1_only(self, mocker):
+        load_mock = mocker.patch('brainscore_vision.model_helpers.generic_plugin_tests.load_model')
+
+        def start_recording(recording_target: BrainModel.RecordingTarget, time_bins):
+            if recording_target != BrainModel.RecordingTarget.V1:
+                raise NotImplementedError()
+
+        load_mock.return_value = self.ModelClass(start_recording=start_recording)
+        generic_plugin_tests.test_supports_behavior_or_neural('dummy')
+
+
+    def test_supports_neural_IT_only(self, mocker):
+        load_mock = mocker.patch('brainscore_vision.model_helpers.generic_plugin_tests.load_model')
+
+        def start_recording(recording_target: BrainModel.RecordingTarget, time_bins):
+            if recording_target != BrainModel.RecordingTarget.IT:
+                raise NotImplementedError()
+
+        load_mock.return_value = self.ModelClass(start_recording=start_recording)
+        generic_plugin_tests.test_supports_behavior_or_neural('dummy')
+
+    def test_supports_None(self, mocker):
+        load_mock = mocker.patch('brainscore_vision.model_helpers.generic_plugin_tests.load_model')
+        load_mock.return_value = self.ModelClass()
+        with pytest.raises(AssertionError):
+            generic_plugin_tests.test_supports_behavior_or_neural('dummy')

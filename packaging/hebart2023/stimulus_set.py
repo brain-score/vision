cd1df1ef6185527bdbebb6f71b29424d80f4a8cc
Linus Sommer <95619282+linus-md@users.noreply.github.com>
2024-01-04 16:07:33 +0100

    Add Hebart2023 Dataset (#420)
    * Add Hebart Dataset
    
    * fix test file naming
    
    * fix test for number of columns and replace with set
    
    * test matching stimulus ids
    
    * Use correct stimulus_id in StimulusSet
    
    * Update lookup.csv
    
    * StimulusSet test are passing
    
    * All tests are passing!!!
    
    * fix expected columns
    
    ---------
    
    Co-authored-by: Martin Schrimpf <m4rtinsch@gmail.com>

---
+++ packaging/hebart2023/stimulus_set.py
@@ -0,0 +1,72 @@
+from brainio.stimuli import StimulusSet
+from brainio.packaging import package_stimulus_set
+import pandas as pd
+import numpy as np
+
+'''
+Dataset Information:
+
+- From Hebart 2023: https://elifesciences.org/articles/82580v1
+- For metadata see https://osf.io/xtafs
+
+- Images were taken from the THINGS object concept and image database.
+- The datasset consists of 1,854 diverse object concepts sampled systematically 
+  from concrete picturable and nameable nouns in the American English language.
+
+Fields:
+- image_number: a number {0,1...1853} indicates the image category
+'''
+
+stimuli = []
+image_paths = {}
+path = '/Users/linussommer/Desktop/Brain-Score-Data/StimulusSet/'
+img_path = '/Users/linussommer/Desktop/Brain-Score-Data/StimulusSet/reference_images/'
+
+df = pd.read_csv(path + 'metadata2.tsv', sep='\t')
+df.drop(columns=['Word'], inplace=True)
+df['image_no'] = df['image_no'].astype(str)
+
+column_mapping = {
+    # ids 
+    'uniqueID': 'unique_id',
+    'image_no': 'stimulus_id',
+    'Example_image': 'example_image',
+    
+    # COCA
+    'COCA_word_freq_online': 'word_freq_online',
+    'COCA_word_freq': 'word_freq',
+    'COCA_dispersion': 'dispersion',
+    'COCA_rank': 'rank',
+    
+    # Frequencies
+    'BNC_freq': 'freq_1',
+    'SUBTLEX_freq': 'freq_2',
+    
+    # Hierarchical properties
+    'Dominant_Part_of_Speech': 'dominant_part',
+    'Bottom_up_Category_human': 'bottom_up',
+    'Top_down_Category_WordNet': 'top_down_1',
+    'Top_down_Category_manual': 'top_down_2',
+    
+    # WordNet
+    'WordNet_Synonyms': 'WordNet_synonyms',
+    'WordNet_ID': 'WordNet_ID',
+    'Wordnet_ID2': 'Wordnet_ID2',
+    'Wordnet_ID3': 'Wordnet_ID3',
+    'Wordnet_ID4': 'Wordnet_ID4',
+}
+
+for index, row in df.iterrows():
+    stimuli.append({column_mapping[col]: row[col] for col in df.columns})
+    image_paths[row['image_no']] = img_path + row['uniqueID'] + '.jpg'
+
+stimuli = StimulusSet(stimuli)
+stimuli.name = 'Hebart2023'
+stimuli.stimulus_paths = image_paths
+
+assert len(stimuli) == 1854
+assert len(stimuli.columns) == 18
+
+package_stimulus_set("brainio_brainscore", stimuli,
+                     stimulus_set_identifier=stimuli.name,
+                     bucket_name="brainio-brainscore")
---
+++ tests/test_benchmarks/test_hebart2023.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pytest
+
+import brainscore
+from brainio.stimuli import StimulusSet
+
+
+@pytest.mark.memory_intense
+@pytest.mark.private_access
+class TestHebart2023:
+    assembly = brainscore.get_assembly('Hebart2023')
+    stimulus_set = brainscore.get_stimulus_set("Hebart2023")
+
+    def test_assembly(self):
+        stimulus_id = self.assembly.coords["stimulus_id"]
+        triplet_id = self.assembly.coords["triplet_id"]
+        assert len(stimulus_id) == len(triplet_id) == 453642
+        assert len(np.unique(stimulus_id)) == 1854
+
+        image_1 = self.assembly.coords["image_1"]
+        image_2 = self.assembly.coords["image_2"]
+        image_3 = self.assembly.coords["image_3"]
+        assert len(image_1) == len(image_2) == len(image_3) == 453642
+
+    def test_assembly_stimulusset_ids_match(self):
+        stimulusset_ids = self.stimulus_set['stimulus_id']
+        for assembly_stimulusid in ['image_1', 'image_2', 'image_3']:
+            assembly_values = self.assembly[assembly_stimulusid].values
+            assert set(assembly_values) == set(stimulusset_ids), \
+                f"Assembly stimulus id reference '{assembly_stimulusid}' does not match stimulus_set"
+
+    def test_stimulus_set(self):
+        assert len(self.stimulus_set) == 1854
+        assert {'unique_id', 'stimulus_id', 'filename',
+                'WordNet_ID', 'Wordnet_ID2', 'Wordnet_ID3', 'Wordnet_ID4', 'WordNet_synonyms',
+                'freq_1', 'freq_2', 'top_down_1', 'top_down_2', 'bottom_up', 'word_freq', 'word_freq_online',
+                'example_image', 'dispersion', 'dominant_part', 'rank'} == set(self.stimulus_set.columns)
+        assert isinstance(self.stimulus_set, StimulusSet)

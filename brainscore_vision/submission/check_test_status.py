3f9b4fad171da523e659e5adfda1fc161f9b6fb3
Katherine Fairchild <kvfairchild@gmail.com>
2024-01-05 23:33:00 -0500

    check test suite results

--- brainscore_vision/submission/check_test_status.py
+++ brainscore_vision/submission/check_test_status.py
@@ -6,7 +6,7 @@ import sys
 BASE_URL = "https://api.github.com/repos/brain-score/vision"
 
 
-def get_data(pr_head_sha: str, url: str) -> dict:
+def get_data(url: str) -> dict:
     r = requests.get(url)
     assert r.status_code == 200
     return r.json()
@@ -21,21 +21,21 @@ def get_statuses_result(context: str, statuses: dict) -> str:
 
 def all_tests_passing(test_results: list):
     if any(result != "success" for result in test_results):
-        print(False)
+        return False
     else:
-        print(True)
+        return True
 
 
 if __name__ == "__main__":
 
     pr_head_sha = sys.argv[1]
 
-    check_runs_json = get_data(pr_head_sha, f"{BASE_URL}/commits/{pr_head_sha}/check-runs")
-    statuses_json = get_data(pr_head_sha, f"{BASE_URL}/statuses/{pr_head_sha}")
+    check_runs_json = get_data(f"{BASE_URL}/commits/{pr_head_sha}/check-runs")
+    statuses_json = get_data(f"{BASE_URL}/statuses/{pr_head_sha}")
 
     travis_branch_result = get_check_runs_result('Travis CI - Branch', check_runs_json)
     travis_pr_result = get_statuses_result('continuous-integration/travis', statuses_json)
     jenkins_plugintests_result = get_statuses_result('Brain-Score Jenkins CI - plugin tests', statuses_json)
     jenkins_unittests_result = get_statuses_result('Brain-Score Jenkins CI', statuses_json)
 
-    all_tests_passing([travis_branch_result, travis_pr_result, jenkins_plugintests_result, jenkins_unittests_result])
+    print(all_tests_passing([travis_branch_result, travis_pr_result, jenkins_plugintests_result, jenkins_unittests_result]))

4e6cd083be1bbbdf0bf413cf8c34cc3fb3f99f67
Katherine Fairchild <kvg0@mit.edu>
2024-01-06 22:54:35 -0500

    Make automerge contingent on both Travis and Jenkins (#443)
    * add trigger on completed check_run
    
    * check test suite results
    
    * check test suite results
    
    * remove plugin_only check (made redundant by check_test_status.py)
    
    * make automerge conditional on both travis and jenkins
    
    * cleanup automerge trigger
    
    * update status description
    
    ---------
    
    Co-authored-by: Katherine Fairchild <kvfairchild@gmail.com>

--- .github/workflows/travis_trigger.sh
+++ .github/workflows/travis_trigger.sh
@@ -5,6 +5,6 @@ TRAVIS_PULL_REQUEST_SHA=$2
 
 curl -L -X POST \
 -H "Authorization: token $GH_WORKFLOW_TRIGGER" \
--d '{"state": "success", "description": "Run automerge workflow for plugin-only PR", 
+-d '{"state": "success", "description": "Successful Travis PR build for plugin-only PR", 
   "context": "continuous-integration/travis"}' \
   "https://api.github.com/repos/brain-score/brain-score/statuses/$TRAVIS_PULL_REQUEST_SHA"

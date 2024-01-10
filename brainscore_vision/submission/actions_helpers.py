cc481da4b1c7b6dc36b5bbcb7e9e9a1642df15e6
Katherine Fairchild <kvg0@mit.edu>
2024-01-09 21:33:14 -0500

    Split `automerge_plugin-only_prs` into two workflows: `check_if_pr_is_automergeable` and `automerge_plugin-only_prs` (#488)
    * generalize trigger for all workflows
    
    * split automergeable check and approval/merge into two workflows
    
    * nit (lowercase -> capital constants)
    
    ---------
    
    Co-authored-by: Katherine Fairchild <kvfairchild@gmail.com>

--- brainscore_vision/submission/actions_helpers.py
+++ brainscore_vision/submission/actions_helpers.py
@@ -1,4 +1,13 @@
-# Uses GitHub API to check test suite status (Travis, Jenkins)
+"""
+Supports two GitHub Actions automerge workflows:
+1. automerge_plugin-only_prs
+2. check_if_pr_is_automergeable
+Uses GitHub API to support the following functions
+- Retrieve a PR Head (SHA or branch name) from the workflow trigger event
+- Retrieve a PR number from a PR Head (SHA or branch name)
+- Check test suite status (Travis, Jenkins)
+Final outputs are accessed by the action via print()
+"""
 
 import json
 import os
@@ -9,44 +18,55 @@ from typing import Union
 BASE_URL = "https://api.github.com/repos/brain-score/vision"
 
 
+def get_data(url: str) -> dict:
+    r = requests.get(url)
+    assert r.status_code == 200, f'{r.status_code}: {r.reason}'
+    return r.json()
+
+def get_pr_num_from_head(pr_head) -> int:
+    """
+    Given either an SHA (check_run or status event) or a branch name (pull request event),
+    returns the number of the pull request with that head SHA or branch name.
+    """
+    event_type = os.environ["GITHUB_EVENT_NAME"]
+
+    if event_type == "pull_request":
+        query = f"repo:brain-score/vision type:pr head:{pr_head}"
+    else:
+        query = f"repo:brain-score/vision type:pr sha:{pr_head}"
+    url = f"https://api.github.com/search/issues?q={query}"
+    pull_requests = get_data(url)
+    assert pull_requests["total_count"] == 1, f'Expected one PR associated with this SHA but found none or more than one, cannot automerge'
+    pr_num = pull_requests["items"][0]["number"]
+
+    return pr_num
+
 def _load_event_file() -> dict:
     with open(os.environ["GITHUB_EVENT_PATH"]) as f:
         return json.load(f)
     
-def _get_pr_head_sha_from_github_event(pr_head_sha):
+def get_pr_head_from_github_event() -> str:
+    """
+    Based on the event that triggered the workflow (check_run, status, or pull_request),
+    returns either an SHA (check_run or status) or branch name (pull_request)
+    """
+    pr_head = None
     event_type = os.environ["GITHUB_EVENT_NAME"]
 
     if event_type == "status":
         f = _load_event_file()
         candidate_branches = [branch for branch in f["branches"] if branch["name"] != "master"]
         if len(candidate_branches) == 1:
-            pr_head_sha = candidate_branches[0]["commit"]["sha"]
+            pr_head = candidate_branches[0]["commit"]["sha"]
 
     elif event_type == "check_run":
         f = _load_event_file()
-        pr_head_sha = f["check_run"]["head_sha"]
+        pr_head = f["check_run"]["head_sha"]
 
     elif event_type == "pull_request":
-        pr_head_sha = os.environ["GITHUB_HEAD_REF"]
+        pr_head = os.environ["GITHUB_HEAD_REF"]
 
-    return pr_head_sha
-
-def get_pr_head_sha() -> Union[str, None]:
-    pr_head_sha = None
-
-    # Running in a GitHub Action
-    if "GITHUB_EVENT_NAME" in os.environ:
-        pr_head_sha = _get_pr_head_sha_from_github_event(pr_head_sha)
-    # If not running in GitHub Action, assume first arg is SHA
-    elif "GITHUB_EVENT_NAME" not in os.environ and len(sys.argv) > 1:
-        pr_head_sha = sys.argv[1]
-
-    return pr_head_sha
-
-def get_data(url: str) -> dict:
-    r = requests.get(url)
-    assert r.status_code == 200, f'{r.status_code}: {r.reason}'
-    return r.json()
+    return pr_head
 
 def _get_end_time(d: dict) -> str:
     return d['end_time']
@@ -86,16 +106,21 @@ def is_labeled_automerge(check_runs_json: dict) -> bool:
 
 if __name__ == "__main__":
     
-    pr_head_sha = get_pr_head_sha()
-    if not pr_head_sha:
-        print("No PR Head SHA found. Exiting."); sys.exit()
-    # if file called with get_sha, print SHA and quit (GitHub Actions logging)
+    pr_head = get_pr_head_from_github_event()
+    if not pr_head:
+        print("No PR head found. Exiting."); sys.exit()
+    
+    # GitHub Actions helpers
     if len(sys.argv) > 1:
-        if sys.argv[1] == "get_sha":
-            print(pr_head_sha); sys.exit()
+        if sys.argv[1] == "get_pr_head":
+            print(pr_head)
+        elif sys.argv[1] == "get_pr_num":
+            print(get_pr_num_from_head(pr_head))
+        sys.exit()
 
-    check_runs_json = get_data(f"{BASE_URL}/commits/{pr_head_sha}/check-runs")
-    statuses_json = get_data(f"{BASE_URL}/statuses/{pr_head_sha}")
+    # Check test results and ensure PR is automergeable
+    check_runs_json = get_data(f"{BASE_URL}/commits/{pr_head}/check-runs")
+    statuses_json = get_data(f"{BASE_URL}/statuses/{pr_head}")
 
     results_dict = {'travis_branch_result': get_check_runs_result('Travis CI - Branch', check_runs_json),
                     'travis_pr_result': get_statuses_result('continuous-integration/travis', statuses_json),

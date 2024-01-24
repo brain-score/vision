87b9188f68e4198911d84328021cbe21027f68c0
linus-md <linus.sommer@protonmail.com>
2024-01-24 17:49:20 +0100

    Update hebart2023/test.py to 2.0 standards

--- brainscore_vision/benchmarks/hebart2023/test.py
+++ brainscore_vision/benchmarks/hebart2023/test.py
@@ -1,15 +1,14 @@
-from pathlib import Path
 import pytest
 from pytest import approx
-from brainio.stimuli import StimulusSet
-from brainscore import benchmark_pool
-# TODO update imports vor brainscore_vision
-from brainscore.benchmarks.hebart2023 import Hebart2023Accuracy
-class TestHebart2023:
-    benchmark = Hebart2023Accuracy()
-    assembly = benchmark._assembly
 
-    def test_ceiling(self):
-        benchmark = Hebart2023Accuracy()
-        ceiling = benchmark.ceiling
-        assert ceiling.sel(aggregation='center') == approx(.6844, abs=.0064)
+from brainio.stimuli import StimulusSet
+from brainscore_vision import benchmark_registry, load_benchmark
+from brainscore_vision.benchmarks.hebart2023 import Hebart2023Accuracy
+
+def test_benchmark_registry(benchmark):
+    assert benchmark in benchmark_registry
+
+def test_ceiling():
+    benchmark = Hebart2023Accuracy()
+    ceiling = benchmark.ceiling
+    assert ceiling.sel(aggregation='center') == approx(None, abs=None)

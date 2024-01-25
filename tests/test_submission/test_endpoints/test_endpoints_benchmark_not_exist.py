a04853a454c68939b82779762152aa3b2f6df8e5
samwinebrake <85908068+samwinebrake@users.noreply.github.com>
2024-01-25 10:26:47 -0500

    update branch w/ master changes (#519)
    * Clean up scoring action (#500)
    
    * check array for automerge-web label
    
    * upgrade get-github-email-by-username-action version
    
    * fix BS_UID reference
    
    * cleanup
    
    * typo
    
    * typo
    
    ---------
    
    Co-authored-by: Katherine Fairchild <kvfairchild@gmail.com>
    
    * add regnet to models (#501)
    
    Co-authored-by: AutoJenkins <brainscore@info.org>
    
    * Revert "brain-score.org (user:484) | Add new plugin(s): models: [regnet] " (#502)
    
    * Fix JSON handling in scoring `get_submitter_info` job (#504)
    
    * handle PLUGIN_INFO as JSON (not str)
    
    * nit
    
    ---------
    
    Co-authored-by: Katherine Fairchild <kvfairchild@gmail.com>
    
    * add regnet to models (#505)
    
    Co-authored-by: AutoJenkins <brainscore@info.org>
    
    * Revert "brain-score.org (user:484) | Add new plugin(s): models: [regnet] " (#506)
    
    * fix jq parsing in get_submitter_info and run_scoring (#507)
    
    Co-authored-by: Katherine Fairchild <kvfairchild@gmail.com>
    
    * add regnet to models (#508)
    
    Co-authored-by: AutoJenkins <brainscore@info.org>
    
    * Revert "brain-score.org (user:484) | Add new plugin(s): models: [regnet] " (#509)
    
    * Convert `BS_UID` to string and general cleanup of scoring pipeline  (#510)
    
    * cast BS_UID to string
    
    * only treat PLUGIN_INFO as JSON (not string)
    
    * cleanup
    
    * typo
    
    * Clarify check if scoring needed job name
    
    * curly brackets around needs & steps
    
    ---------
    
    Co-authored-by: Katherine Fairchild <kvfairchild@gmail.com>
    
    * add regnet to models (#511)
    
    Co-authored-by: AutoJenkins <brainscore@info.org>
    
    * Fix endpoint tests (#503)
    
    * fix benchmark identifier
    
    * split tests into separate files to test jenkins build
    
    * added pytest mock to test dependencies
    
    * testing separate classes in same test file
    
    * forgot to inherit from base class
    
    * adding teardown method, some other changes
    
    * refer to database_proxy
    
    * clear schema in teardown method
    
    * add back clear schema in setup class
    
    * Revert "added pytest mock to test dependencies"
    
    This reverts commit 320a0fd70a20e5628192d1442dff48d3d01e5d81.
    
    * reverting to old commit: 320a0fd70a20e5628192d1442dff48d3d01e5d81
    
    * move to subdirectory
    
    * change to absolute imports
    
    * pytest-mock didn't add
    
    * delete scores
    
    ---------
    
    Co-authored-by: Martin Schrimpf <m4rtinsch@gmail.com>
    
    ---------
    
    Co-authored-by: Katherine Fairchild <kvg0@mit.edu>
    Co-authored-by: Katherine Fairchild <kvfairchild@gmail.com>
    Co-authored-by: AutoJenkins <brainscore@info.org>
    Co-authored-by: Martin Schrimpf <m4rtinsch@gmail.com>

---
+++ tests/test_submission/test_endpoints/test_endpoints_benchmark_not_exist.py
@@ -0,0 +1,42 @@
+import logging
+
+import numpy as np
+import pytest
+from pytest import approx
+
+""" the mock import has to be before importing endpoints so that the database is properly mocked """
+from tests.test_submission.mock_config import test_database
+
+from brainscore_core.submission import database_models
+from brainscore_core.submission.database import connect_db
+from brainscore_core.submission.database_models import clear_schema
+from brainscore_vision.submission.endpoints import run_scoring
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.mark.private_access
+@pytest.mark.travis_slow
+class TestRunScoring:
+    @classmethod
+    def setup_class(cls):
+        logger.info('Connect to database')
+        connect_db(test_database)
+        clear_schema()
+
+    def setup_method(self):
+        logger.info('Initialize database entries')
+        database_models.User.create(id=1, email='test@brainscore.com', password='abcde',
+                                    is_active=True, is_staff=False, is_superuser=False, last_login='2022-10-14 9:25:00')
+
+    def teardown_method(self):
+        logger.info('Clean database')
+        clear_schema()
+
+    def test_benchmark_does_not_exist(self):
+        args_dict = {'jenkins_id': 62, 'user_id': 1, 'model_type': 'brainmodel',
+                     'public': True, 'competition': None, 'new_models': ['alexnet'],
+                     'new_benchmarks': ['idonotexist'], 'specified_only': True}
+        run_scoring(args_dict)
+        score_entries = database_models.Score.select()
+        assert len(score_entries) == 0

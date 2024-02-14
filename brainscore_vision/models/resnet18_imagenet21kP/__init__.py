48b1be9b365fc41b2029340d5777f556e6b3795a
Katherine Fairchild <kvg0@mit.edu>
2024-02-14 08:35:05 -0500

    add resnet18_imagenet21kP to models (#549)
    Co-authored-by: AutoJenkins <brainscore@info.org>

---
+++ brainscore_vision/models/resnet18_imagenet21kP/__init__.py
@@ -0,0 +1,6 @@
+from brainscore_vision import model_registry
+from .model import get_model
+
+MODEL_NAME = "resnet18_imagenet21kP"
+
+model_registry["resnet18_imagenet21kP-abdulkadir.gokce@epfl.ch"] = lambda: get_model(MODEL_NAME)

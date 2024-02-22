71ac5277bb9e57466c7ca46f395704e8e84137f6
AutoJenkins <brainscore@info.org>
2024-02-22 12:10:40 -0500

    add resnet50_eMMCR_eqp10_lm1 to models

--- brainscore_vision/models/resnet50_eMMCR_eqp10_lm1/model.py
+++ brainscore_vision/models/resnet50_eMMCR_eqp10_lm1/model.py
@@ -1,6 +1,7 @@
 from brainscore_vision.model_helpers.check_submission import check_models
 import functools
 import os
+from urllib.request import urlretrieve
 import torchvision.models
 from brainscore_vision.model_helpers.activations.pytorch import PytorchWrapper
 from brainscore_vision.model_helpers.activations.pytorch import load_preprocess_images
@@ -18,29 +19,48 @@ from brainscore_vision.model_helpers.check_submission import check_models
 
 
 def get_model_list():
-    return ['resnet50_eMMCR_eqp10_lm1']
+    return ["resnet50_eMMCR_eqp10_lm1V2"]
 
 
 def get_model(name):
-    assert name == 'resnet50_eMMCR_eqp10_lm1'
+    assert name == "resnet50_eMMCR_eqp10_lm1V2"
     model = torchvision.models.resnet50(pretrained=False)
-    ckpt_path = 'classifier.pt'
-    state_dict = torch.load(ckpt_path, map_location=torch.device('cpu'))
+    url = "https://users.flatironinstitute.org/~tyerxa/equi_proj/training_checkpoints/classifiers/equi_proj_10_lmda_1/classifier.pt"
+    fh = urlretrieve(url)
+    state_dict = torch.load(fh, map_location=torch.device("cpu"))
     model.load_state_dict(state_dict)
     preprocessing = functools.partial(load_preprocess_images, image_size=224)
-    wrapper = PytorchWrapper(identifier='resnet50', model=model, preprocessing=preprocessing)
+    wrapper = PytorchWrapper(
+        identifier="resnet50", model=model, preprocessing=preprocessing
+    )
     wrapper.image_size = 224
     return wrapper
 
 
 def get_layers(name):
-    assert name == 'resnet50_eMMCR_eqp10_lm1'
-    layers = ['layer1.0', 'layer1.1', 'layer1.2', 
-        'layer2.0', 'layer2.1', 'layer2.2', 'layer2.3',
-        'layer3.0', 'layer3.1', 'layer3.2', 'layer3.3',
-        'layer3.4', 'layer3.5', 'layer4.0', 'layer4.1', 'layer4.2','avgpool', 'fc']
+    assert name == "resnet50_eMMCR_eqp10_lm1V2"
+    layers = [
+        "layer1.0",
+        "layer1.1",
+        "layer1.2",
+        "layer2.0",
+        "layer2.1",
+        "layer2.2",
+        "layer2.3",
+        "layer3.0",
+        "layer3.1",
+        "layer3.2",
+        "layer3.3",
+        "layer3.4",
+        "layer3.5",
+        "layer4.0",
+        "layer4.1",
+        "layer4.2",
+        "avgpool",
+        "fc",
+    ]
 
-    outs = ['conv1','layer1', 'layer2', 'layer3', 'layer4']
+    outs = ["conv1", "layer1", "layer2", "layer3", "layer4"]
     return layers + outs
 
 
@@ -48,5 +68,5 @@ def get_bibtex(model_identifier):
     return """xx"""
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     check_models.check_base_models(__name__)

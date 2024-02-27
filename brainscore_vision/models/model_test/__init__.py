4883ba649dcd1381f4c4120c1417002868690e00
Sam Winebrake <85908068+samwinebrake@users.noreply.github.com>
2024-02-27 11:20:07 -0500

    Update __init__.py

--- brainscore_vision/models/model_test/__init__.py
+++ brainscore_vision/models/model_test/__init__.py
@@ -1,3 +1,4 @@
+# dummy change
 from brainscore_vision import model_registry
 from brainscore_vision.model_helpers.brain_transformation import ModelCommitment
 from .model import get_model, get_layers

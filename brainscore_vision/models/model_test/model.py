8807062c1d5a8566e420f4b2f9a99a5c07d0f50e
Sam Winebrake <85908068+samwinebrake@users.noreply.github.com>
2024-02-27 11:20:27 -0500

    Update model.py

--- brainscore_vision/models/model_test/model.py
+++ brainscore_vision/models/model_test/model.py
@@ -1,3 +1,4 @@
+# dummy change
 from brainscore_vision.model_helpers.check_submission import check_models
 import functools
 from brainscore_vision.model_helpers.activations.pytorch import PytorchWrapper

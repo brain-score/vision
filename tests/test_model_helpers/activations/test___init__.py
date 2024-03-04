453a2a6306114f39538a1a10ef4c128bb7a8d3c7
Ben Lonnqvist <ben.lonnqvist@epfl.ch>
2024-03-04 09:27:57 +0100

    rename test to be in line with current naming

--- tests/test_model_helpers/activations/test___init__.py
+++ tests/test_model_helpers/activations/test___init__.py
@@ -229,7 +229,7 @@ def test_require_variance_has_shift_coords(model_ctr, layers, image_name, number
 @pytest.mark.parametrize(["model_ctr", "layers"], models_layers)
 @pytest.mark.parametrize("require_variance", [False, True])
 @pytest.mark.parametrize("number_of_trials", [1, 2, 10])
-def test_model_requirements(model_ctr, layers, image_name, require_variance, number_of_trials):
+def test_require_variance_presentation_length(model_ctr, layers, image_name, require_variance, number_of_trials):
     stimulus_paths = [os.path.join(os.path.dirname(__file__), image_name)]
     activations_extractor = model_ctr()
     # when using microsaccades, the ModelCommitment sets its visual angle. Since this test skips the ModelCommitment,

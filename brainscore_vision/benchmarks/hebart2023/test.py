2eb41adf9ecbbde793f7de2fdb92f52d03272ef3
Linus Sommer <95619282+linus-md@users.noreply.github.com>
2024-03-05 16:51:48 +0100

    Update test.py

--- brainscore_vision/benchmarks/hebart2023/test.py
+++ brainscore_vision/benchmarks/hebart2023/test.py
@@ -1,18 +1,16 @@
 import pytest
 
 from brainscore_vision import benchmark_registry, load_model
-from brainscore_vision.benchmarks.hebart2023 import Hebart2023Match
 
-def test_benchmark_registry():
-    assert "Hebart2023-match" in benchmark_registry
-
+@pytest.mark.private_access
 def test_ceiling():
-    benchmark = Hebart2023Match()
+    benchmark = load_benchmark('Hebart2023-match')
     ceiling = benchmark.ceiling
     assert ceiling == pytest.approx(0.6767, abs=0.0001)
 
+@pytest.mark.private_access
 def test_alexnet_consistency():
-    benchmark = Hebart2023Match()
+    benchmark = load_benchmark('Hebart2023-match')
     benchmark.set_number_of_triplets(n=1000)
     model = load_model('alexnet')
     score = benchmark(model)

b3ad20ba6840e046af757de7bb9ce4e9c3c3dbde
Katherine Fairchild <kvg0@mit.edu>
2024-03-05 11:54:10 -0500

    Revert "add unet_ethan to models (#590)"
    This reverts commit 9a8774abdcd65e171f48d33a0cc7b20442d4def2.

--- brainscore_vision/models/unet_ethan/__init__.py
+++
@@ -1,5 +0,0 @@
-from brainscore_vision import model_registry
-from brainscore_vision.model_helpers.brain_transformation import ModelCommitment
-from .model import get_model, get_layers
-
-model_registry['alexnet'] = lambda: ModelCommitment(identifier='alexnet', activations_model=get_model('alexnet'), layers=get_layers('alexnet'))

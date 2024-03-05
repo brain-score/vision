bf99da3df595b95ce4025e5ca3e480c4ac47544b
Linus Sommer <95619282+linus-md@users.noreply.github.com>
2024-03-05 11:59:15 +0100

    Update test.py

--- brainscore_vision/benchmarks/hebart2023/test.py
+++ brainscore_vision/benchmarks/hebart2023/test.py
@@ -9,11 +9,11 @@ def test_benchmark_registry():
 def test_ceiling():
     benchmark = Hebart2023Match()
     ceiling = benchmark.ceiling
-    assert ceiling == pytest.approx(0.6844, abs=0.0001)
+    assert ceiling == pytest.approx(0.6767, abs=0.0001)
 
 def test_alexnet_consistency():
     benchmark = Hebart2023Match()
     benchmark.set_number_of_triplets(n=1000)
     model = load_model('alexnet')
     score = benchmark(model)
-    assert score == pytest.approx(0.38, abs=0.02)
+    assert score == pytest.approx(0.39, abs=0.02)

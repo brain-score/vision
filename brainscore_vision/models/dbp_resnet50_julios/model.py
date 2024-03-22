191419d7a68a731af6dfdab79b007725e291d768
deirdre-k <95875723+deirdre-k@users.noreply.github.com>
2024-03-22 16:52:07 -0400

    Update path import 2
    Co-authored-by: Martin Schrimpf <mschrimpf@users.noreply.github.com>

--- brainscore_vision/models/dbp_resnet50_julios/model.py
+++ brainscore_vision/models/dbp_resnet50_julios/model.py
@@ -30,7 +30,7 @@ def get_model(name):
         filename_version_sha=[(ckpt_name, 'HpyXY.W.WtS8zD.wJ7BVfvSbQDX.1Md4', '6efa18bfe4b70221d7e88ac2d2279d08042f23da')],
         save_directory=Path(__file__).parent
     )
-    ckpt_path = os.path.join(os.path.dirname(__file__), ckpt_name)
+    ckpt_path = Path(__file__).parent / ckpt_name
     model = torch.load(ckpt_path)    
     preprocessing = functools.partial(load_preprocess_images, image_size=224)
     print(preprocessing)

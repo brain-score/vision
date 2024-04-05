0fec013db91bd885a8d657de20763d53d3b2127c
AutoJenkins <brainscore@info.org>
2024-04-05 14:13:25 -0400

    add unet_again to models

---
+++ brainscore_vision/models/unet_again/__init__.py
@@ -0,0 +1,5 @@
+from brainscore_vision import model_registry
+from brainscore_vision.model_helpers.brain_transformation import ModelCommitment
+from .model import get_model, get_layers
+
+model_registry['alexnet_10'] = lambda: ModelCommitment(identifier='alexnet_10', activations_model=get_model('alexnet_10'), layers=get_layers('alexnet_10'))

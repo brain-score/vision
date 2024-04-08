d4301411587af79269975593eaea396ae10bc13b
YingtianDt <yingtian.tang@epfl.ch>
2024-04-08 14:18:40 +0200

    minor

--- brainscore_vision/model_helpers/activations/temporal/inputs/base.py
+++ brainscore_vision/model_helpers/activations/temporal/inputs/base.py
@@ -1,3 +1,7 @@
+from typing import Union
+from pathlib import Path
+
+
 class Stimulus:
     def from_path(self, path):
         raise NotImplementedError("Choose a concrete Stimulus type to use.")
@@ -8,7 +12,7 @@ class Stimulus:
         return extension in ['mp4', 'avi', 'mov', 'flv', 'wmv', 'webm', 'mkv', 'gif']
     
     @staticmethod
-    def is_image_path(path: Union[str, Path]) -> bool
+    def is_image_path(path: Union[str, Path]) -> bool:
         extension = path.split('.')[-1]
         return extension in ['jpg', 'jpeg', 'png', 'bmp', 'tiff']
         

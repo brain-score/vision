5b7a039416626e2d6384a712b5e2cab095504135
Mike Ferguson <mef8dd@virginia.edu>
2024-04-12 16:08:06 -0400

    added bibtex for transformer model

--- brainscore_vision/models/ViT_L_32_imagenet1k/model.py
+++ brainscore_vision/models/ViT_L_32_imagenet1k/model.py
@@ -24,3 +24,14 @@ def get_layers(name):
     name = name[4:]
     number_of_blocks = 12 if name.startswith('B') else 24
     return [f'transformer.blocks.{i}.pwff.fc2' for i in range(number_of_blocks)]
+
+
+def get_bibtex():
+    return """@inproceedings{
+                dosovitskiy2021an,
+                title={An Image is Worth 16x16 Words: Transformers for Image Recognition at Scale},
+                author={Alexey Dosovitskiy and Lucas Beyer and Alexander Kolesnikov and Dirk Weissenborn and Xiaohua Zhai and Thomas Unterthiner and Mostafa Dehghani and Matthias Minderer and Georg Heigold and Sylvain Gelly and Jakob Uszkoreit and Neil Houlsby},
+                booktitle={International Conference on Learning Representations},
+                year={2021},
+                url={https://openreview.net/forum?id=YicbFdNTTy}
+                }"""

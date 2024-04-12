b43cc76a1b0db74fe14b333b42ec2835556641a6
Mike Ferguson <mef8dd@virginia.edu>
2024-04-12 12:59:47 -0400

    added resnet-50-robust model

---
+++ brainscore_vision/models/resnet-50-robust/__init__.py
@@ -0,0 +1,7 @@
+from brainscore_vision import model_registry
+from brainscore_vision.model_helpers.brain_transformation import ModelCommitment
+from .model import get_model, get_layers
+
+model_registry['resnet-50-robust'] = lambda: ModelCommitment(identifier='resnet-50-robust',
+                                                               activations_model=get_model(),
+                                                               layers=get_layers('resnet-50-robust'))

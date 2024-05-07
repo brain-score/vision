4038480534e3981823e2bde12086a580f8fc3508
Michael Ferguson <mferg@mit.edu>
2024-05-07 11:04:57 -0400

    Update brainscore_vision/model_helpers/s3.py
    Co-authored-by: deirdre-k <95875723+deirdre-k@users.noreply.github.com>

--- brainscore_vision/model_helpers/s3.py
+++ brainscore_vision/model_helpers/s3.py
@@ -45,7 +45,7 @@ def load_file_from_s3(bucket: str, path: str, local_filepath: Union[Path, str],
 def load_weight_file(bucket: str, relative_path: str, version_id: str, sha1: str, folder_name: str = None) -> Path:
     """
     :param bucket: main bucket to add file to. Usually is 'brainscore_vision' for vision model weights.
-    :param relative_path: The path of the file inside the S3 bucket, relative to the `models/` directory.
+    :param relative_path: The path of the file inside the S3 bucket, relative to the `{folder_name}/` directory.
         The local path will be the same, relative to the local cache's `models/` directory (inside `BRAINSCORE_HOME`).
         Example: `alexnet/weights.pth` will download from brain-score S3:models/alexnet/weights.pth and
         and store into local ~/.brain-score/models/alexnet/weights.pth.

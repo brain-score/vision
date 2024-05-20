714f8e87e2a76db78ed51e64612da45f558c9ee4
Deirdre Kelliher <dmkelliher@gmail.com>
2024-05-20 11:22:22 -0400

    first pass: removing non-alexnet test params

--- tests/test_models.py
+++ tests/test_models.py
@@ -8,8 +8,10 @@ import brainscore_vision
 
 
 @pytest.mark.travis_slow
-@pytest.mark.parametrize('identifier',
-                         ['pixels', 'alexnet'])
+@pytest.mark.parametrize('identifier', [
+                                        # 'pixels', 
+                                        'alexnet'
+                                        ])
 def test_has_identifier(identifier):
     model = brainscore_vision.load_model(identifier)
     assert model.identifier == identifier

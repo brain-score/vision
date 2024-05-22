8b28319f7aa4b4347537b76a7d4e01bb6fd2937f
Mike Ferguson <mef8dd@virginia.edu>
2024-05-22 14:24:12 -0400

    Benchmark round 2

--- brainscore_vision/benchmarks/ferguson2024/__init__.py
+++ brainscore_vision/benchmarks/ferguson2024/__init__.py
@@ -1,24 +1,24 @@
 from brainscore_vision import benchmark_registry
-from benchmarks.ferguson2024.benchmark import Ferguson2024AlignmentMeasure
+from benchmarks.ferguson2024.benchmark import Ferguson2024ValueDelta
 
 DATASETS = ['circle_line', 'color', 'convergence', 'eighth',
             'gray_easy', 'gray_hard', 'half', 'juncture',
             'lle', 'llh', 'quarter', 'round_f',
             'round_v', 'tilted_line']
 
-benchmark_registry['Ferguson2024circle_line-alignment_measure'] = Ferguson2024AlignmentMeasure("circle_line")
-benchmark_registry['Ferguson2024color-alignment_measure'] = Ferguson2024AlignmentMeasure("color")
-benchmark_registry['Ferguson2024convergence-alignment_measure'] = Ferguson2024AlignmentMeasure("convergence")
-benchmark_registry['Ferguson2024eighth-alignment_measure'] = Ferguson2024AlignmentMeasure("eighth")
-benchmark_registry['Ferguson2024gray_easy-alignment_measure'] = Ferguson2024AlignmentMeasure("gray_easy")
-benchmark_registry['Ferguson2024gray_hard-alignment_measure'] = Ferguson2024AlignmentMeasure("gray_hard")
-benchmark_registry['Ferguson2024half-alignment_measure'] = Ferguson2024AlignmentMeasure("half")
-benchmark_registry['Ferguson2024juncture-alignment_measure'] = Ferguson2024AlignmentMeasure("juncture")
-benchmark_registry['Ferguson2024lle-alignment_measure'] = Ferguson2024AlignmentMeasure("lle")
-benchmark_registry['Ferguson2024llh-alignment_measure'] = Ferguson2024AlignmentMeasure("llh")
-benchmark_registry['Ferguson2024quarter-alignment_measure'] = Ferguson2024AlignmentMeasure("quarter")
-benchmark_registry['Ferguson2024round_f-alignment_measure'] = Ferguson2024AlignmentMeasure("round_f")
-benchmark_registry['Ferguson2024round_v-alignment_measure'] = Ferguson2024AlignmentMeasure("round_v")
-benchmark_registry['Ferguson2024tilted_line-alignment_measure'] = Ferguson2024AlignmentMeasure("tilted_line")
+benchmark_registry['Ferguson2024circle_line-value_delta'] = Ferguson2024ValueDelta("circle_line")
+benchmark_registry['Ferguson2024color-value_delta'] = Ferguson2024ValueDelta("color")
+benchmark_registry['Ferguson2024convergence-value_delta'] = Ferguson2024ValueDelta("convergence")
+benchmark_registry['Ferguson2024eighth-value_delta'] = Ferguson2024ValueDelta("eighth")
+benchmark_registry['Ferguson2024gray_easy-value_delta'] = Ferguson2024ValueDelta("gray_easy")
+benchmark_registry['Ferguson2024gray_hard-value_delta'] = Ferguson2024ValueDelta("gray_hard")
+benchmark_registry['Ferguson2024half-value_delta'] = Ferguson2024ValueDelta("half")
+benchmark_registry['Ferguson2024juncture-value_delta'] = Ferguson2024ValueDelta("juncture")
+benchmark_registry['Ferguson2024lle-value_delta'] = Ferguson2024ValueDelta("lle")
+benchmark_registry['Ferguson2024llh-value_delta'] = Ferguson2024ValueDelta("llh")
+benchmark_registry['Ferguson2024quarter-value_delta'] = Ferguson2024ValueDelta("quarter")
+benchmark_registry['Ferguson2024round_f-value_delta'] = Ferguson2024ValueDelta("round_f")
+benchmark_registry['Ferguson2024round_v-value_delta'] = Ferguson2024ValueDelta("round_v")
+benchmark_registry['Ferguson2024tilted_line-value_delta'] = Ferguson2024ValueDelta("tilted_line")
 
 
--- brainscore_vision/benchmarks/ferguson2024/benchmark.py
+++ brainscore_vision/benchmarks/ferguson2024/benchmark.py
@@ -1,13 +1,18 @@
 import numpy as np
+from brainscore_core import Metric
+from tqdm import tqdm
 
 from brainio.assemblies import walk_coords
 from brainscore_vision import load_dataset, load_metric, load_stimulus_set
-
+from brainio.assemblies import BehavioralAssembly
 from brainscore_vision.benchmark_helpers.screen import place_on_screen
 from brainscore_vision.benchmarks import BenchmarkBase
 from brainscore_vision.metrics import Score
+from brainscore_vision.metrics.value_delta import ValueDelta
 from brainscore_vision.model_interface import BrainModel
 from brainscore_vision.utils import LazyLoad
+import xarray as xr
+from .helpers.helpers import generate_summary_df, calculate_integral, HUMAN_INTEGRAL_ERRORS, LAPSE_RATES
 
 BIBTEX = """TBD"""
 
@@ -19,34 +24,102 @@ DATASETS = ['circle_line', 'color', 'convergence', 'eighth',
 for dataset in DATASETS:
     # behavioral benchmark
     identifier = f"Ferguson2024{dataset}AlignmentMeasure"
-    globals()[identifier] = lambda dataset=dataset: _Ferguson2024AlignmentMeasure(dataset)
+    globals()[identifier] = lambda dataset=dataset: _Ferguson2024ValueDelta(dataset)
 
 
-class _Ferguson2024AlignmentMeasure(BenchmarkBase):
-    def __init__(self, dataset):
-        self._metric = load_metric('error_consistency')
+class _Ferguson2024ValueDelta(BenchmarkBase):
+    def __init__(self, dataset, precompute_ceiling=False):
+        self._metric = ValueDelta(scale=0.75)  # 0.75 chosen after calibrating with ceiling
         self._fitting_stimuli = load_stimulus_set(f'Ferguson2024_{dataset}')
-        self._assembly = LazyLoad(lambda: load_dataset(f'Ferguson2024_{dataset}'))
+        self._assembly = load_dataset(f'Ferguson2024_{dataset}')
         self._visual_degrees = 8
         self._number_of_trials = 3
-        super(_Ferguson2024AlignmentMeasure, self).__init__(
+        self._ceiling = 0.0885 if precompute_ceiling is True else calculate_ceiling(dataset, self._assembly,
+                                                                                    self._metric, num_loops=500)
+        super(_Ferguson2024ValueDelta, self).__init__(
             identifier="Ferguson2024", version=2,
-            ceiling_func=lambda: self._metric.ceiling(self._assembly),
+            ceiling_func=self._ceiling,
             parent='behavior',
             bibtex=BIBTEX)
 
     def __call__(self, candidate: BrainModel) -> Score:
-        fitting_stimuli = place_on_screen(self._fitting_stimuli, target_visual_degrees=candidate.visual_degrees(),
-                                          source_visual_degrees=self._visual_degrees)
-        candidate.start_task(BrainModel.Task.probabilities, fitting_stimuli)
-        stimulus_set = place_on_screen(self._assembly.stimulus_set, target_visual_degrees=candidate.visual_degrees(),
-                                       source_visual_degrees=self._visual_degrees)
-        probabilities = candidate.look_at(stimulus_set, number_of_trials=self._number_of_trials)
-        score = self._metric(probabilities, self._assembly)
+        # fitting_stimuli = place_on_screen(self._fitting_stimuli, target_visual_degrees=candidate.visual_degrees(),
+        #                                   source_visual_degrees=self._visual_degrees)
+        # candidate.start_task(BrainModel.Task.probabilities, fitting_stimuli)
+        # stimulus_set = place_on_screen(self._assembly.stimulus_set, target_visual_degrees=candidate.visual_degrees(),
+        #                                source_visual_degrees=self._visual_degrees)
+        # probabilities = candidate.look_at(stimulus_set, number_of_trials=self._number_of_trials)
+
+        human_integral, human_integral_error = get_human_integral_data(self._assembly, dataset)
+        # model_integral = accuracy_metric(ground_truth, probabilities)
+        # score = self._metric(model_accuracy, human_accuracy)
         ceiling = self.ceiling
-        score = self.ceil_score(score, ceiling)
+        #score = self.ceil_score(1, ceiling)
         return score
 
 
-def Ferguson2024AlignmentMeasure(experiment):
-    return _Ferguson2024AlignmentMeasure(experiment)
+def Ferguson2024ValueDelta(experiment):
+    return _Ferguson2024ValueDelta(experiment)
+
+
+def calculate_ceiling(dataset: str, assembly: BehavioralAssembly, metric: Metric, num_loops: int) -> Score:
+    """
+    A Version of split-half reliability, in which the data is split randomly in half
+    and the metric is called on those two halves.
+
+    :param dataset: str, the prefix of the experiment subtype, ex: "tilted_line" or "lle"
+    :param assembly: the human behavioral data to look at
+    :param metric: of type Metric, used to calculate the score between two subjects
+    :return: Score object consisting of the score between two halfs of hman data
+    :param num_loops: int. number of times the score is calculated. Final score is the average of all of these.
+    """
+
+    scores = []
+    for i in tqdm(range(num_loops)):
+        half_1, half_2 = split_dataframe(assembly, seed=i)
+        half_1_score = get_human_integral_data(half_1, dataset)["human_integral"]
+        half_2_score = get_human_integral_data(half_2, dataset)["human_integral"]
+        score = metric(half_1_score, half_2_score)
+        scores.append(score)
+
+    score = Score(np.mean(scores))
+    scores = np.array(scores, dtype=float)
+    score.attrs['error'] = np.std(scores)
+    score.attrs[Score.RAW_VALUES_KEY] = [np.mean(scores), np.std(scores)]
+    return score
+
+
+def split_dataframe(df: BehavioralAssembly, seed: int) -> (BehavioralAssembly, BehavioralAssembly):
+    """
+    Takes in one DF and splits it into two, randomly, on the presentation dim
+
+    :param df: The DataFrame (assembly) to split
+    :param seed: a seed for the numpy rng
+    :return: Two DataFrames (assemblies)
+    """
+    if seed is not None:
+        np.random.seed(seed)
+    shuffled_indices = np.random.permutation(df.presentation.size)
+    half = len(shuffled_indices) // 2
+    indices_1 = shuffled_indices[:half]
+    indices_2 = shuffled_indices[half:]
+    dataarray_1 = df.isel(presentation=indices_1)
+    dataarray_2 = df.isel(presentation=indices_2)
+    return dataarray_1, dataarray_2
+
+
+def get_human_integral_data(assembly: BehavioralAssembly, dataset: str) -> (float, float):
+    """
+    Generates summary data for the experiment and calculates the human integral of delta line
+
+    :param assembly: the human behavioral data to look at
+    :param dataset: str, the prefix of the experiment subtype, ex: "tilted_line" or "lle"
+    :return: tuple of calculated human integral and its boostrapped (precomputed) error
+    """
+    lapse_rate = LAPSE_RATES[dataset]
+    blue_data = generate_summary_df(assembly, lapse_rate, "first")
+    orange_data = generate_summary_df(assembly, lapse_rate, "second")
+    human_integral = calculate_integral(blue_data, orange_data)
+    human_integral_error = HUMAN_INTEGRAL_ERRORS[dataset]
+
+    return dict(zip(["human_integral", "human_integral_error"], [human_integral, human_integral_error]))

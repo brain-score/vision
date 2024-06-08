0c339a70b7716c9133082da37f75debec7c2c2f9
Ben Lonnqvist <ben.lonnqvist@epfl.ch>
2024-06-08 15:58:59 +0200

    fix .nc name error in test

--- brainscore_vision/benchmarks/scialom2024/test.py
+++ brainscore_vision/benchmarks/scialom2024/test.py
@@ -143,6 +143,7 @@ class TestEngineering:
     ])
     def test_accuracy(self, dataset, expected_accuracy):
         benchmark = load_benchmark(f"Scialom2024_{dataset}EngineeringAccuracy")
+        nc_filename = dataset.upper() if dataset == 'rgb' else dataset
         filename = f"resnet50_julios_Scialom2024_{dataset}.nc"
         precomputed_features = Path(__file__).parent / filename
         s3.download_file_if_not_exists(precomputed_features,

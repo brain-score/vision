3bd9b385bbcf0aa01ec9e092ce38f629298136a2
Deirdre Kelliher <dmkelliher@gmail.com>
2024-06-12 14:30:44 -0400

    adding two models to be tested

--- brainscore_vision/models/alexnet_ks_torevert/__init__.py
+++ brainscore_vision/models/alexnet_ks_torevert/__init__.py
@@ -2,6 +2,7 @@ from brainscore_vision import model_registry
 from brainscore_vision.model_helpers.brain_transformation import ModelCommitment
 from .model import get_model, LAYERS
 
+
 model_registry['alexnet_ks_torevert'] = lambda: ModelCommitment(
     identifier='alexnet',
     activations_model=get_model(),

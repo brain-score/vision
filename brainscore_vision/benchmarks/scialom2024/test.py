b5eaef449a41a1d1ac05f6dc95761f316472d049
Ben Lonnqvist <ben.lonnqvist@epfl.ch>
2024-06-20 16:41:19 +0200

    Add composite benchmark to Scialom2024, and update AccuracyDistance (#929)
    * add composite accuracydistance
    
    * make accuracydistance more robust and interpretable
    
    * add explanation blurb about accuracydistance
    
    * rename variable to be more descriptive
    
    * update accuracy distance metric to be relative to floor and update expected scores accordingly
    
    * remove redundant line
    
    * remove redundant line
    
    * use precomputed features to compute correct expected scores
    
    * add correct test score
    
    * add comment to score computation

--- brainscore_vision/benchmarks/scialom2024/test.py
+++ brainscore_vision/benchmarks/scialom2024/test.py
@@ -32,7 +32,9 @@ from brainscore_vision.data_helpers import s3
     'Scialom2024_segments-77BehavioralAccuracyDistance',
     'Scialom2024_segments-100BehavioralAccuracyDistance',
     'Scialom2024_phosphenes-allBehavioralErrorConsistency',
-    'Scialom2024_segments-allBehavioralErrorConsistency'
+    'Scialom2024_segments-allBehavioralErrorConsistency',
+    'Scialom2024_phosphenes-allBehavioralAccuracyDistance',
+    'Scialom2024_segments-allBehavioralAccuracyDistance'
 ])
 def test_benchmark_registry(benchmark):
     assert benchmark in benchmark_registry
@@ -41,32 +43,34 @@ def test_benchmark_registry(benchmark):
 class TestBehavioral:
     @pytest.mark.private_access
     @pytest.mark.parametrize('dataset, expected_ceiling', [
-        ('rgb', approx(0.98513, abs=0.001)),
-        ('contours', approx(0.97848, abs=0.001)),
-        ('phosphenes-12', approx(0.95416, abs=0.001)),
-        ('phosphenes-16', approx(0.92583, abs=0.001)),
-        ('phosphenes-21', approx(0.92166, abs=0.001)),
-        ('phosphenes-27', approx(0.86888, abs=0.001)),
-        ('phosphenes-35', approx(0.87277, abs=0.001)),
-        ('phosphenes-46', approx(0.87125, abs=0.001)),
-        ('phosphenes-59', approx(0.87625, abs=0.001)),
-        ('phosphenes-77', approx(0.89277, abs=0.001)),
-        ('phosphenes-100', approx(0.89930, abs=0.001)),
-        ('segments-12', approx(0.89847, abs=0.001)),
-        ('segments-16', approx(0.89055, abs=0.001)),
-        ('segments-21', approx(0.88083, abs=0.001)),
-        ('segments-27', approx(0.87083, abs=0.001)),
-        ('segments-35', approx(0.86333, abs=0.001)),
-        ('segments-46', approx(0.90250, abs=0.001)),
-        ('segments-59', approx(0.87847, abs=0.001)),
-        ('segments-77', approx(0.89013, abs=0.001)),
-        ('segments-100', approx(0.93236, abs=0.001)),  # all of the above are AccuracyDistance
-        ('phosphenes-all', approx(0.45755, abs=0.01)),  # alls are ErrorConsistency
-        ('segments-all', approx(0.42529, abs=0.01)),
+        ('rgb', approx(0.98484, abs=0.001)),
+        ('contours', approx(0.97794, abs=0.001)),
+        ('phosphenes-12', approx(0.94213, abs=0.001)),
+        ('phosphenes-16', approx(0.90114, abs=0.001)),
+        ('phosphenes-21', approx(0.89134, abs=0.001)),
+        ('phosphenes-27', approx(0.77732, abs=0.001)),
+        ('phosphenes-35', approx(0.78466, abs=0.001)),
+        ('phosphenes-46', approx(0.78246, abs=0.001)),
+        ('phosphenes-59', approx(0.79745, abs=0.001)),
+        ('phosphenes-77', approx(0.83475, abs=0.001)),
+        ('phosphenes-100', approx(0.86073, abs=0.001)),
+        ('segments-12', approx(0.86167, abs=0.001)),
+        ('segments-16', approx(0.8202, abs=0.001)),
+        ('segments-21', approx(0.8079, abs=0.001)),
+        ('segments-27', approx(0.78282, abs=0.001)),
+        ('segments-35', approx(0.77724, abs=0.001)),
+        ('segments-46', approx(0.85391, abs=0.001)),
+        ('segments-59', approx(0.83410, abs=0.001)),
+        ('segments-77', approx(0.86227, abs=0.001)),
+        ('segments-100', approx(0.92517, abs=0.001)),  # all of the above are AccuracyDistance
+        ('phosphenes-allBehavioralErrorConsistency', approx(0.45755, abs=0.01)),
+        ('segments-allBehavioralErrorConsistency', approx(0.42529, abs=0.01)),
+        ('phosphenes-allBehavioralAccuracyDistance', approx(0.89533, abs=0.01)),
+        ('segments-allBehavioralAccuracyDistance', approx(0.89052, abs=0.01)),
     ])
     def test_dataset_ceiling(self, dataset, expected_ceiling):
         if 'all' in dataset:
-            benchmark = f"Scialom2024_{dataset}BehavioralErrorConsistency"
+            benchmark = f"Scialom2024_{dataset}"
         else:
             benchmark = f"Scialom2024_{dataset}BehavioralAccuracyDistance"
         benchmark = load_benchmark(benchmark)
@@ -75,26 +79,26 @@ class TestBehavioral:
 
     @pytest.mark.private_access
     @pytest.mark.parametrize('dataset, expected_raw_score', [
-        ('rgb', approx(0.92666, abs=0.001)),
-        ('contours', approx(0.26708, abs=0.001)),
-        ('phosphenes-12', approx(0.87666, abs=0.001)),
-        ('phosphenes-16', approx(0.83666, abs=0.001)),
-        ('phosphenes-21', approx(0.83166, abs=0.001)),
-        ('phosphenes-27', approx(0.73666, abs=0.001)),
-        ('phosphenes-35', approx(0.72416, abs=0.001)),
-        ('phosphenes-46', approx(0.59500, abs=0.001)),
-        ('phosphenes-59', approx(0.50666, abs=0.001)),
-        ('phosphenes-77', approx(0.42083, abs=0.001)),
-        ('phosphenes-100', approx(0.33166, abs=0.001)),
-        ('segments-12', approx(0.81500, abs=0.001)),
-        ('segments-16', approx(0.73750, abs=0.001)),
-        ('segments-21', approx(0.69666, abs=0.001)),
-        ('segments-27', approx(0.59500, abs=0.001)),
-        ('segments-35', approx(0.52666, abs=0.001)),
-        ('segments-46', approx(0.42166, abs=0.001)),
-        ('segments-59', approx(0.34583, abs=0.001)),
-        ('segments-77', approx(0.28916, abs=0.001)),
-        ('segments-100', approx(0.19750, abs=0.001)),  # all of the above are AccuracyDistance
+        ('rgb', approx(0.92616, abs=0.001)),
+        ('contours', approx(0.25445, abs=0.001)),
+        ('phosphenes-12', approx(0.84177, abs=0.001)),
+        ('phosphenes-16', approx(0.77513, abs=0.001)),
+        ('phosphenes-21', approx(0.76437, abs=0.001)),
+        ('phosphenes-27', approx(0.56895, abs=0.001)),
+        ('phosphenes-35', approx(0.52008, abs=0.001)),
+        ('phosphenes-46', approx(0.29478, abs=0.001)),
+        ('phosphenes-59', approx(0.19022, abs=0.001)),
+        ('phosphenes-77', approx(0.13569, abs=0.001)),
+        ('phosphenes-100', approx(0.11234, abs=0.001)),
+        ('segments-12', approx(0.72937, abs=0.001)),
+        ('segments-16', approx(0.57043, abs=0.001)),
+        ('segments-21', approx(0.49300, abs=0.001)),
+        ('segments-27', approx(0.30014, abs=0.001)),
+        ('segments-35', approx(0.22442, abs=0.001)),
+        ('segments-46', approx(0.14312, abs=0.001)),
+        ('segments-59', approx(0.12072, abs=0.001)),
+        ('segments-77', approx(0.12996, abs=0.001)),
+        ('segments-100', approx(0.11540, abs=0.001)),  # all of the above are AccuracyDistance
         ('phosphenes-all', approx(0.18057, abs=0.01)),  # alls are ErrorConsistency
         ('segments-all', approx(0.15181, abs=0.01)),
     ])

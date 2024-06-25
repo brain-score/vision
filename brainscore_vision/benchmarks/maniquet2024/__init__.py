9d16271fd1ffad607928e0201cfe4550fca832e8
Mike Ferguson <mef8dd@virginia.edu>
2024-06-25 18:17:32 -0400

    use relative import for init file

--- brainscore_vision/benchmarks/maniquet2024/__init__.py
+++ brainscore_vision/benchmarks/maniquet2024/__init__.py
@@ -1,5 +1,5 @@
 from brainscore_vision import benchmark_registry
-from benchmark import Maniquet2024ConfusionSimilarity
+from .benchmark import Maniquet2024ConfusionSimilarity
 
 benchmark_registry['Maniquet2024ConfusionSimilarity'] = lambda: Maniquet2024ConfusionSimilarity()
 

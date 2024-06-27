edfd1bc5e316b3adc63fc924b23feabf508765e4
Mike Ferguson <mef8dd@virginia.edu>
2024-06-27 13:35:56 -0400

    add decoder and generate first model scores

--- brainscore_vision/benchmarks/ferguson2024/benchmark.py
+++ brainscore_vision/benchmarks/ferguson2024/benchmark.py
@@ -3,6 +3,7 @@ from brainscore_core import Metric
 from brainio.stimuli import StimulusSet
 from tqdm import tqdm
 from typing import Dict
+import xarray as xr
 
 from brainscore_vision import load_dataset, load_stimulus_set
 from brainio.assemblies import BehavioralAssembly
@@ -35,7 +36,7 @@ class _Ferguson2024ValueDelta(BenchmarkBase):
         self._experiment = experiment
         self._precompute_ceiling = precompute_ceiling
         self._metric = ValueDelta(scale=0.75)  # 0.75 chosen after calibrating with ceiling
-        self._fitting_stimuli = gather_fitting_stimuli()
+        self._fitting_stimuli = gather_fitting_stimuli(combine_all=False, experiment=self._experiment)
         self._assembly = load_dataset(f'Ferguson2024_{self._experiment}')
         self._visual_degrees = 8
         self._number_of_trials = 3
@@ -49,15 +50,17 @@ class _Ferguson2024ValueDelta(BenchmarkBase):
         self._assembly.stimulus_set["image_label"] = np.where(self._assembly.stimulus_set["image_number"] % 2 == 0, "oddball", "same")
         self._fitting_stimuli["image_label"] = np.where(self._fitting_stimuli["image_number"] % 2 == 0, "oddball", "same")
 
+        # fit logistic binary decoder and perform task:
         fitting_stimuli = place_on_screen(self._fitting_stimuli, target_visual_degrees=candidate.visual_degrees(),
                                            source_visual_degrees=self._visual_degrees)
-        # candidate.start_task(BrainModel.Task.probabilities, fitting_stimuli)
-        # stimulus_set = place_on_screen(self._assembly.stimulus_set, target_visual_degrees=candidate.visual_degrees(),
-        #                                source_visual_degrees=self._visual_degrees)
-        # probabilities = candidate.look_at(stimulus_set, number_of_trials=self._number_of_trials)
+        candidate.start_task(BrainModel.Task.probabilities, fitting_stimuli)
+        stimulus_set = place_on_screen(self._assembly.stimulus_set, target_visual_degrees=candidate.visual_degrees(),
+                                       source_visual_degrees=self._visual_degrees)
+        model_labels_raw = candidate.look_at(stimulus_set, number_of_trials=self._number_of_trials)
+        model_labels = proces_model_choices(model_labels_raw)
 
         human_integral = get_integral_data(self._assembly, self._experiment)['integral']
-        model_integral = -1.67
+        model_integral = get_integral_data(model_labels, self._experiment)['integral']
         raw_score = self._metric(model_integral, human_integral)
         ceiling = self._ceiling
         score = Score(min(max(raw_score / ceiling, 0), 1))  # ensure ceiled score is between 0 and 1
@@ -145,3 +148,55 @@ def gather_fitting_stimuli(combine_all=True, experiment="") -> StimulusSet:
         return merged_stimulus_set
     else:
         return load_stimulus_set(f"Ferguson2024_{experiment}_training_stimuli")
+
+
+def proces_model_choices(raw_model_labels: BehavioralAssembly) -> BehavioralAssembly:
+    """
+    Takes in a raw Assembly and applies a softmax and threshold to get a string label for a class. Also
+    builds the model's assembly to resemble a humans by adding fields (trial_type, num_distractors, etc)
+
+    :param raw_model_labels: a BehavioralAssembly that has two raw values corresponding to class choices
+    :return: new assembly with an added dim, "model_choice" based on the raw values
+    """
+    distractor_mapping = {
+        1.0: [0, 1, 6, 7, 12, 13, 18, 19],
+        5.0: [2, 3, 8, 9, 14, 15, 20, 21]
+    }
+    distractor_lookup = {image_num: str(distractor) for distractor, images in distractor_mapping.items() for image_num
+                         in images}
+
+    def num_distractors(image_num):
+        return distractor_lookup.get(image_num, "11.0")  # default to 11.0 if not found, which should never happen
+
+    def softmax(x):
+        e_x = np.exp(x - np.max(x, axis=1, keepdims=True))
+        return e_x / e_x.sum(axis=1, keepdims=True)
+
+    def is_even(image_num):
+        return 1 if image_num % 2 == 0 else 0
+
+    def is_equal(label, image_type):
+        return 1 if label == image_type else 0
+
+    softmax_values = softmax(raw_model_labels.values)
+    labels = np.where(softmax_values[:, 0] > 0.5, 1, 0)
+    labels_string = np.where(softmax_values[:, 0] > 0.5, "oddball", "same")
+    model_choices = xr.DataArray(labels, dims=["presentation"], coords={"presentation": raw_model_labels.coords["presentation"]})
+    model_choices = model_choices.assign_coords(trial_type=('presentation', np.array(['normal'] * 48)))
+    model_choices = model_choices.assign_coords(participant_id=('presentation', np.array(['model'] * 48)))
+    distractor_nums = [num_distractors(image_num) for image_num in model_choices.coords['image_number'].values]
+    model_choices = model_choices.assign_coords(distractor_nums=('presentation', distractor_nums))
+    target_present = [is_even(image_num) for image_num in model_choices.coords['image_number'].values]
+    model_choices = model_choices.assign_coords(target_present=('presentation', target_present))
+    model_choices = model_choices.assign_coords(labels_string=('presentation', labels_string))
+    correct = [
+        is_equal(label, image_type)
+        for label, image_type in zip(
+            model_choices.coords['labels_string'].values,
+            model_choices.coords['image_label'].values
+        )
+    ]
+    model_choices = model_choices.assign_coords(correct=('presentation', correct))
+    return BehavioralAssembly(model_choices)
+
+

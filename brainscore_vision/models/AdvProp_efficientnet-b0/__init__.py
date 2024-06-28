812220d71aca4e5cc71daa98e4297fb4a62fc6aa
SusanWYS <susanw03@outlook.com>
2024-06-28 14:20:17 -0400

    “Advpropeffb0”

---
+++ brainscore_vision/models/AdvProp_efficientnet-b0/__init__.py
@@ -0,0 +1,5 @@
+from brainscore_vision import model_registry
+from brainscore_vision.model_helpers.brain_transformation import ModelCommitment
+from .model import get_model, get_layers
+
+model_registry['AdvProp_efficientnet-b0'] = lambda: ModelCommitment(identifier='AdvProp_efficientnet-b0', activations_model=get_model('AdvProp_efficientnet-b0'), layers=get_layers('AdvProp_efficientnet-b0'))

452ce233992eb76772bc0e37942d56932e0e9bee
pellegreene <36171165+pellegreene@users.noreply.github.com>
2024-07-02 10:49:38 -0400

    Ep/add inception v3 pytorch (#944)
    * Add inception_v3_pytorch
    
    * Update model.py
    
    * Update model.py
    
    ---------
    
    Co-authored-by: Ethan Pellegrini <epellegr@mit.edu>
    Co-authored-by: deirdre-k <95875723+deirdre-k@users.noreply.github.com>

---
+++ brainscore_vision/models/inception_v3_pytorch/test.py
@@ -0,0 +1,8 @@
+import pytest
+import brainscore_vision
+
+
+@pytest.mark.travis_slow
+def test_has_identifier():
+    model = brainscore_vision.load_model('inception_v3_pytorch')
+    assert model.identifier == 'inception_v3_pytorch'

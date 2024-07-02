275ad76724ec7fb4d9648fd9909924a1bc4e195b
David Coggan <ddcoggan@icloud.com>
2024-07-02 11:17:16 -0500

    Update brainscore_vision/benchmarks/coggan2024_fMRI/__init__.py
    Co-authored-by: Martin Schrimpf <mschrimpf@users.noreply.github.com>

--- brainscore_vision/benchmarks/coggan2024_fMRI/__init__.py
+++ brainscore_vision/benchmarks/coggan2024_fMRI/__init__.py
@@ -8,8 +8,8 @@ from .benchmark import (
     Coggan2024_IT,
 )
 
-benchmark_registry['Coggan2024_fMRI.V1'] = Coggan2024_V1
-benchmark_registry['Coggan2024_fMRI.V2'] = Coggan2024_V2
-benchmark_registry['Coggan2024_fMRI.V4'] = Coggan2024_V4
-benchmark_registry['Coggan2024_fMRI.IT'] = Coggan2024_IT
+benchmark_registry['Coggan2024_fMRI.V1-rdm'] = Coggan2024_V1
+benchmark_registry['Coggan2024_fMRI.V2-rdm'] = Coggan2024_V2
+benchmark_registry['Coggan2024_fMRI.V4-rdm'] = Coggan2024_V4
+benchmark_registry['Coggan2024_fMRI.IT-rdm'] = Coggan2024_IT
 

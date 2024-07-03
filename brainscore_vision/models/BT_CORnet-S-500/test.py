ceada03d154f0d930025b1f0442b58d9d533b865
SusanWYS <susanw03@outlook.com>
2024-07-03 16:14:08 -0400

    A

--- brainscore_vision/models/BT_CORnet-S-500/test.py
+++
@@ -1,7 +0,0 @@
-import pytest
-import brainscore_vision
-
-@pytest.mark.travis_slow
-def test_has_identifier():
-    model = brainscore_vision.load_model('BT_CORnet-S-500')
-    assert model.identifier == 'BT_CORnet-S-500'

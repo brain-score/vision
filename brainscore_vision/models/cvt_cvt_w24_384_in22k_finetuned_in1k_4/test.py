392fe357c94c69ba4bd936ab91dfeb33a29f89cd
pellegreene <36171165+pellegreene@users.noreply.github.com>
2024-07-03 10:51:47 -0400

    Revert "Add cvt_cvt-w24-384-in22k_finetuned-in1k_4 (#939)"
    This reverts commit fa4c774643c696d23c53c96c0d4292af463f726b.

--- brainscore_vision/models/cvt_cvt_w24_384_in22k_finetuned_in1k_4/test.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import brainscore_vision
-
-
-@pytest.mark.travis_slow
-def test_has_identifier():
-    model = brainscore_vision.load_model('cvt_cvt-w24-384-in22k_finetuned-in1k_4')
-    assert model.identifier == 'cvt_cvt-w24-384-in22k_finetuned-in1k_4'

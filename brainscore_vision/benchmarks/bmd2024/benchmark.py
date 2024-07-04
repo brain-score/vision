d41752b8f64853d951c69e81076bb6ddea82d446
Martin Schrimpf <m4rtinsch@gmail.com>
2024-07-04 14:16:13 +0200

    place stimuli on screen

--- brainscore_vision/benchmarks/bmd2024/benchmark.py
+++ brainscore_vision/benchmarks/bmd2024/benchmark.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from brainio.assemblies import BehavioralAssembly
 from brainscore_vision import load_dataset, load_metric
+from brainscore_vision.benchmark_helpers.screen import place_on_screen
 from brainscore_vision.benchmarks import BenchmarkBase
 from brainscore_vision.metrics import Score
 from brainscore_vision.model_interface import BrainModel
@@ -25,8 +26,11 @@ class _BMD_2024_BehavioralAccuracyDistance(BenchmarkBase):
         choice_labels = set(self._assembly.stimulus_set['truth'].values)
         choice_labels = list(sorted(choice_labels))
         candidate.start_task(BrainModel.Task.label, choice_labels)
-        labels = candidate.look_at(self._assembly.stimulus_set, number_of_trials=1)
-        raw_score = self._metric(labels, target=self._assembly)
+        stimulus_set = place_on_screen(self._assembly.stimulus_set,
+                                       target_visual_degrees=candidate.visual_degrees(),
+                                       source_visual_degrees=self._visual_degrees)
+        labels = candidate.look_at(stimulus_set, number_of_trials=1)
+        raw_score = self._metric(labels, self._assembly)
         ceiling = self.ceiling
         score = raw_score / ceiling
         # ensure score <= 1.0

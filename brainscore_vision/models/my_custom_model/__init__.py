6aaaa9d754407df3f9505b4e153828891df27bab
AutoJenkins <brainscore@info.org>
2024-07-05 11:23:52 -0400

    add my_custom_model to models

--- brainscore_vision/models/my_custom_model/__init__.py
+++ brainscore_vision/models/my_custom_model/__init__.py
@@ -2,4 +2,4 @@ from brainscore_vision import model_registry
 from brainscore_vision.model_helpers.brain_transformation import ModelCommitment
 from .model import get_model, get_layers
 
-model_registry['fast_2px_step2_eps2_repeat1_trial1_model_best'] = lambda: ModelCommitment(identifier='fast_2px_step2_eps2_repeat1_trial1_model_best', activations_model=get_model('fast_2px_step2_eps2_repeat1_trial1_model_best'), layers=get_layers('fast_2px_step2_eps2_repeat1_trial1_model_best'))
+model_registry['Soumyadeep_inf_1'] = lambda: ModelCommitment(identifier='Soumyadeep_inf_1', activations_model=get_model('Soumyadeep_inf_1'), layers=get_layers('Soumyadeep_inf_1'))

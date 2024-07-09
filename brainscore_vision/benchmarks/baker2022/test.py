42c31574ae69eae1792949591cec8427b52581af
Martin Schrimpf <mschrimpf@users.noreply.github.com>
2024-07-09 14:05:02 +0200

    fix identifiers (previously duplicate) and score object (#1020)

--- brainscore_vision/benchmarks/baker2022/test.py
+++ brainscore_vision/benchmarks/baker2022/test.py
@@ -16,19 +16,21 @@ class TestBaker2022:
         assert len(DATASETS) == 2
 
     # ensure the three benchmarks themselves are there
-    @pytest.mark.parametrize('benchmark', [
-        'Baker2022-inverted_accuracy_delta',
-        'Baker2022-accuracy_delta_fragmented',
-        'Baker2022-inverted_accuracy_delta'
+    @pytest.mark.parametrize('identifier', [
+        'Baker2022inverted-accuracy_delta',
+        'Baker2022fragmented-accuracy_delta',
+        'Baker2022frankenstein-accuracy_delta',
     ])
-    def test_in_pool(self, benchmark):
-        assert benchmark in benchmark_registry
+    def test_identifier(self, identifier):
+        assert identifier in benchmark_registry
+        benchmark = load_benchmark(identifier=identifier)
+        assert benchmark.identifier == identifier
 
     # Test expected ceiling
     @pytest.mark.parametrize('benchmark, expected_ceiling', [
-        ('Baker2022-accuracy_delta_frankenstein', 0.8498),
-        ('Baker2022-accuracy_delta_fragmented', 0.9385),
-        ('Baker2022-inverted_accuracy_delta', 0.6538),
+        ('Baker2022frankenstein-accuracy_delta', 0.8498),
+        ('Baker2022fragmented-accuracy_delta', 0.9385),
+        ('Baker2022inverted-accuracy_delta', 0.6538),
     ])
     def test_benchmark_ceiling(self, benchmark, expected_ceiling):
         benchmark = load_benchmark(benchmark)
@@ -42,12 +44,12 @@ class TestBaker2022:
 
     # Test raw scores
     @pytest.mark.parametrize('benchmark, model, expected_raw_score', [
-        ('Baker2022-accuracy_delta_frankenstein', 'resnet-50-pytorch', approx(0.2847, abs=0.0001)),
-        ('Baker2022-accuracy_delta_fragmented', 'resnet-50-pytorch', approx(0.8452, abs=0.0001)),
-        ('Baker2022-inverted_accuracy_delta', 'resnet-50-pytorch', approx(0.0, abs=0.0001)),
-        ('Baker2022-accuracy_delta_frankenstein', 'resnet50-SIN', approx(0.6823, abs=0.0001)),
-        ('Baker2022-accuracy_delta_fragmented', 'resnet50-SIN', approx(0.9100, abs=0.0001)),
-        ('Baker2022-inverted_accuracy_delta', 'resnet50-SIN', approx(0.7050, abs=0.0001)),
+        ('Baker2022frankenstein-accuracy_delta', 'resnet-50-pytorch', approx(0.2847, abs=0.0001)),
+        ('Baker2022fragmented-accuracy_delta', 'resnet-50-pytorch', approx(0.8452, abs=0.0001)),
+        ('Baker2022inverted-accuracy_delta', 'resnet-50-pytorch', approx(0.0, abs=0.0001)),
+        ('Baker2022frankenstein-accuracy_delta', 'resnet50-SIN', approx(0.6823, abs=0.0001)),
+        ('Baker2022fragmented-accuracy_delta', 'resnet50-SIN', approx(0.9100, abs=0.0001)),
+        ('Baker2022inverted-accuracy_delta', 'resnet50-SIN', approx(0.7050, abs=0.0001)),
     ])
     def test_model_raw_score(self, benchmark, model, expected_raw_score):
 
@@ -63,17 +65,17 @@ class TestBaker2022:
         raw_score = score.raw
 
         # division by ceiling <= 1 should result in higher score
-        assert score.sel(aggregation='center') >= raw_score.sel(aggregation='center')
-        assert raw_score.sel(aggregation='center') == expected_raw_score
+        assert score >= raw_score
+        assert raw_score == expected_raw_score
 
     # test ceiled score
     @pytest.mark.parametrize('benchmark, model, expected_ceiled_score', [
-        ('Baker2022-accuracy_delta_frankenstein', 'resnet-50-pytorch', approx(0.3350, abs=0.0001)),
-        ('Baker2022-accuracy_delta_fragmented', 'resnet-50-pytorch', approx(0.9005, abs=0.0001)),
-        ('Baker2022-inverted_accuracy_delta', 'resnet-50-pytorch', approx(0.0, abs=0.0001)),
-        ('Baker2022-accuracy_delta_frankenstein', 'resnet50-SIN', approx(0.8029, abs=0.0001)),
-        ('Baker2022-accuracy_delta_fragmented', 'resnet50-SIN', approx(0.9696, abs=0.0001)),
-        ('Baker2022-inverted_accuracy_delta', 'resnet50-SIN', approx(1.000, abs=0.0001)),
+        ('Baker2022frankenstein-accuracy_delta', 'resnet-50-pytorch', approx(0.3350, abs=0.0001)),
+        ('Baker2022fragmented-accuracy_delta', 'resnet-50-pytorch', approx(0.9005, abs=0.0001)),
+        ('Baker2022inverted-accuracy_delta', 'resnet-50-pytorch', approx(0.0, abs=0.0001)),
+        ('Baker2022frankenstein-accuracy_delta', 'resnet50-SIN', approx(0.8029, abs=0.0001)),
+        ('Baker2022fragmented-accuracy_delta', 'resnet50-SIN', approx(0.9696, abs=0.0001)),
+        ('Baker2022inverted-accuracy_delta', 'resnet50-SIN', approx(1.000, abs=0.0001)),
     ])
     def test_model_ceiled_score(self, benchmark, model, expected_ceiled_score):
         benchmark_object = load_benchmark(benchmark)
@@ -85,4 +87,4 @@ class TestBaker2022:
         precomputed_features = BehavioralAssembly.from_files(file_path=precomputed_features)
         precomputed_features = PrecomputedFeatures(precomputed_features, visual_degrees=8)
         score = benchmark_object(precomputed_features)
-        assert score.sel(aggregation='center') == expected_ceiled_score
+        assert score == expected_ceiled_score

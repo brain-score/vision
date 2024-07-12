df751a4759e2a17ce97ce481871d67f75add3f37
Martin Schrimpf <m4rtinsch@gmail.com>
2024-07-12 09:13:47 +0200

    fix superfluous apostrophe in benchmark identifier

--- brainscore_vision/benchmarks/maniquet2024/test.py
+++ brainscore_vision/benchmarks/maniquet2024/test.py
@@ -9,9 +9,12 @@ Created on Mon Jun 24 17:22:59 2024
 import pytest
 from brainscore_vision import load_benchmark
 
-@pytest.mark.parametrize('benchmark', [
+
+@pytest.mark.parametrize("identifier", [
     'Maniquet2024-confusion_similarity',
     'Maniquet2024-tasks_consistency',
 ])
-def test_benchmark_registry(benchmark):
-    assert load_benchmark(benchmark) is not None
+def test_benchmark_registry(identifier):
+    benchmark = load_benchmark(identifier)
+    assert benchmark is not None
+    assert benchmark.identifier == identifier

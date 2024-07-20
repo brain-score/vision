85d60c46dcdec6f7c796b4d1f5017f76ee274566
Khaled Jedoui <thekej@stanford.edu>
2024-07-20 00:33:07 -0700

    add intra scenario bench

--- brainscore_vision/benchmarks/physion_engineering_2024/__init__.py
+++ brainscore_vision/benchmarks/physion_engineering_2024/__init__.py
@@ -2,7 +2,11 @@ from brainscore_vision import benchmark_registry
 from .benchmark import PhysionGlobalPredictionAccuracy
 from .benchmark import PhysionGlobalDetectionAccuracy
 from .benchmark import PhysionSnippetDetectionAccuracy
+from .benchmark import PhysionGlobalPredictionIntraScenarioAccuracy
+from .benchmark import PhysionGlobalDetectionIntraScenarioAccuracy
 
 benchmark_registry['Physionv1.5-ocd'] = PhysionGlobalDetectionAccuracy
 benchmark_registry['Physionv1.5-ocp'] = PhysionGlobalPredictionAccuracy
 benchmark_registry['Physionv1.5-snippet-rollout-performance'] = PhysionSnippetDetectionAccuracy
+benchmark_registry['Physionv1.5-ocp-intra-generalization'] = PhysionGlobalPredictionIntraScenarioAccuracy
+benchmark_registry['Physionv1.5-ocd-intra-generalization'] = PhysionGlobalDetectionIntraScenarioAccuracy

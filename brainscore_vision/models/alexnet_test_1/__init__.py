b19a71c6572f2db0ece6e540aa18f6c5e0f83469
AutoJenkins <brainscore@info.org>
2024-07-30 13:12:50 -0400

    add alexnet_test_1 to models

---
+++ brainscore_vision/models/alexnet_test_1/__init__.py
@@ -0,0 +1,8 @@
+from brainscore_vision import model_registry
+from brainscore_vision.model_helpers.brain_transformation import ModelCommitment
+from .model import get_model, LAYERS
+
+model_registry['alexnet'] = lambda: ModelCommitment(
+    identifier='alexnet',
+    activations_model=get_model(),
+    layers=LAYERS)

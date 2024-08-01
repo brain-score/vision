a0a807205f26b1a1fd6d8c605d77c079d4ed4819
SusanWYS <susanw03@outlook.com>
2024-08-01 17:37:31 -0400

    deleted expired files

--- brainscore_vision/models/resnet_101_v2/test.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import brainscore_vision
-
-
-@pytest.mark.travis_slow
-def test_has_identifier():
-    model = brainscore_vision.load_model('resnet-101_v2')
-    assert model.identifier == 'resnet-101_v2'

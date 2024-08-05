bb71cde28c4240c27d1cccf03a90ffb9bd3fe759
SusanWYS <susanw03@outlook.com>
2024-08-05 18:14:06 -0400

    a

---
+++ brainscore_vision/models/mobilenet_v2_1_0_224/model.py
@@ -0,0 +1,54 @@
+import functools
+from brainscore_vision.model_helpers.activations.pytorch import load_preprocess_images
+from brainscore_vision.model_helpers.activations.pytorch import PytorchWrapper
+from brainscore_vision.model_helpers.check_submission import check_models
+from transformers import MobileNetV2ForImageClassification
+
+
+model = MobileNetV2ForImageClassification.from_pretrained("google/mobilenet_v2_1.0_224")
+def get_model(name):
+    """
+    This method fetches an instance of a base model. The instance has to be callable and return a xarray object,
+    containing activations. There exist standard wrapper implementations for common libraries, like pytorch and
+    keras. Checkout the examples folder, to see more. For custom implementations check out the implementation of the
+    wrappers.
+    :param name: the name of the model to fetch
+    :return: the model instance
+    """
+    assert name == 'mobilenet_v2_1_0_224'
+    preprocessing = functools.partial(load_preprocess_images, image_size=224)
+    wrapper = PytorchWrapper(identifier='name', model=model, preprocessing=preprocessing)
+    wrapper.image_size = 224
+    return wrapper
+
+
+def get_layers(name):
+    assert name == 'mobilenet_v2_1_0_224'
+    layer_names = []
+    for name, module in model.named_modules():
+        layer_names.append(name)
+
+    return layer_names[-50:]
+
+
+def get_bibtex(name):
+    """
+    A method returning the bibtex reference of the requested model as a string.
+    """
+    return '''
+@article{DBLP:journals/corr/ZagoruykoK16,
+@misc{chollet2017xception,
+      title={Xception: Deep Learning with Depthwise Separable Convolutions}, 
+      author={François Chollet},
+      year={2017},
+      eprint={1610.02357},
+      archivePrefix={arXiv},
+      primaryClass={cs.CV}
+}
+'''
+
+
+if __name__ == '__main__':
+    # Use this method to ensure the correctness of the BaseModel implementations.
+    # It executes a mock run of brain-score benchmarks.
+    check_models.check_base_models(__name__)

0fb026fa1799734a9f51d5a352698bb0f91ecdf4
deirdre-k <95875723+deirdre-k@users.noreply.github.com>
2024-08-06 12:44:44 -0400

    Removing dash from python module name (#1118)

--- brainscore_vision/models/temporal_model_AVID_CMA/model.py
+++ brainscore_vision/models/temporal_model_AVID_CMA/model.py
@@ -20,7 +20,7 @@ def get_model(identifier):
         cfg_path = os.path.join(HOME, "configs/main/avid-cma/kinetics/InstX-N1024-PosW-N64-Top32.yaml")
         weight_path = load_weight_file(
             bucket="brainscore-vision",
-            relative_path="temporal_model_AVID-CMA/AVID-CMA_Kinetics_InstX-N1024-PosW-N64-Top32_checkpoint.pth.tar",
+            relative_path="temporal_model_AVID_CMA/AVID-CMA_Kinetics_InstX-N1024-PosW-N64-Top32_checkpoint.pth.tar",
             version_id="yx9Pbq3SuNOOd4sX7csTolaHD1iTCx8y",
             sha1="6efe4464ca654a56affff766acf24e89e6f3ffbf"
         )
@@ -29,7 +29,7 @@ def get_model(identifier):
         cfg_path = os.path.join(HOME, "configs/main/avid-cma/audioset/InstX-N1024-PosW-N64-Top32.yaml")
         weight_path = load_weight_file(
             bucket="brainscore-vision",
-            relative_path="temporal_model_AVID-CMA/AVID-CMA_Audioset_InstX-N1024-PosW-N64-Top32_checkpoint.pth.tar",
+            relative_path="temporal_model_AVID_CMA/AVID-CMA_Audioset_InstX-N1024-PosW-N64-Top32_checkpoint.pth.tar",
             version_id="jSaZgbUohM0ZeoEUUKZiLBo6iz_v8VvQ",
             sha1="9db5eba9aab6bdbb74025be57ab532df808fe3f6"
         )
@@ -38,7 +38,7 @@ def get_model(identifier):
         cfg_path = os.path.join(HOME, "configs/main/avid/kinetics/Cross-N1024.yaml")
         weight_path = load_weight_file(
             bucket="brainscore-vision",
-            relative_path="temporal_model_AVID-CMA/AVID_Kinetics_Cross-N1024_checkpoint.pth.tar",
+            relative_path="temporal_model_AVID_CMA/AVID_Kinetics_Cross-N1024_checkpoint.pth.tar",
             version_id="XyKt0UOUFsuuyrl6ZREivK8FadRPx34u",
             sha1="d3a04f856d29421ba8de37808593a3fad4d4794f"
         )
@@ -47,7 +47,7 @@ def get_model(identifier):
         cfg_path = os.path.join(HOME, "configs/main/avid/audioset/Cross-N1024.yaml")
         weight_path = load_weight_file(
             bucket="brainscore-vision",
-            relative_path="temporal_model_AVID-CMA/AVID_Audioset_Cross-N1024_checkpoint.pth.tar",
+            relative_path="temporal_model_AVID_CMA/AVID_Audioset_Cross-N1024_checkpoint.pth.tar",
             version_id="0Sxuhn8LsYXQC4FnPfJ7rw7uU6kDlKgc",
             sha1="b48d8428a1a2526ccca070f810333df18bfce5fd"
         )

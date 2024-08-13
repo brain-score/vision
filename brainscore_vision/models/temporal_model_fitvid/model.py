e7de5d7c30b793a57bcc4ceca65ca6d34883cffd
Khaled Jedoui <thekej@stanford.edu>
2024-08-12 23:20:25 -0700

    add mcvd + fitvid

--- brainscore_vision/models/temporal_model_fitvid/model.py
+++ brainscore_vision/models/temporal_model_fitvid/model.py
@@ -2,11 +2,11 @@ import torch as th
 
 from brainscore_vision.model_helpers.activations.temporal.model import PytorchWrapper
 from brainscore_vision.model_helpers.s3 import load_weight_file
-from r3m_model import pfR3M_LSTM_physion, load_model
+from r3m_model import FitVidEncoder, load_model
 
 from torchvision import transforms as T
 
-class R3MLSTMWrapper(PytorchWrapper):
+class FitVidWrapper(PytorchWrapper):
     def forward(self, inputs):
         tensor = th.stack(inputs)
         tensor = tensor.permute(0, 2, 1, 3, 4)
@@ -15,8 +15,7 @@ class R3MLSTMWrapper(PytorchWrapper):
             output = self._model(tensor)
         return output#features  # encoder only
 
-transform_img = T.Compose([T.Resize(256),
-    T.CenterCrop(224)]) # ToTensor() divides by 255
+transform_img = T.Compose([T.Resize(64)])
 
 def transform_video(video):
     frames = th.Tensor(video.to_numpy()).permute(0, 3, 1, 2)
@@ -25,26 +24,23 @@ def transform_video(video):
 
 
 def get_model(identifier, num_frames=7):
-    assert identifier.startswith("R3M-LSTM")
+    assert identifier.startswith("FITVID")
     pretrain_only = True
 
-    if identifier == "R3M-LSTM-EGO4D":
+    if identifier == "FITVID-EGO4D":
         model_path = "TBD"
-    elif identifier == "R3M-LSTM-PHYS":
-        model_path = "TBD"
-    elif identifier == "R3M-LSTM-ARAN":
+    elif identifier == "FITVID-PHYS":
         model_path = "TBD"
     model_path = '/ccn2/u/thekej/R3M_pretrain/weights_ego4d/checkpoint_final.pt'
     # Instantiate the model
-        
-    net = pfR3M_LSTM_physion(n_past=num_frames)
-    net = load_model(net, identifier, model_path)
+    
+    net = FitVidEncoder(n_past=num_frames)
+    net = load_model(net, model_path)
 
     inferencer_kwargs = {
         "fps": 16,
         "layer_activation_format": {
             "encoder": "TC",
-            "dynamics": "TC",
         },
         "duration": None,
         "time_alignment": "per_frame_aligned",#"evenly_spaced",
@@ -55,14 +51,7 @@ def get_model(identifier, num_frames=7):
     for layer in inferencer_kwargs["layer_activation_format"].keys():
         assert "decoder" not in layer, "Decoder layers are not supported."
 
-    def process_activation(layer, layer_name, inputs, output):
-        if layer_name == 'encoder':
-            activations = output["observed_states"]
-        else:
-            activations = output["rollout_states"]
-        return activations 
-
-    wrapper = R3MLSTMWrapper(identifier, net, transform_video, 
+    wrapper = FitVidWrapper(identifier, net, transform_video, 
                                 process_output=process_activation,
                                 **inferencer_kwargs)
     return wrapper
--- brainscore_vision/models/temporal_model_mcvd/model.py
+++ brainscore_vision/models/temporal_model_mcvd/model.py
@@ -2,11 +2,11 @@ import torch as th
 
 from brainscore_vision.model_helpers.activations.temporal.model import PytorchWrapper
 from brainscore_vision.model_helpers.s3 import load_weight_file
-from r3m_model import pfR3M_LSTM_physion, load_model
+from mcvd_model import MCVDEncoder
 
 from torchvision import transforms as T
 
-class R3MLSTMWrapper(PytorchWrapper):
+class MCVDWrapper(PytorchWrapper):
     def forward(self, inputs):
         tensor = th.stack(inputs)
         tensor = tensor.permute(0, 2, 1, 3, 4)
@@ -15,8 +15,7 @@ class R3MLSTMWrapper(PytorchWrapper):
             output = self._model(tensor)
         return output#features  # encoder only
 
-transform_img = T.Compose([T.Resize(256),
-    T.CenterCrop(224)]) # ToTensor() divides by 255
+transform_img = T.Compose([T.Resize(64)]) # ToTensor() divides by 255
 
 def transform_video(video):
     frames = th.Tensor(video.to_numpy()).permute(0, 3, 1, 2)
@@ -25,31 +24,28 @@ def transform_video(video):
 
 
 def get_model(identifier, num_frames=7):
-    assert identifier.startswith("R3M-LSTM")
+    assert identifier.startswith("MCVD")
     pretrain_only = True
 
-    if identifier == "R3M-LSTM-EGO4D":
+    if identifier == "MCVD-EGO4D":
         model_path = "TBD"
-    elif identifier == "R3M-LSTM-PHYS":
+    elif identifier == "MCVD-PHYS":
         model_path = "TBD"
-    elif identifier == "R3M-LSTM-ARAN":
-        model_path = "TBD"
+        
     model_path = '/ccn2/u/thekej/R3M_pretrain/weights_ego4d/checkpoint_final.pt'
     # Instantiate the model
-        
-    net = pfR3M_LSTM_physion(n_past=num_frames)
-    net = load_model(net, identifier, model_path)
+    
+    net = MCVDEncoder(model_path)
 
     inferencer_kwargs = {
-        "fps": 16,
+        "fps": 25,
         "layer_activation_format": {
             "encoder": "TC",
-            "dynamics": "TC",
         },
         "duration": None,
         "time_alignment": "per_frame_aligned",#"evenly_spaced",
         "convert_img_to_video":True,
-        "img_duration":900
+        "img_duration":450
     }
 
     for layer in inferencer_kwargs["layer_activation_format"].keys():

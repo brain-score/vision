7e79de8aecba3917eecc3a1b168175623c24cc48
Khaled Jedoui <thekej@node13-ccn2cluster.stanford.edu>
2024-08-21 22:32:31 -0700

    dino lstm works

--- brainscore_vision/models/temporal_model_dino_lstm/dino_model.py
+++ brainscore_vision/models/temporal_model_dino_lstm/dino_model.py
@@ -26,62 +26,46 @@ def load_model(
     return model
 
 class DINO_pretrained(nn.Module):
-    def __init__(self, model_name="resnet50"):
+    def __init__(self):
         super().__init__()
         self.model = automodel.from_pretrained('facebook/dinov2-large')
         self.latent_dim = 8192
 
-    def forward(self, x, n_past=None):
-        inputs = x[:, : n_past]
-        input_states = self.get_encoder_feats(inputs)
-
-        # roll out the entire trajectory
-        label_images = x[:, n_past :]
-        observed_states = self.get_encoder_feats(label_images)
-        
-        output = {
-            "input_states": input_states,
-            "observed_states": torch.stack(input_states + observed_states, axis=1),
-        }
-        return output
-
-    def get_encoder_feats(self, x):
-        # applies encoder to each image in x: (Bs, T, 3, H, W) or (Bs, 3, H, W)
-        with torch.no_grad():  # TODO: best place to put this?
-            if x.ndim == 4:  # (Bs, 3, H, W)
-                feats = [self._extract_feats(x)]
-            else:
-                assert x.ndim == 5, "Expected input to be of shape (Bs, T, 3, H, W)"
-                feats = []
-                for _x in torch.split(x, 1, dim=1):
-                    _x = torch.squeeze(
-                        _x, dim=1
-                    )  # _x is shape (Bs, 1, 3, H, W) => (Bs, 3, H, W) TODO: put this in _extract_feats?
-                    feats.append(self._extract_feats(_x))
-        return feats
-
-    def _extract_feats(self, x):
-        input_dict = {'pixel_values': x}
-
+    def extract_features(self, images):
+        '''
+        images: [B, C, H, W], Image is normalized with imagenet norm
+        '''
+        input_dict = {'pixel_values': images}
         decoder_outputs = self.model(**input_dict, output_hidden_states=True)
-
         features = decoder_outputs.last_hidden_state
         features_1 = features[:, 0]
         features_2 = nn.AdaptiveAvgPool1d((7168))(features[:, 1:].reshape(features.shape[0], -1).float())
-        feats = torch.cat((features_1, features_2), dim=1)
+        features = torch.cat((features_1, features_2), dim=1)
+        return features
 
-        feats = torch.flatten(feats, start_dim=1)  # (Bs, -1)
-        return feats
+    def forward(self, videos, n_past=None):
+        '''
+        videos: [B, C, T, H, W], T is usually 4 and videos are normalized with imagenet norm
+        returns: [B, T, D] extracted features
+        '''
+        bs, num_frames, num_channels, h, w = videos.shape
+        videos = videos.flatten(0, 1)
+        features = self.extract_features(videos)
+        features = features.reshape(bs, num_frames, -1)
+        output = {
+            "input_states": features[:, : n_past],
+            "observed_states": features,
+        }
+        return output
 
 class LSTM(nn.Module):
     def __init__(self, latent_dim):
         super().__init__()
         self.latent_dim = latent_dim
-        self.lstm = nn.LSTM(self.latent_dim, 1024)
+        self.lstm = nn.LSTM(self.latent_dim, 1024, batch_first=True)
         self.regressor = nn.Linear(1024, self.latent_dim)
 
-    def forward_step(self, x):
-        feats = torch.stack(x)  # (T, Bs, self.latent_dim)
+    def forward_step(self, feats):
         assert feats.ndim == 3
         # note: for lstms, hidden is the last timestep output
         _, hidden = self.lstm(feats)
@@ -98,12 +82,12 @@ class LSTM(nn.Module):
             pred_state = self.forward_step(prev_states)
             simulated_states.append(pred_state)
             # add most recent pred and delete oldest (to maintain a temporal window of length n_past)
-            prev_states.append(pred_state)
-            prev_states.pop(0)
+            
+            prev_states = torch.cat([prev_states[:, 1:], pred_state.unsqueeze(1)], axis=1)
 
         output = {
             "simulated_states": torch.stack(simulated_states, axis=1),
-            "rollout_states": torch.cat([torch.stack(input_states, axis=1), torch.stack(simulated_states, axis=1)], axis=1),
+            "rollout_states": torch.cat([input_states, torch.stack(simulated_states, axis=1)], axis=1),
         }
         return output
 
@@ -131,7 +115,7 @@ class FrozenPretrainedEncoder(nn.Module):
         dynamics_output = self.dynamics(encoder_output['input_states'], rollout_steps)
 
         output = {
-            "input_states": torch.stack(encoder_output['input_states'], axis=1),
+            "input_states": encoder_output['input_states'],
             "observed_states": encoder_output['observed_states'],
             "simulated_states": dynamics_output['simulated_states'],
             "rollout_states": dynamics_output['rollout_states'],

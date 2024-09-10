1a16905b96216543411f3cb2f5515405b2890279
Khaled Jedoui <thekej@stanford.edu>
2024-09-10 09:39:38 -0700

    Delete brainscore_vision/data/ssv2_engineering_2024/test.py

--- brainscore_vision/data/ssv2_engineering_2024/test.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-import brainscore_vision
-
-
-@pytest.mark.private_access
-def test_existence():
-    assembly = load_dataset("SSV2ActivityRec2024")
-    assert assembly is not None

d46669e6a04914476e92e133201451ba9403570d
Khaled Jedoui <thekej@stanford.edu>
2024-10-01 16:05:09 -0700

    Update model.py

--- brainscore_vision/models/temporal_model_dfm_lstm/model.py
+++ brainscore_vision/models/temporal_model_dfm_lstm/model.py
@@ -13,8 +13,25 @@ class DFMLSTMWrapper(PytorchWrapper):
         output = self._model(tensor)
         return output
 
+class GroupNormalize(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor):
+        rep_mean = self.mean * (tensor.size()[0]//len(self.mean))
+        rep_std = self.std * (tensor.size()[0]//len(self.std))
+
+        # TODO: make efficient
+        for t, m, s in zip(tensor, rep_mean, rep_std):
+            t.sub_(m).div_(s)
+
+        return tensor
+
 transform_img = T.Compose([T.Resize(128),
-                          T.ToTensor()])
+                           T.ToTensor(),
+                           GroupNormalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                           ])
 
 def transform_video(video):
     frames = []

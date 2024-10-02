031d178952dc6c1a23a82a8bc0396631594f75e0
Khaled Jedoui <thekej@stanford.edu>
2024-10-02 06:46:56 -0700

    Update model.py

--- brainscore_vision/models/temporal_model_dinov2/model.py
+++ brainscore_vision/models/temporal_model_dinov2/model.py
@@ -17,7 +17,7 @@ IMAGENET_DEFAULT_MEAN = (0.485, 0.456, 0.406)
 IMAGENET_DEFAULT_STD = (0.229, 0.224, 0.225)
 
 transform_img = transforms.Compose([transforms.Resize(256),
-    transforms.CenterCrop(224)
+    transforms.CenterCrop(224),
     transforms.ToTensor(),
     transforms.Normalize(mean=IMAGENET_DEFAULT_MEAN, std=IMAGENET_DEFAULT_STD),])
 

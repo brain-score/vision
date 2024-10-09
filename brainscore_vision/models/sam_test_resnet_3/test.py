49365b17ddf60cb4aca6d7b5ff22ac6268b5b39b
Sam Winebrake <85908068+samwinebrake@users.noreply.github.com>
2024-10-09 16:29:17 -0400

    Revert "add sam_test_resnet_3 to models (#1309)" (#1310)
    This reverts commit 85ed01e1c6cc5760438e8067dc4a176b3c8af914.

--- brainscore_vision/models/sam_test_resnet_3/test.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import brainscore_vision
-
-
-@pytest.mark.travis_slow
-def test_has_identifier():
-    model = brainscore_vision.load_model('sam_test_resnet_3')
-    assert model.identifier == 'sam_test_resnet_3'

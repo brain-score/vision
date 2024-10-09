1acda4e4ff5c11c65d224acc0ee91c57b6b4d1cb
Kartik Pradeepan <kartikspradeepan@gmail.com>
2024-10-09 16:45:22 -0400

    Revert "add sam_test_resnet_3 to models (#1309)" (#1310) (#1311)

--- brainscore_vision/models/sam_test_resnet_3/__init__.py
+++
@@ -1,5 +0,0 @@
-from brainscore_vision import model_registry
-from brainscore_vision.model_helpers.brain_transformation import ModelCommitment
-from .model import get_model, get_layers
-
-model_registry['sam_test_resnet_3'] = lambda: ModelCommitment(identifier='sam_test_resnet_3', activations_model=get_model('sam_test_resnet_3'), layers=get_layers('sam_test_resnet_3'))

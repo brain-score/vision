17b659058f2fefdd8012ddb76de260c3ea58335a
Carol Jiang <caroljiang@Carols-MBP.attlocal.net>
2024-10-18 15:40:44 -0400

    fix zip

--- brainscore_vision/nwb_integration/generate_data_files.py
+++ brainscore_vision/nwb_integration/generate_data_files.py
@@ -13,8 +13,6 @@ class DataFactory:
     def __init__(self, user_json: str):
         self._logger = logging.getLogger(fullname(self))
 
-        # with open('/Users/caroljiang/Downloads/vision/brainscore_vision/nwb_integration/example_json.json', 'r') as file:
-        #     self.user_json = json.load(file)
         self.user_json = json.loads(user_json)
         self.parse_json()
 
@@ -177,19 +175,5 @@ def test_stimulus_set():
             for root, _, files in os.walk(folder_path):
                 for file in files:
                     file_path = os.path.join(root, file)
-                    relative_path = os.path.relpath(file_path, folder_path)
-                    zipf.write(file_path, relative_path)
-
-
-# if __name__ == '__main__':
-#     directory = '/Users/caroljiang/Downloads/vision/brainscore_vision/data/data_generation_trial'
-#     exp_path = f"/Users/caroljiang/Downloads/vision/brainscore_vision/data/data_generation_trial/"
-#     dandiset_id = '000788'
-#     nwb_file_path = 'sub-pico/sub-pico_ecephys.nwb'
-#     # nwb_file_path = 'sub-pico/sub-pico_ecephys+image.nwb'
-#     # data_factory = DataFactory(directory=directory, exp_path=exp_path, identifier=NWB_METADATA['stimulus_set']['identifier'], dandiset_id=dandiset_id, nwb_file_path=nwb_file_path)
-
-#     with open('/Users/caroljiang/Downloads/vision/brainscore_vision/nwb_integration/example_json.json', 'r') as file:
-#         user_json = json.load(file)
-#     data_factory = DataFactory(user_json='/Users/caroljiang/Downloads/vision/brainscore_vision/nwb_integration/example_json.json')
-#     data_factory()
+                    relative_path = os.path.relpath(file_path, os.path.dirname(folder_path))
+                    zipf.write(file_path, os.path.join(os.path.basename(folder_path), relative_path))

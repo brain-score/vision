a36be1c63eb7f3a1e5c1002b5ce8f670afcea422
Sam Winebrake <85908068+samwinebrake@users.noreply.github.com>
2024-10-22 12:34:48 -0400

    add tong. to identifier

--- brainscore_vision/benchmarks/coggan2024_behavior/__init__.py
+++ brainscore_vision/benchmarks/coggan2024_behavior/__init__.py
@@ -4,5 +4,5 @@ from brainscore_vision import benchmark_registry
 from .benchmark import (
     Coggan2024_behavior_ConditionWiseAccuracySimilarity)
 
-benchmark_registry['Coggan2024_behavior-ConditionWiseAccuracySimilarity'] = (
+benchmark_registry['tong.Coggan2024_behavior-ConditionWiseAccuracySimilarity'] = (
     Coggan2024_behavior_ConditionWiseAccuracySimilarity)

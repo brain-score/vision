ef11e1d3feaaab14226685e2cc41c2444d0fa45e
SusanWYS <127456911+SusanWYS@users.noreply.github.com>
2024-11-13 11:45:52 -0500

    Converted Xception (#1146)
    * Add BiT-M-R50x3
    
    * a
    
    * Add BiT-M-R50x3
    
    * aaa
    
    * add
    
    ---------
    
    Co-authored-by: Kartik Pradeepan <kartikspradeepan@gmail.com>

---
+++ brainscore_vision/models/xception/model.py
@@ -0,0 +1,52 @@
+import functools
+from brainscore_vision.model_helpers.activations.pytorch import load_preprocess_images
+from brainscore_vision.model_helpers.activations.pytorch import PytorchWrapper
+from brainscore_vision.model_helpers.check_submission import check_models
+import timm
+
+
+
+def get_model(name):
+    """
+    This method fetches an instance of a base model. The instance has to be callable and return a xarray object,
+    containing activations. There exist standard wrapper implementations for common libraries, like pytorch and
+    keras. Checkout the examples folder, to see more. For custom implementations check out the implementation of the
+    wrappers.
+    :param name: the name of the model to fetch
+    :return: the model instance
+    """
+    assert name == 'xception'
+    model = timm.create_model('xception', pretrained=True)
+    preprocessing = functools.partial(load_preprocess_images, image_size=224)
+    wrapper = PytorchWrapper(identifier='name', model=model, preprocessing=preprocessing)
+    wrapper.image_size = 224
+    return wrapper
+
+
+def get_layers(name):
+    assert name == 'xception'
+    model = torchvision.models.vgg19()
+    return list(dict(model.named_modules()).keys())[1:]
+
+
+def get_bibtex(name):
+    """
+    A method returning the bibtex reference of the requested model as a string.
+    """
+    return '''
+@article{DBLP:journals/corr/ZagoruykoK16,
+@misc{chollet2017xception,
+      title={Xception: Deep Learning with Depthwise Separable Convolutions}, 
+      author={François Chollet},
+      year={2017},
+      eprint={1610.02357},
+      archivePrefix={arXiv},
+      primaryClass={cs.CV}
+}
+'''
+
+
+if __name__ == '__main__':
+    # Use this method to ensure the correctness of the BaseModel implementations.
+    # It executes a mock run of brain-score benchmarks.
+    check_models.check_base_models(__name__)

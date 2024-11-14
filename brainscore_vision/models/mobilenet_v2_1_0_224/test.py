27a626e30a936d08ecb96a126f4c39e166c89bd6
Michael Ferguson <mferg@mit.edu>
2024-11-14 13:06:56 -0500

    Revert "Converted Mobilenet v2 1.0 224 (#1151)"
    This reverts commit 416d5b00439227951aa6054d222ead008f486675.

--- brainscore_vision/models/mobilenet_v2_1_0_224/test.py
+++
@@ -1,9 +0,0 @@
-import brainscore_vision
-import pytest
-
-
-
-@pytest.mark.travis_slow
-def test_has_identifier():
-    model = brainscore_vision.load_model('mobilenet_v2_1_0_224')
-    assert model.identifier == 'mobilenet_v2_1_0_224'

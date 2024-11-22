d645050f6ca77496c4129f10849b1543286bfe88
Mike Ferguson <mef8dd@virginia.edu>
2024-11-22 11:18:24 -0500

    remove unneeded model

--- brainscore_vision/models/BiT_M_R101x3/__init__.py
+++
@@ -1,7 +0,0 @@
-from brainscore_vision.model_helpers.brain_transformation import ModelCommitment
-from brainscore_vision import model_registry
-from .model import get_layers,get_model
-
-
-model_registry['BiT-M-R101x3'] = \
-    lambda: ModelCommitment(identifier='BiT-M-R101x3', activations_model=get_model('BiT-M-R101x3'), layers=get_layers('BiT-M-R101x3'))

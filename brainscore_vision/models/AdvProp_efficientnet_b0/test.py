87d54ea2fee134b360de7332390f618e972ddcfe
Mike Ferguson <mef8dd@virginia.edu>
2024-11-22 11:06:41 -0500

    remove 3 unneeded models

--- brainscore_vision/models/AdvProp_efficientnet_b0/test.py
+++
@@ -1,9 +0,0 @@
-import brainscore_vision
-import pytest
-
-
-
-@pytest.mark.travis_slow
-def test_has_identifier():
-    model = brainscore_vision.load_model('AdvProp_efficientnet-b0')
-    assert model.identifier == 'AdvProp_efficientnet-b0'

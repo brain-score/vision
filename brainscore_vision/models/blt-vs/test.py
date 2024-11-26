7cefe24f658129a70b0687291247d58b144892cd
kapoorlab <randomaccessiblekapoor@gmail.com>
2024-11-26 23:24:16 +0100

    empty test without identifier

--- brainscore_vision/models/blt-vs/test.py
+++
@@ -1,7 +0,0 @@
-import pytest
-import brainscore_vision
-
-@pytest.mark.travis_slow
-def test_has_identifier():
-    model = brainscore_vision.load_model('blt-vs')
-    assert model.identifier == 'blt-vs'

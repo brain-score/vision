6f0b868a4717d541472a1a51488cbb065fe2d0d6
kapoorlab <randomaccessiblekapoor@gmail.com>
2024-11-27 17:36:07 +0100

    add region layer map json modified

--- brainscore_vision/models/blt-vs/__init__.py
+++ brainscore_vision/models/blt-vs/__init__.py
@@ -6,17 +6,11 @@ import os
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 from model import get_model, LAYERS
 
-region_layer_map = {
-    "V1": "V1_5",
-    "V2": "V2_5",
-    "V4": "V4_5",
-    "IT": "LOC_5"
-}
+
 
 model_registry['blt_vs'] = lambda: ModelCommitment(
     identifier='blt_vs',
     activations_model=get_model(),
     layers=LAYERS,
-    region_layer_map=region_layer_map,
     visual_degrees=5)
 

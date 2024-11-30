3da4b065b67143660cc819bdd7908210cbb75e69
Jenkins <jenkins@ip-172-31-29-217.us-east-2.compute.internal>
2024-11-30 18:39:15 -0500

    add resnet50_less_variation_1 to models

---
+++ brainscore_vision/models/resnet50_less_variation_1/test.py
@@ -0,0 +1,8 @@
+
+import pytest
+import brainscore_vision
+
+@pytest.mark.travis_slow
+def test_random():
+    model = brainscore_vision.load_model('resnet50_less_variation_iteration=1')
+    assert model.identifier == 'resnet50_less_variation_iteration=1'

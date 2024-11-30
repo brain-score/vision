3f9b33073fd80e5e1af4758bce1fe5f89f9bfa40
Jenkins <jenkins@ip-172-31-29-217.us-east-2.compute.internal>
2024-11-30 09:28:28 -0500

    add vgg16_no_specularities_1 to models

---
+++ brainscore_vision/models/vgg16_no_specularities_1/test.py
@@ -0,0 +1,9 @@
+
+import pytest
+import brainscore_vision
+
+
+@pytest.mark.travis_slow
+def test_has_identifier():
+    model = brainscore_vision.load_model(f'vgg16_no_specularities_iteration=1')
+    assert model.identifier == f'vgg16_no_specularities_iteration=1'

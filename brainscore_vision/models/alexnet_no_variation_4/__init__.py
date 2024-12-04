61a5d9e281593f94cf512793b988adfbed0248f4
Jenkins <jenkins@ip-172-31-29-217.us-east-2.compute.internal>
2024-12-04 11:57:28 -0500

    add alexnet_no_variation_4 to models

--- brainscore_vision/models/alexnet_no_variation_4/__init__.py
+++ brainscore_vision/models/alexnet_no_variation_4/__init__.py
@@ -1,6 +1,5 @@
 
 from brainscore_vision import model_registry
 from brainscore_vision.model_helpers.brain_transformation import ModelCommitment
-from .model import get_model, get_layers
-
-model_registry['alexnet_no_variation_iteration=4'] = lambda: ModelCommitment(identifier='alexnet_no_variation_iteration=4', activations_model=get_model('alexnet_no_variation_iteration=4'), layers=get_layers('alexnet_no_variation_iteration=4'))
+from .model import get_model, MODEL_CONFIGS, get_layers
+            

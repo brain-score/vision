e97dfc1648506b207bce1fcdc0c5280132046138
Jenkins <jenkins@ip-172-31-29-217.us-east-2.compute.internal>
2024-12-04 11:54:51 -0500

    add alexnet_less_variation_5 to models

---
+++ brainscore_vision/models/alexnet_less_variation_5/__init__.py
@@ -0,0 +1,6 @@
+
+from brainscore_vision import model_registry
+from brainscore_vision.model_helpers.brain_transformation import ModelCommitment
+from .model import get_model, MODEL_CONFIGS, get_layers
+            
+model_registry['alexnet_less_variation_iteration=5'] = lambda: ModelCommitment(identifier='alexnet_less_variation_iteration=5', activations_model=get_model('alexnet_less_variation_iteration=5'), layers=get_layers('alexnet_less_variation_iteration=5'))

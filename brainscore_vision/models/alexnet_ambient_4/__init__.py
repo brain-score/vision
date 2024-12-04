40dd679126c888c467c59fd95075a244eabb7eff
Jenkins <jenkins@ip-172-31-29-217.us-east-2.compute.internal>
2024-12-04 11:40:54 -0500

    add alexnet_ambient_4 to models

---
+++ brainscore_vision/models/alexnet_ambient_4/__init__.py
@@ -0,0 +1,6 @@
+
+from brainscore_vision import model_registry
+from brainscore_vision.model_helpers.brain_transformation import ModelCommitment
+from .model import get_model, MODEL_CONFIGS, get_layers
+            
+model_registry['alexnet_ambient_iteration=4'] = lambda: ModelCommitment(identifier='alexnet_ambient_iteration=4', activations_model=get_model('alexnet_ambient_iteration=4'), layers=get_layers('alexnet_ambient_iteration=4'))

1e1cd6fa795f4076a4d0961e0f40aaa313d7b671
Mike Ferguson <mef8dd@virginia.edu>
2024-12-19 10:19:43 -0500

    change coggan versions

--- brainscore_vision/data/coggan2024_fMRI/__init__.py
+++ brainscore_vision/data/coggan2024_fMRI/__init__.py
@@ -12,13 +12,13 @@ stimulus_set_registry['Coggan2024_fMRI'] = lambda: load_stimulus_set_from_s3(
     bucket="brainscore-storage/brainio-brainscore",
     csv_sha1="0089f5f8fd3f2de14de12ed736a0f88575f8e1ee",
     zip_sha1="e26fdea4d866799526dea183f5bfb9792718822a",
-    csv_version_id="null",
-    zip_version_id="null")
+    csv_version_id="kAS9zqRPf7zmLCN73I2VB5gBvhrZoU0p",
+    zip_version_id="jmiuKdlsh6.1suR_5FvLFDyg.PCzEZTn")
 
 # fMRI data
 data_registry['Coggan2024_fMRI'] = lambda: load_assembly_from_s3(
     identifier="tong.Coggan2024_fMRI",
-    version_id="null",
+    version_id="pNHkcl7putBblyhdFqBX3d1F2kid6TGj",
     sha1="da3adbca5247d0491d366f94e8431fb3e4e58db2",
     bucket="brainscore-storage/brainio-brainscore",
     cls=NeuroidAssembly,

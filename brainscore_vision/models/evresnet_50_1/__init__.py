f16eff543c84734bfb0f9430c87567427a582975
Lucas Piper <92276213+lucaspiper99@users.noreply.github.com>
2024-12-19 21:29:58 +0000

    fixed V1 layer commitment name

--- brainscore_vision/models/evresnet_50_1/__init__.py
+++ brainscore_vision/models/evresnet_50_1/__init__.py
@@ -7,6 +7,6 @@ model_registry['evresnet_50_1'] = lambda: ModelCommitment(
     activations_model=get_model('evresnet_50_1'),
     layers=get_layers('evresnet_50_1'),
     behavioral_readout_layer='model.5',
-    region_layer_map={'V1': 'vonenet'},
+    region_layer_map={'V1': 'voneblock'},
     visual_degrees=7,
     )

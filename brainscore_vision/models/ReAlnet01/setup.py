1b58625035a6c76613504c670fbe3f1517ff07d2
Jenkins <jenkins@ip-172-31-29-217.us-east-2.compute.internal>
2025-01-07 16:53:06 -0500

    add ReAlnet01 to models

---
+++ brainscore_vision/models/ReAlnet01/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='ReAlnet01',
+    version='1.0.0',
+    author='Zitong Lu',
+    install_requires=[
+        'math',
+        'collections',
+        'torch',
+        'torchvision',
+        'os',
+        'pandas',
+        'numpy',
+        'h5py',
+        'random',
+        'gdown'],
+)

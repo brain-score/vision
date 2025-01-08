553cedc0c15546481976160b082b400d5f4ce8b7
Mike Ferguson <mef8dd@virginia.edu>
2025-01-08 09:59:18 -0500

    remove models that are ready to be merged

--- brainscore_vision/models/resnet_SIN_IN_FT_IN/test.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import brainscore_vision
-
-
-@pytest.mark.travis_slow
-def test_has_identifier():
-    model = brainscore_vision.load_model('resnet_SIN_IN_FT_IN')
-    assert model.identifier == 'resnet_SIN_IN_FT_IN'

4240ba489b8986965d9194ed8d8a21b4f5ebbc84
Mike Ferguson <mef8dd@virginia.edu>
2025-01-08 10:03:23 -0500

    remove models that are ready to be merged

--- brainscore_vision/models/imagenet_l2_3_0/test.py
+++
@@ -1,8 +0,0 @@
-import brainscore_vision
-import pytest
-
-
-@pytest.mark.travis_slow
-def test_has_identifier():
-    model = brainscore_vision.load_model('imagenet_l2_3_0')
-    assert model.identifier == 'imagenet_l2_3_0'

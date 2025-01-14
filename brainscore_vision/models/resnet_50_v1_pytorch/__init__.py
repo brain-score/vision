49bbdbb151c244662f107de6ac28db6976d1e50e
samwinebrake <samwinebrake@gmail.com>
2025-01-14 16:27:46 -0500

    delete old resnet 50 v1

--- brainscore_vision/models/resnet_50_v1_pytorch/__init__.py
+++
@@ -1,5 +0,0 @@
-from brainscore_vision import model_registry
-from brainscore_vision.model_helpers.brain_transformation import ModelCommitment
-from .model import get_model, get_layers
-
-model_registry['resnet-50_v1_pytorch'] = lambda: ModelCommitment(identifier='resnet-50_v1_pytorch', activations_model=get_model('resnet-50_v1_pytorch'), layers=get_layers('resnet-50_v1_pytorch'))

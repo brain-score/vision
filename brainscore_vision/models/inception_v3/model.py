7d7edeab09b76309fd8500700e92cd318f1c7425
Sam Winebrake <85908068+samwinebrake@users.noreply.github.com>
2025-01-21 16:13:26 -0500

    add inception_v3 (#1739)
    * add inception_v3
    
    * Add inception_v3.json to region_layer_map for model inception_v3
    
    ---------
    
    Co-authored-by: KartikP <kpradeep@mit.edu>

--- brainscore_vision/models/inception_v3/model.py
+++ brainscore_vision/models/inception_v3/model.py
@@ -21,9 +21,9 @@ implementation.
 MODEL = timm.create_model('inception_v3', pretrained=True)
 
 def get_model(name):
-    assert name == 'inception_v3_pytorch'
-    preprocessing = functools.partial(load_preprocess_images, image_size=299)
-    wrapper = PytorchWrapper(identifier='inception_v3_pytorch', model=MODEL,
+    assert name == 'inception_v3'
+    preprocessing = functools.partial(load_preprocess_images, image_size=299, preprocess_type='inception')
+    wrapper = PytorchWrapper(identifier='inception_v3', model=MODEL,
                              preprocessing=preprocessing,
                              batch_size=4)  # doesn't fit into 12 GB GPU memory otherwise
     wrapper.image_size = 299
@@ -31,13 +31,13 @@ def get_model(name):
 
 
 def get_layers(name):
-    assert name == 'inception_v3_pytorch'
-    layer_names = []
-
-    for name, module in MODEL.named_modules():
-        layer_names.append(name)
-
-    return layer_names[2:]
+    assert name == 'inception_v3'
+    layer_names = (['Conv2d_1a_3x3', 'Pool1', 'Pool2'] +
+                   [f'Mixed_5{i}' for i in ['b', 'c', 'd']] +
+                   [f'Mixed_6{i}' for i in ['a', 'b', 'c', 'd', 'e']] +
+                   [f'Mixed_7{i}' for i in ['a', 'b', 'c']] +
+                   ['global_pool'])
+    return layer_names
 
 
 def get_bibtex(model_identifier):

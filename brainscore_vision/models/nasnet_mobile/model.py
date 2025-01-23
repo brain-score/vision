1215426037151b244fcfd4330ca7d1bd88d3e5fe
samwinebrake <samwinebrake@gmail.com>
2025-01-23 12:39:57 -0500

    update folder name of mobile

--- brainscore_vision/models/nasnet_mobile/model.py
+++ brainscore_vision/models/nasnet_mobile/model.py
@@ -637,7 +637,7 @@ def get_model(name):
     assert name == 'nasnet_mobile'
     preprocessing = functools.partial(load_preprocess_images, image_size=224, preprocess_type='inception')
     model = nasnetamobile()
-    weights_path = load_weight_file(bucket="brainscore-storage", folder_name="brainscore_vision/models",
+    weights_path = load_weight_file(bucket="brainscore-storage", folder_name="brainscore-vision/models",
                                    relative_path="nasnet_mobile/nasnetamobile-7e03cead.pth",
                                    version_id="null",
                                    sha1="2f88f50ca7b16c9462cdf4b001bdca6cc82aea35")

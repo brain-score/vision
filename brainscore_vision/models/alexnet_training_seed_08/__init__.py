ad9a61cc789e8e7bd0c6bfb4d63325f5e7e5630b
ben22020 <bbruceh@gmail.com>
2025-01-29 11:27:31 -0700

    add alexnet training seeds for scoring

---
+++ brainscore_vision/models/alexnet_training_seed_08/__init__.py
@@ -0,0 +1,6 @@
+from brainscore_vision import model_registry
+from brainscore_vision.model_helpers.brain_transformation import ModelCommitment
+from .model import get_model, get_layers
+
+model_registry['alexnet_training_seed_08'] = lambda: ModelCommitment(identifier='alexnet_training_seed_08', activations_model=get_model('alexnet_training_seed_08'), layers=get_layers('alexnet_training_seed_08'))
+    

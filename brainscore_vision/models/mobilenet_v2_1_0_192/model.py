a9f1883cee38fc4149fd13efe93166468d20542f
Sam Winebrake <85908068+samwinebrake@users.noreply.github.com>
2025-01-31 11:45:25 -0500

    fix weights only error

--- brainscore_vision/models/mobilenet_v2_1_0_192/model.py
+++ brainscore_vision/models/mobilenet_v2_1_0_192/model.py
@@ -27,7 +27,7 @@ def get_model(name):
     """
     assert name == 'mobilenet_v2_1_0_192'
     preprocessing = functools.partial(load_preprocess_images, image_size=192, preprocess_type='inception')
-    model = torch.load(model_weight_path.as_posix())
+    model = torch.load(model_weight_path.as_posix(), weights_only=False)
     wrapper = MobilenetPytorchWrapper(identifier=name, model=model, preprocessing=preprocessing)
     wrapper.image_size = 192
     return wrapper

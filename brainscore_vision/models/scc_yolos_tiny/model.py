28b7c83476f1c795fc36e2d61884ff540627a76c
Sofie Chung <sofiecchung@gmail.com>
2025-04-09 09:40:14 -0400

    added custom preprocessing function

--- brainscore_vision/models/scc_yolos_tiny/model.py
+++ brainscore_vision/models/scc_yolos_tiny/model.py
@@ -1,63 +1,84 @@
 from brainscore_vision.model_helpers.check_submission import check_models
 import functools
-from transformers import AutoImageProcessor, AutoModelForObjectDetection
+import numpy as np
+import torch
+from transformers import YolosImageProcessor, YolosForObjectDetection
 from brainscore_vision.model_helpers.activations.pytorch import PytorchWrapper
-from brainscore_vision.model_helpers.activations.pytorch import load_preprocess_images
+from PIL import Image
+
+class PytorchWrapperFixed(PytorchWrapper):
+    @staticmethod
+    def _tensor_to_numpy(output):
+        if isinstance(output, tuple):
+            output = output[0]
+        return output.cpu().data.numpy()
+
+    def register_hook(self, layer, layer_name, target_dict):
+        def hook_function(_layer, _input, output, name=layer_name):
+            if isinstance(output, tuple):
+                output = output[0]
+            target_dict[name] = PytorchWrapperFixed._tensor_to_numpy(output)
+        hook = layer.register_forward_hook(hook_function)
+        return hook
+
+def get_model_list():
+    return ['yolos_tiny']
 
 def get_model(name):
-    assert name == "yolos_tiny"
-    
-    model = AutoModelForObjectDetection.from_pretrained("hustvl/yolos-tiny")
-    preprocessing = functools.partial(load_preprocess_images, image_size=224)
-    wrapper = PytorchWrapper(identifier='yolos_tiny', model=model, preprocessing=preprocessing, batch_size=4)
-    wrapper.image_size = 224
+    assert name == 'yolos_tiny'
+    image_size = 224  # YOLOS works with 640x640 images
+    processor = YolosImageProcessor.from_pretrained('hustvl/yolos-tiny')
+    model = YolosForObjectDetection.from_pretrained('hustvl/yolos-tiny')
+    preprocessing = functools.partial(load_preprocess_images, processor=processor, image_size=image_size)
+    wrapper = PytorchWrapperFixed(identifier=name, model=model, preprocessing=preprocessing)
+    wrapper.image_size = image_size
     return wrapper
-
 
 def get_layers(name):
     assert name == "yolos_tiny"
     layer_names = [
-        "vit.embeddings.patch_embeddings.projection",
-        "vit.embeddings.interpolation",
+        "vit.encoder.layer.0.attention.output.dense",
+        "vit.encoder.layer.0.output.dense",
+        "vit.encoder.layer.2.attention.output.dense",
+        "vit.encoder.layer.2.output.dense",
+        "vit.encoder.layer.4.attention.output.dense",
+        "vit.encoder.layer.4.output.dense",
+        "vit.encoder.layer.6.attention.output.dense",
+        "vit.encoder.layer.6.output.dense",
+        "vit.encoder.layer.8.attention.output.dense",
+        "vit.encoder.layer.8.output.dense",
+        "vit.encoder.layer.10.attention.output.dense",
+        "vit.encoder.layer.10.output.dense",
+        "vit.encoder.layer.11.attention.output.dense",
+        "vit.encoder.layer.11.output.dense",
+
         "vit.layernorm",
+
+        "class_labels_classifier.layers.0",
+        "class_labels_classifier.layers.1",
         "class_labels_classifier.layers.2",
+
+        "bbox_predictor.layers.0",
+        "bbox_predictor.layers.1",
         "bbox_predictor.layers.2",
     ]
-
-    for i in range(12):
-        layer_names.append(f"vit.encoder.layer.{i}.attention.output.dense") 
-        layer_names.append(f"vit.encoder.layer.{i}.output.dense")          
-
-    # for n,_ in AutoModelForObjectDetection.from_pretrained("hustvl/yolos-tiny").named_modules():
-    #     print(n)
     return layer_names
 
+def get_bibtex(model_identifier):
+    return """@article{fang2021yolos,
+      title={You Only Look at One Sequence: Rethinking Transformer in Vision through Object Detection},
+      author={Tianyang Fang and Wen Wang and Bohan Zhuang and Chunhua Shen},
+      journal={arXiv preprint arXiv:2106.00666},
+      year={2021}
+    }"""
 
-def get_bibitex(model_identifier):
-    return """
-        @article{DBLP:journals/corr/abs-2106-00666,
-        author    = {Yuxin Fang and
-                    Bencheng Liao and
-                    Xinggang Wang and
-                    Jiemin Fang and
-                    Jiyang Qi and
-                    Rui Wu and
-                    Jianwei Niu and
-                    Wenyu Liu},
-        title     = {You Only Look at One Sequence: Rethinking Transformer in Vision through
-                    Object Detection},
-        journal   = {CoRR},
-        volume    = {abs/2106.00666},
-        year      = {2021},
-        url       = {https://arxiv.org/abs/2106.00666},
-        eprinttype = {arXiv},
-        eprint    = {2106.00666},
-        timestamp = {Fri, 29 Apr 2022 19:49:16 +0200},
-        biburl    = {https://dblp.org/rec/journals/corr/abs-2106-00666.bib},
-        bibsource = {dblp computer science bibliography, https://dblp.org}
-        }
-"""
+def load_preprocess_images(image_filepaths, image_size, processor=None, **kwargs):
+    images = [Image.open(filepath).convert('RGB') for filepath in image_filepaths]
+    images = [image.resize((image_size, image_size)) for image in images]
+    if processor is not None:
+        images = [processor(images=image, return_tensors="pt", **kwargs)['pixel_values'] for image in images]
+        images = torch.cat(images).cpu().numpy()
+    return images
 
 if __name__ == '__main__':
-    # get_layers("yolos_tiny")
     check_models.check_base_models(__name__)

5623affc16ff50c8b31c873f1029953ca71690ad
Jenkins <jenkins@ip-172-31-29-217.us-east-2.compute.internal>
2025-04-20 20:33:05 -0500

    add CE-SimCLR_lmda_05 to models

---
+++ brainscore_vision/models/CE-SimCLR_lmda_05/__init__.py
@@ -0,0 +1,9 @@
+from brainscore_vision import model_registry
+from brainscore_vision.model_helpers.brain_transformation import ModelCommitment
+from .model import get_model, get_layers
+
+model_registry["CE-SimCLR_lmda_05"] = lambda: ModelCommitment(
+    identifier="CE-SimCLR_lmda_05",
+    activations_model=get_model("CE-SimCLR_lmda_05"),
+    layers=get_layers("CE-SimCLR_lmda_05"),
+)

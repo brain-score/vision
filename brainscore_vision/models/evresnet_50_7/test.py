dc3c9c0f5c01dcd349cbd40e2c31f854296c65e7
Jenkins <jenkins@ip-172-31-29-217.us-east-2.compute.internal>
2025-04-24 14:02:09 -0500

    add evresnet_50_7 to models

---
+++ brainscore_vision/models/evresnet_50_7/test.py
@@ -0,0 +1,8 @@
+import pytest
+import brainscore_vision
+
+
+@pytest.mark.travis_slow
+def test_has_identifier():
+    model = brainscore_vision.load_model('evresnet_50_7')
+    assert model.identifier == 'evresnet_50_7'

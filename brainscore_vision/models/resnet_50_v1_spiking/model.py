b16c71aa814e1914b6e8a7656d7aeae3dd4ff038
Sofie Chung <sofiecchung@gmail.com>
2025-04-30 14:45:58 -0400

    Update model.py

--- brainscore_vision/models/resnet_50_v1_spiking/model.py
+++ brainscore_vision/models/resnet_50_v1_spiking/model.py
@@ -23,8 +23,6 @@ class SpikingBottleneck(nn.Module):
         self.relu2 = neuron.LIFNode(step_mode='s')
         self.relu3 = neuron.LIFNode(step_mode='s')
 
-        # No downsample for layer2[1], so we skip that
-
     def forward(self, x):
         # Reset neuron states
         self.relu1.reset()
@@ -50,7 +48,6 @@ class SpikingBottleneck(nn.Module):
         return out
 
 def get_model(name):
-
 # Modify the model
     model = resnet50(weights='IMAGENET1K_V1')
     model.layer2[1] = SpikingBottleneck(model.layer2[1])  # Replace a safe block
@@ -80,53 +77,5 @@ def get_bibtex(model_identifier):
   year={2016}
 }"""
 
-
 if __name__ == '__main__':
     check_models.check_base_models(__name__)
-
-
-
-
-
-# from torchvision.models import resnet50
-# from brainscore_vision.model_helpers.check_submission import check_models
-# from brainscore_vision.model_helpers.activations.pytorch import PytorchWrapper
-# from brainscore_vision.model_helpers.activations.pytorch import load_preprocess_images
-# from brainscore_vision.model_helpers.check_submission import check_models
-# import functools
-
-
-# model = resnet50(weights='IMAGENET1K_V1')
-
-# def get_model(name):
-#     assert name == 'resnet_50_v1_spiking'
-#     preprocessing = functools.partial(load_preprocess_images, image_size=224)
-#     wrapper = PytorchWrapper(identifier='resnet_50_v1_spiking', model=model, preprocessing=preprocessing)
-#     wrapper.image_size = 224
-#     return wrapper
-
-# def get_layers(name):
-#     assert name == 'resnet_50_v1_spiking'
-#     units = [3, 4, 6, 3]
-#     layer_names = ['conv1'] + [f'layer{block+1}.{unit}' for block, block_units in
-#                                enumerate(units) for unit in range(block_units)] + ['avgpool']
-#     return layer_names
-
-
-# def get_bibtex(model_identifier):
-#     assert model_identifier == 'resnet_50_v1_spiking'
-#     return """
-#     @inproceedings{he2016deep,
-#   title={Deep residual learning for image recognition},
-#   author={He, Kaiming and Zhang, Xiangyu and Ren, Shaoqing and Sun, Jian},
-#   booktitle={Proceedings of the IEEE conference on computer vision and pattern recognition},
-#   pages={770--778},
-#   year={2016}
-# }"""
-
-
-
-# if __name__ == '__main__':
-#     # Use this method to ensure the correctness of the BaseModel implementations.
-#     # It executes a mock run of brain-score benchmarks.
-#     check_models.check_base_models(__name__)

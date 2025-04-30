f7fc6720418995b17b6b3d45d3d447521b330184
Sofie Chung <sofiecchung@gmail.com>
2025-04-30 12:59:51 -0400

    Update model.py

--- brainscore_vision/models/resnet_50_v1_spiking/model.py
+++ brainscore_vision/models/resnet_50_v1_spiking/model.py
@@ -31,10 +31,9 @@ class SpikingHead(nn.Module):
 def replace_relu_with_spiking(module):
     for name, child in module.named_children():
         if isinstance(child, nn.ReLU):
-            setattr(module, name, neuron.LIFNode())
+            setattr(module, name, neuron.LIFNode(step_mode='m', backend='torch'))
         else:
             replace_relu_with_spiking(child)
-
 
 
 # Brain-Score interface

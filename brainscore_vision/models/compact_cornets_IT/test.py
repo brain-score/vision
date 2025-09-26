ac3a0777d0b125256fa9fd658dbbd77b6b794334
Kartik Pradeepan <kartikspradeepan@gmail.com>
2025-09-26 16:53:16 -0400

    brain-score.org submission (user:718) | (public:True) (#2014)
    * add compact_cornets_IT to models
    
    * Add compact_cornets_IT.json to region_layer_map for model compact_cornets_IT
    
    ---------
    
    Co-authored-by: Jenkins <jenkins@ip-172-31-29-217.us-east-2.compute.internal>

---
+++ brainscore_vision/models/compact_cornets_IT/test.py
@@ -0,0 +1,8 @@
+import pytest
+import brainscore_vision
+
+
+@pytest.mark.travis_slow
+def test_has_identifier():
+    model = brainscore_vision.load_model('compact_cornets_IT')
+    assert model.identifier == 'compact_cornets_IT'

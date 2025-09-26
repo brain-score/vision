acf04b73b08ac2aeacd93d45146b3aaca182f214
Jenkins <jenkins@ip-172-31-29-217.us-east-2.compute.internal>
2025-09-26 14:10:23 -0500

    add compact_resnet50_IT to models

---
+++ brainscore_vision/models/compact_resnet50_IT/__init__.py
@@ -0,0 +1,5 @@
+from brainscore_vision import model_registry
+from brainscore_vision.model_helpers.brain_transformation import ModelCommitment
+from .model import get_model, get_layers
+
+model_registry['compact_resnet50_IT'] = lambda: ModelCommitment(identifier='compact_resnet50_IT', activations_model=get_model('compact_resnet50_IT'), layers=get_layers('compact_resnet50_IT'))

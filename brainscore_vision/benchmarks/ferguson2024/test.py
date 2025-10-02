0f86563b3774f7c318fc2628b7ff9e4a8e0c6a46
mferg <mferg@mit.edu>
2025-10-02 13:09:32 -0400

    fix import in test.py

--- brainscore_vision/benchmarks/ferguson2024/test.py
+++ brainscore_vision/benchmarks/ferguson2024/test.py
@@ -4,7 +4,7 @@ from pytest import approx
 from brainscore_core.supported_data_standards.brainio.assemblies import BehavioralAssembly
 from brainscore_vision import load_benchmark
 from brainscore_vision.benchmark_helpers import PrecomputedFeatures
-from brainscore_vision.data_helpers import s3
+from brainscore_core.supported_data_standards.brainio import s3
 
 
 class TestExist:

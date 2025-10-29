6db18ea746cfbabbfa51c3ddc2e5b32e8fa633e1
KogniJannis <jannis.ahlert@student.uni-tuebingen.de>
2025-10-29 16:37:39 +0000

    update Papale2025 benchmark: use common interface + parametrize metric & region

--- brainscore_vision/benchmarks/papale2025/benchmark.py
+++ brainscore_vision/benchmarks/papale2025/benchmark.py
@@ -1,8 +1,5 @@
 from brainscore_vision import load_dataset, load_metric
-from brainscore_vision.benchmark_helpers.screen import place_on_screen
-from brainscore_vision.benchmark_helpers.neural_common import timebins_from_assembly  
-from brainscore_vision.benchmarks import BenchmarkBase
-from brainscore_vision.model_interface import BrainModel
+from brainscore_vision.benchmark_helpers.neural_common import TrainTestNeuralBenchmark, average_repetition
 from brainscore_vision.utils import LazyLoad
 
 
@@ -19,62 +16,33 @@ BIBTEX = """@article{papale_extensive_2025,
 }"""
 VISUAL_DEGREES = 8
 
-class _Papale2025(BenchmarkBase):
-	def __init__(self, region, similarity_metric):
-		self._region = region
-		self._number_of_trials = 1
-		self._visual_degrees = VISUAL_DEGREES
-		self.train_assembly = LazyLoad(lambda region=region: load_assembly(region=region, split='train'))
-		self.test_assembly = LazyLoad(lambda region=region: load_assembly(region=region, split='test'))
-		self._similarity_metric = similarity_metric
-		super().__init__(
-			identifier=f'Papale2025.{region}-pls', 
-			version=1,
-			ceiling_func=lambda: 1,  # TODO: replace with actual ceiling
-			parent=region,
-			bibtex=BIBTEX
-		)
-	def __call__(self, candidate : BrainModel):
+def _Papale2025(region, similarity_metric, identifier_metric_suffix):
+	number_of_trials = 1
+	visual_degrees = VISUAL_DEGREES
+	train_assembly = LazyLoad(lambda region=region: load_assembly(region=region, split='train', average_repetitions=False)) #train has no repetitions
+	test_assembly = LazyLoad(lambda region=region: load_assembly(region=region, split='test', average_repetitions=True))
+	return TrainTestNeuralBenchmark(identifier=f'Papale2025-{region}-{identifier_metric_suffix}',
+	                          version=1,
+	                          ceiling_func=lambda: 1.0,
+	                          train_assembly=train_assembly,
+	                          test_assembly=test_assembly,
+	                          similarity_metric=similarity_metric,
+	                          visual_degrees=visual_degrees,
+	                          number_of_trials=number_of_trials,
+	                          parent=region,
+							  bibtex=BIBTEX)    
 
-		# get the activations from the train set
-		train_stimulus_set = self.train_assembly.stimulus_set
-		timebins = timebins_from_assembly(self.train_assembly)
-		candidate.start_recording(self._region, time_bins=timebins)
-		stimulus_set = place_on_screen(train_stimulus_set, target_visual_degrees=candidate.visual_degrees(),
-                                    source_visual_degrees=self._visual_degrees)
-		train_activations = candidate.look_at(stimulus_set, number_of_trials=self._number_of_trials)
-		
-		# get the activations from the test set
-		test_stimulus_set = self.test_assembly.stimulus_set
-		timebins = timebins_from_assembly(self.test_assembly)
-		candidate.start_recording(self._region, time_bins=timebins)
-		stimulus_set = place_on_screen(test_stimulus_set, target_visual_degrees=candidate.visual_degrees(),
-									source_visual_degrees=self._visual_degrees)
-		test_activations = candidate.look_at(stimulus_set, number_of_trials=self._number_of_trials)
+def Papale2025(region, metric_type):
+    similarity_metric = load_metric(f'{metric_type}-split')
+    return _Papale2025(region, similarity_metric=similarity_metric, identifier_metric_suffix=metric_type)
 
-		raw_score = self._similarity_metric(source_train=train_activations, source_test=test_activations,
-                target_train=self.train_assembly, target_test=self.test_assembly)
-		#TODO ceil score
-		return raw_score
-        
-
-def Papale2025PLS(region):
-    similarity_metric = load_metric('pls-split')
-    return _Papale2025(region, similarity_metric=similarity_metric)
-
-def Papale2025Ridge(region):
-	similarity_metric = load_metric('ridge-split')
-	return _Papale2025(region, similarity_metric=similarity_metric)
-
-def Papale2025NeuronToNeuron(region):
-	similarity_metric = load_metric('neuron_to_neuron-split')
-	return _Papale2025(region, similarity_metric=similarity_metric)
-
-def load_assembly(region, split):
+def load_assembly(region, split, average_repetitions):
 	assembly = load_dataset(f'Papale2025_{split}')
 	assembly = assembly.sel(region=region)
 	assembly['region'] = 'neuroid', [region] * len(assembly['neuroid'])
 	assembly.load()
 	assembly = assembly.isel(time_bin=0) #TODO: check if intended
+	if average_repetitions:
+		assembly = average_repetition(assembly)
 	#TODO assert VISUAL_DEGREES == assembly.attrs['image_size_degree']
 	return assembly

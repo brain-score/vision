7ddbdcc0f150b2977d97d7231c5a0060374d8e0e
KogniJannis <jannis.ahlert@student.uni-tuebingen.de>
2025-11-08 14:17:58 +0000

    updated all 3 benchmark __init__: only ridge and ridgecv in registry

--- brainscore_vision/benchmarks/hebart2023_fmri/__init__.py
+++ brainscore_vision/benchmarks/hebart2023_fmri/__init__.py
@@ -1,17 +1,12 @@
 from brainscore_vision import benchmark_registry
 from .benchmark import Hebart2023fmri
 
-benchmark_registry['Hebart2023_fmri.V1-pls'] = lambda: Hebart2023fmri('V1', 'pls')
-benchmark_registry['Hebart2023_fmri.V2-pls'] = lambda: Hebart2023fmri('V2', 'pls')
-benchmark_registry['Hebart2023_fmri.V4-pls'] = lambda: Hebart2023fmri('V4', 'pls')
-benchmark_registry['Hebart2023_fmri.IT-pls'] = lambda: Hebart2023fmri('IT', 'pls')
-
 benchmark_registry['Hebart2023_fmri.V1-ridge'] = lambda: Hebart2023fmri('V1', 'ridge')
 benchmark_registry['Hebart2023_fmri.V2-ridge'] = lambda: Hebart2023fmri('V2', 'ridge')
 benchmark_registry['Hebart2023_fmri.V4-ridge'] = lambda: Hebart2023fmri('V4', 'ridge')
 benchmark_registry['Hebart2023_fmri.IT-ridge'] = lambda: Hebart2023fmri('IT', 'ridge')
 
-benchmark_registry['Hebart2023_fmri.V1-neuron_to_neuron'] = lambda: Hebart2023fmri('V1', 'neuron_to_neuron')
-benchmark_registry['Hebart2023_fmri.V2-neuron_to_neuron'] = lambda: Hebart2023fmri('V2', 'neuron_to_neuron')
-benchmark_registry['Hebart2023_fmri.V4-neuron_to_neuron'] = lambda: Hebart2023fmri('V4', 'neuron_to_neuron')
-benchmark_registry['Hebart2023_fmri.IT-neuron_to_neuron'] = lambda: Hebart2023fmri('IT', 'neuron_to_neuron')
+benchmark_registry['Hebart2023_fmri.V1-ridgecv'] = lambda: Hebart2023fmri('V1', 'ridgecv')
+benchmark_registry['Hebart2023_fmri.V2-ridgecv'] = lambda: Hebart2023fmri('V2', 'ridgecv')
+benchmark_registry['Hebart2023_fmri.V4-ridgecv'] = lambda: Hebart2023fmri('V4', 'ridgecv')
+benchmark_registry['Hebart2023_fmri.IT-ridgecv'] = lambda: Hebart2023fmri('IT', 'ridgecv')
--- brainscore_vision/benchmarks/papale2025/__init__.py
+++ brainscore_vision/benchmarks/papale2025/__init__.py
@@ -1,15 +1,11 @@
 from brainscore_vision import benchmark_registry
 from .benchmark import Papale2025
 
-benchmark_registry['Papale2025.V1-pls'] = lambda: Papale2025('V1', 'pls')
-benchmark_registry['Papale2025.V4-pls'] = lambda: Papale2025('V4', 'pls')
-benchmark_registry['Papale2025.IT-pls'] = lambda: Papale2025('IT', 'pls')
-
 benchmark_registry['Papale2025.V1-ridge'] = lambda: Papale2025('V1', 'ridge')
 benchmark_registry['Papale2025.V4-ridge'] = lambda: Papale2025('V4', 'ridge')
 benchmark_registry['Papale2025.IT-ridge'] = lambda: Papale2025('IT', 'ridge')
 
-benchmark_registry['Papale2025.V1-neuron_to_neuron'] = lambda: Papale2025('V1', 'neuron_to_neuron')
-benchmark_registry['Papale2025.V4-neuron_to_neuron'] = lambda: Papale2025('V4', 'neuron_to_neuron')
-benchmark_registry['Papale2025.IT-neuron_to_neuron'] = lambda: Papale2025('IT', 'neuron_to_neuron')
+benchmark_registry['Papale2025.V1-ridgecv'] = lambda: Papale2025('V1', 'ridgecv')
+benchmark_registry['Papale2025.V4-ridgecv'] = lambda: Papale2025('V4', 'ridgecv')
+benchmark_registry['Papale2025.IT-ridgecv'] = lambda: Papale2025('IT', 'ridgecv')
 

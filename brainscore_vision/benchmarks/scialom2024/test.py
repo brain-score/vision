bbf1e9c40297c340acd2c0d8664b3a24a265b9a8
Michael Ferguson <mferg@mit.edu>
2025-11-10 14:22:13 -0500

    update brainio paths (#2126)

--- brainscore_vision/benchmarks/scialom2024/test.py
+++ brainscore_vision/benchmarks/scialom2024/test.py
@@ -7,7 +7,7 @@ from pytest import approx
 from brainscore_core.supported_data_standards.brainio.assemblies import BehavioralAssembly
 from brainscore_vision import benchmark_registry, load_benchmark
 from brainscore_vision.benchmark_helpers import PrecomputedFeatures
-from brainscore_vision.data_helpers import s3
+from brainscore_core.supported_data_standards.brainio import s3
 
 
 @pytest.mark.parametrize('benchmark', [

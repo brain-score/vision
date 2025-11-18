82585b35f4b593487ee79102574f796db1bb2408
Jenkins <jenkins@ip-172-31-29-217.us-east-2.compute.internal>
2025-11-18 09:27:26 -0500

    add resnet50_st_wzc_seed4_2 to models

---
+++ brainscore_vision/models/resnet50_st_wzc_seed4_2/__init__.py
@@ -0,0 +1,5 @@
+from brainscore_vision import model_registry
+from brainscore_vision.model_helpers.brain_transformation import ModelCommitment
+from .model import get_model, get_layers
+
+model_registry['resnet50_st_wzc_seed4_2'] = lambda: ModelCommitment(identifier='resnet50_st_wzc_seed4_2', activations_model=get_model('resnet50_st_wzc_seed4_2'), layers=get_layers('resnet50_st_wzc_seed4_2'))

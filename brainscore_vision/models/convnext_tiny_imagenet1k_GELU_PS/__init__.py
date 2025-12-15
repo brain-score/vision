ddf5da52d2dc345f25c12ec57cf23d6bfcb09200
Jenkins <jenkins@ip-172-31-29-217.us-east-2.compute.internal>
2025-12-15 09:11:38 -0500

    add convnext_tiny_imagenet1k_GELU_PS to models

---
+++ brainscore_vision/models/convnext_tiny_imagenet1k_GELU_PS/__init__.py
@@ -0,0 +1,9 @@
+from brainscore_vision import model_registry
+from brainscore_vision.model_helpers.brain_transformation import ModelCommitment
+from .model import get_model, get_layers
+
+model_registry['convnext_tiny_imagenet1k_GELU_PS'] = lambda: ModelCommitment(
+    identifier='convnext_tiny_imagenet1k_GELU_PS',
+    activations_model=get_model('convnext_tiny_imagenet1k_GELU_PS'),
+    layers=get_layers('convnext_tiny_imagenet1k_GELU_PS')
+)

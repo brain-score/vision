fe9cd00d4510076db6e480f86ce1097a339a80cd
Kartik Pradeepan <kartikspradeepan@gmail.com>
2026-01-15 14:16:48 -0500

    Hotfix: Update coggan ceiling function to match #2169 (#2248)
    * update coggan benchmarks
    
    * update test values

--- brainscore_vision/benchmarks/coggan2024_fMRI/test.py
+++ brainscore_vision/benchmarks/coggan2024_fMRI/test.py
@@ -14,10 +14,10 @@ def test_benchmark_registry(region):
 @pytest.mark.parametrize('region', ['V1', 'V2', 'V4', 'IT'])
 def test_benchmarks(region):
     expected_score = dict(
-        V1=0.0182585,
-        V2=0.3352083,
-        V4=0.3008136,
-        IT=0.4486508)[region]
+        V1=0.00817351,
+        V2=0.15040114,
+        V4=0.10059367,
+        IT=0.10755558)[region]
     model = load_model('alexnet')
     benchmark = load_benchmark(f'tong.Coggan2024_fMRI.{region}-rdm')
     score = benchmark(model)

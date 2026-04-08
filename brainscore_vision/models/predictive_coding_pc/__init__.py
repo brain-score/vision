0c335d9fdf1e90318398429868e03eaeaa498a7b
Nils Leutenegger <nils.leutenegger@gmail.com>
2026-04-08 09:45:13 +0200

    Add behavioral readout layer to model registry

--- brainscore_vision/models/predictive_coding_pc/__init__.py
+++ brainscore_vision/models/predictive_coding_pc/__init__.py
@@ -6,5 +6,11 @@ model_registry['predictive_coding_pc'] = lambda: ModelCommitment(
     identifier='predictive_coding_pc',
     activations_model=get_model('predictive_coding_pc'),
     layers=get_layers('predictive_coding_pc'),
-    region_layer_map={'V1': 'r0', 'V2': 'r1', 'V4': 'r1', 'IT': 'r3'},
+    region_layer_map={
+        'V1': 'r0',
+        'V2': 'r1',
+        'V4': 'r1',
+        'IT': 'r3',
+    },
+    behavioral_readout_layer='fc',
 )

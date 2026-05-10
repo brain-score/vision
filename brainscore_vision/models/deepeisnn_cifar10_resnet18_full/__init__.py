6d05ece59dec819a6c4027ff1b8a90ea31bbff90
Jenkins <jenkins@ip-172-31-29-217.us-east-2.compute.internal>
2026-05-10 01:58:21 -0500

    add deepeisnn_cifar10_resnet18_full to models

---
+++ brainscore_vision/models/deepeisnn_cifar10_resnet18_full/__init__.py
@@ -0,0 +1,13 @@
+from brainscore_vision import model_registry
+from brainscore_vision.model_helpers.brain_transformation import ModelCommitment
+
+from .model import get_behavioral_readout_layer, get_layers, get_model
+
+
+model_registry["deepeisnn_cifar10_resnet18_full"] = lambda: ModelCommitment(
+    identifier="deepeisnn_cifar10_resnet18_full",
+    activations_model=get_model("deepeisnn_cifar10_resnet18_full"),
+    layers=get_layers("deepeisnn_cifar10_resnet18_full"),
+    behavioral_readout_layer=get_behavioral_readout_layer("deepeisnn_cifar10_resnet18_full"),
+    visual_degrees=8,
+)

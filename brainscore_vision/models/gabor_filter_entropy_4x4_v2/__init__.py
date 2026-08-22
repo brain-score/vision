c642dbe42aae3ed064bc0b66f70a80b264021164
Kartik Pradeepan <kartikspradeepan@gmail.com>
2026-08-22 14:49:57 -0400

    add gabor_filter_entropy_4x4_v2 to models (#2495)
    Co-authored-by: Jenkins <jenkins@ip-172-31-29-217.us-east-2.compute.internal>

---
+++ brainscore_vision/models/gabor_filter_entropy_4x4_v2/__init__.py
@@ -0,0 +1,18 @@
+from brainscore_vision import model_registry
+from brainscore_vision.model_helpers.brain_transformation import ModelCommitment
+from .model import get_model, get_layers, LAYER_NAME
+
+
+_REGION_LAYER_MAP = {
+    'V1': LAYER_NAME,
+    'V2': LAYER_NAME,
+    'V4': LAYER_NAME,
+    'IT': LAYER_NAME,
+}
+
+
+model_registry['gabor_filter_entropy_4x4_v2'] = lambda: ModelCommitment(
+    identifier='gabor_filter_entropy_4x4_v2',
+    activations_model=get_model('gabor_filter_entropy_4x4_v2'),
+    layers=get_layers('gabor_filter_entropy_4x4_v2'),
+    region_layer_map=_REGION_LAYER_MAP)
---
+++ brainscore_vision/models/gabor_filter_entropy_4x4_v2/test.py
@@ -0,0 +1,8 @@
+from .descriptor import FoveatedGaborFilterEntropyExtractor
+
+
+def test_feature_count():
+    extractor = FoveatedGaborFilterEntropyExtractor()
+    n_filters = len(extractor.config.spatial_frequencies) * extractor.config.n_orientations
+    n_positions = len(range(0, extractor.config.image_size - extractor.mid_window + 1, extractor.base_stride))
+    assert n_filters * n_positions * n_positions == 2916

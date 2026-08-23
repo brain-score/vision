50bdfe6e7e8447304b994a32964d147b4d57e246
Jenkins <jenkins@ip-172-31-29-217.us-east-2.compute.internal>
2026-08-23 16:52:23 -0500

    add gabor_filter_entropy_4x4_v2_forcedCosine to models

---
+++ brainscore_vision/models/gabor_filter_entropy_4x4_v2_forcedCosine/__init__.py
@@ -0,0 +1,18 @@
+from brainscore_vision import model_registry
+from brainscore_vision.model_helpers.brain_transformation import ModelCommitment
+from .model import get_model, get_layers, LAYER_NAME
+
+
+_REGION_LAYER_MAP = {
+    'V1': LAYER_NAME,
+    'V2': LAYER_NAME,
+    'V4': LAYER_NAME,
+    'IT': LAYER_NAME,
+}
+
+
+model_registry['gabor_filter_entropy_4x4_v2_forcedCosine'] = lambda: ModelCommitment(
+    identifier='gabor_filter_entropy_4x4_v2_forcedCosine',
+    activations_model=get_model('gabor_filter_entropy_4x4_v2_forcedCosine'),
+    layers=get_layers('gabor_filter_entropy_4x4_v2_forcedCosine'),
+    region_layer_map=_REGION_LAYER_MAP)
---
+++ brainscore_vision/models/gabor_filter_entropy_4x4_v2_forcedCosine/model.py
@@ -0,0 +1,54 @@
+from collections import OrderedDict
+
+import numpy as np
+
+from brainscore_vision.model_helpers.activations.core import ActivationsExtractorHelper
+from .descriptor import PositiveNegativeGlobalGaborFilterEntropyExtractor, GaborEntropyConfig
+
+
+MODEL_IDENTIFIER = 'gabor_filter_entropy_4x4_v2_forcedCosine'
+LAYER_NAME = 'gabor_entropy_forcedCosine_foveated32_48_64'
+ALPHA = 0.5
+ACTIVATION_OFFSET = 8.0
+
+
+class GaborFilterEntropyForcedCosineModel:
+    def __init__(self, identifier=MODEL_IDENTIFIER, config=None):
+        self.config = config or GaborEntropyConfig()
+        self._descriptor = PositiveNegativeGlobalGaborFilterEntropyExtractor(self.config, alpha=ALPHA)
+        self._extractor = ActivationsExtractorHelper(
+            identifier=identifier,
+            preprocessing=None,
+            get_activations=self._get_activations)
+        self._extractor.insert_attrs(self)
+        self.image_size = self.config.image_size
+
+    @property
+    def identifier(self):
+        return self._extractor.identifier
+
+    @identifier.setter
+    def identifier(self, value):
+        self._extractor.identifier = value
+
+    def __call__(self, *args, **kwargs):
+        return self._extractor(*args, **kwargs)
+
+    def _get_activations(self, paths, layer_names):
+        np.testing.assert_array_equal(layer_names, [LAYER_NAME])
+        features = self._descriptor.extract_from_paths(paths) + ACTIVATION_OFFSET
+        return OrderedDict([(LAYER_NAME, features.astype(self.config.output_dtype, copy=False))])
+
+
+def get_model(name):
+    assert name == MODEL_IDENTIFIER
+    return GaborFilterEntropyForcedCosineModel(identifier=MODEL_IDENTIFIER)
+
+
+def get_layers(name):
+    assert name == MODEL_IDENTIFIER
+    return [LAYER_NAME]
+
+
+def get_bibtex(model_identifier):
+    return """"""

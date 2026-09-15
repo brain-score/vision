6947dbe348da87c562d6a0d1714311470778b2e2
Jenkins <jenkins@ip-172-31-29-217.us-east-2.compute.internal>
2026-09-15 07:13:43 -0500

    add resnet50_dvd_pure10deg_wzc_20260913_seed6_epoch90 to models

---
+++ brainscore_vision/models/resnet50_dvd_pure10deg_wzc_20260913_seed6_epoch90/test.py
@@ -0,0 +1,8 @@
+import pytest
+import brainscore_vision
+
+
+@pytest.mark.travis_slow
+def test_has_identifier():
+    model = brainscore_vision.load_model('resnet50_dvd_pure10deg_wzc_20260913_seed6_epoch90')
+    assert model.identifier == 'resnet50_dvd_pure10deg_wzc_20260913_seed6_epoch90'

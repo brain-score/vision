9c3a19de3fda17cacb73689688cf4eebd74c7e3e
Kartik Pradeepan <kartikspradeepan@gmail.com>
2026-09-22 12:08:18 -0400

    brain-score.org submission (user:843) | (public:False) (#2539)
    * add jem_imagenet_p0p3_s1 to models
    
    * Add jem_imagenet_p0p3_s1.json to region_layer_map for model jem_imagenet_p0p3_s1
    
    ---------
    
    Co-authored-by: Jenkins <jenkins@ip-172-31-29-217.us-east-2.compute.internal>

---
+++ brainscore_vision/models/jem_imagenet_p0p3_s1/test.py
@@ -0,0 +1,9 @@
+from brainscore_vision import load_model
+
+
+def test_load_model():
+    model = load_model('jem_imagenet_p0p3_s1')
+    assert model.identifier == 'jem_imagenet_p0p3_s1'
+    # Brain-Score reads 'logits' from the last registered leaf module: it must be the 1000-way classifier
+    activations_model = model.activations_model
+    assert activations_model._output_layer() is activations_model._model.jem.class_output

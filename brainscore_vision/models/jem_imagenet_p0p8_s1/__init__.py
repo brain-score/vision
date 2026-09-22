68e3db06a2383c56c07b65b3f0cf45ad022bfd7b
Jenkins <jenkins@ip-172-31-29-217.us-east-2.compute.internal>
2026-09-22 08:43:44 -0500

    add jem_imagenet_p0p8_s1 to models

---
+++ brainscore_vision/models/jem_imagenet_p0p8_s1/__init__.py
@@ -0,0 +1,5 @@
+from brainscore_vision import model_registry
+from brainscore_vision.model_helpers.brain_transformation import ModelCommitment
+from .model import get_model, get_layers
+
+model_registry['jem_imagenet_p0p8_s1'] = lambda: ModelCommitment(identifier='jem_imagenet_p0p8_s1', activations_model=get_model('jem_imagenet_p0p8_s1'), layers=get_layers('jem_imagenet_p0p8_s1'), visual_degrees=8)

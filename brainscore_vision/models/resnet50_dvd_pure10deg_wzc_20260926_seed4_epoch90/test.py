0e5a0ea0620ae3da5322ec5bbdc52cde87435695
Kartik Pradeepan <kartikspradeepan@gmail.com>
2026-09-26 09:41:39 -0400

    add resnet50_dvd_pure10deg_wzc_20260926_seed4_epoch90 to models (#2553)
    Co-authored-by: Jenkins <jenkins@ip-172-31-29-217.us-east-2.compute.internal>

---
+++ brainscore_vision/models/resnet50_dvd_pure10deg_wzc_20260926_seed4_epoch90/test.py
@@ -0,0 +1,8 @@
+import pytest
+import brainscore_vision
+
+
+@pytest.mark.travis_slow
+def test_has_identifier():
+    model = brainscore_vision.load_model('resnet50_dvd_pure10deg_wzc_20260926_seed4_epoch90')
+    assert model.identifier == 'resnet50_dvd_pure10deg_wzc_20260926_seed4_epoch90'

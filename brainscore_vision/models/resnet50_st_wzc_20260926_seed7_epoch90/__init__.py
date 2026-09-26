1ba4331a0ba4dd55ef31bfbf08fd87ef9bc9cf67
Jenkins <jenkins@ip-172-31-29-217.us-east-2.compute.internal>
2026-09-26 10:37:02 -0500

    add resnet50_st_wzc_20260926_seed7_epoch90 to models

---
+++ brainscore_vision/models/resnet50_st_wzc_20260926_seed7_epoch90/__init__.py
@@ -0,0 +1,12 @@
+from brainscore_vision import model_registry
+from brainscore_vision.model_helpers.brain_transformation import ModelCommitment
+from .model import get_model, get_layers
+
+REGION_LAYER_MAP = {'V1': 'layer2.1.conv1', 'V2': 'layer3.3.conv2', 'V4': 'layer4.0.conv2', 'IT': 'layer4.1.conv1'}
+
+model_registry['resnet50_st_wzc_20260926_seed7_epoch90'] = lambda: ModelCommitment(
+    identifier='resnet50_st_wzc_20260926_seed7_epoch90',
+    activations_model=get_model('resnet50_st_wzc_20260926_seed7_epoch90'),
+    layers=get_layers('resnet50_st_wzc_20260926_seed7_epoch90'),
+    region_layer_map=REGION_LAYER_MAP,
+)
